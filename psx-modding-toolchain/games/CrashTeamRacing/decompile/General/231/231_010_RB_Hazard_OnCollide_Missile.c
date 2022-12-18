4ccbe43de9d2b80867283b971e8ed17d32e91d23
Niko <a>
2022-12-18 17:29:42 -0500

    fix decomp missile crash

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_010_RB_Hazard_OnCollide_Missile.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_010_RB_Hazard_OnCollide_Missile.c
@@ -11,23 +11,25 @@ int RB_Hazard_OnCollide_Missile(struct Thread* thread)
 	
 	// could I also just do thread->modelID?
 	if (inst->model->id == 0x29)
-	{
+	{		
+		// play audio of explosion
+		PlaySound3D(0x4c, inst);
+		
+		// kill thread
+		thread->flags |= 0x800;
+
+		// stop audio of moving
+		OtherFX_Stop_Safe(&tw->audioPtr);
+
+		// get driver
 		driver = tw->driverTarget;
 		
+		// if driver is valid
 		if(driver != 0)
 		{
 			// remove 2D square-target being drawn on the player's screen 
 			driver->actionsFlagSet &= 0xfbffffff;
 		}
-		
-		// play audio of explosion
-		PlaySound3D(0x4c, inst);
-		
-		// stop audio of moving
-		OtherFX_Stop_Safe(tw->audioPtr);
-		
-		// kill thread
-		thread->flags |= 0x800;
 	}
 	
 	return 1;
