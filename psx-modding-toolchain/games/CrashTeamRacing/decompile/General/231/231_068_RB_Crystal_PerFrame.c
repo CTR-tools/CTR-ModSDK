6b534b444264cc945c088804238abf130c4ac75a
Niko <a>
2022-12-19 23:26:45 -0500

    progress

---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_068_RB_Crystal_PerFrame.c
@@ -0,0 +1,27 @@
+#include <common.h>
+
+void DECOMP_RB_Crystal_PerFrame(struct Thread* t)
+{
+	int sine;
+	struct Instance* crystalInst;
+	struct Crystal* crystalObj;
+	
+	crystalInst = t->inst;
+	crystalObj = t->object;
+	
+	// rotate each frame
+	crystalObj->rot[1] += 0x80;
+	ConvertRotToMatrix(&crystalInst->matrix, &crystalObj->rot[0]);
+	
+	// sine curve for vertical bounce
+	sine = MATH_Sin(crystalObj->rot[1]);
+	
+	// set posY
+	crystalInst->matrix.t[1] =
+		crystalInst->instDef->pos[1] + 	// original posY
+		((sine << 4) >> 0xc) +			// sine (bounce up/down)
+		0x30;							// airborne bump
+	
+	// to do: find 0x800b5968 and reallocate (probably light dir)
+	SpecularLight_Spinning3D(crystalInst, &crystalObj->rot[0], 0x800b5968);
+}
