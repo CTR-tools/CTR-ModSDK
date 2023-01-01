c80a1a6f95b2b9feafbd4ae138c7aa9e05546728
Niko <a>
2022-12-31 21:34:33 -0500

    optimize function

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DecalFont_GetLineWidthStrlen.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DecalFont_GetLineWidthStrlen.c
@@ -6,6 +6,7 @@ int DECOMP_DecalFont_GetLineWidthStrlen(char* character, int len, int fontType)
 	short font_buttonPixWidth;
 	short font_puncPixWidth;
 	int pixLength;
+	char c;
 	
 	font_charPixWidth = data.font_charPixWidth[fontType];
 	font_buttonPixWidth = data.font_buttonPixWidth[fontType];
@@ -14,44 +15,51 @@ int DECOMP_DecalFont_GetLineWidthStrlen(char* character, int len, int fontType)
 
 	while((*character != 0) && (len != 0))
 	{
-		switch(*character)
+		c = *character;
+		
+		// do not use "switch" or "else if" that increases 
+		// number of bytes, and makes the function too large
+		
+		// if the character is one of the PSX buttons
+		// @ is circle, [ is square, ^ is triangle, * is cross
+		if(
+			(c == '@') ||
+			(c == '[') ||
+			(c == '^') ||
+			(c == '*')
+		)
 		{
-			// if the character is one of the PSX buttons
-			// @ is circle, [ is square, ^ is triangle, * is cross
-			case '@':
-			case '[':
-			case '^':
-			case '*':
-				// character width, plus extra spacing for button
-				pixLength += font_charPixWidth + font_buttonPixWidth;
-				break;
-				
-			// colon or period
-			case ':':
-			case '.':
-				// punctuation spacing
-				pixLength += font_puncPixWidth;
-				break;
+			// character width, plus extra spacing for button
+			pixLength += font_charPixWidth + font_buttonPixWidth;
+		}
+		
+		// colon or period
+		if(
+			(c == ':') ||
+			(c == '.')
+		)
+		{
+			// punctuation spacing
+			pixLength += font_puncPixWidth;
+		}
+		
+		#if BUILD > UsaRetail
+		if(c == '~')
+		{
+			character += 2;
+			len += 2;
+		}
+		#endif
 
-			#if BUILD > UsaRetail
-			case '~':
-				character += 2;
-				len += 2;
-				break;
-			#endif
-
-			default:
-				// if normal character
-				#if BUILD != EurRetail
-				if(*character > 2)
-				#else
-				if(*character > 3)
-				#endif
-				{
-					// normal character spacing
-					pixLength += font_charPixWidth;
-				}
-				break;
+		// if normal character
+		#if BUILD != EurRetail
+		if(c > 2)
+		#else
+		if(c > 3)
+		#endif
+		{
+			// normal character spacing
+			pixLength += font_charPixWidth;
 		}
 
 		character++;
