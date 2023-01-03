8dbed9cfaf46b8a602f031cbcbc660efe583bdfb
Niko <a>
2023-01-03 17:05:10 -0500

    progress

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/GAMEPROG_ResetHighScores.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/GAMEPROG_ResetHighScores.c
@@ -15,6 +15,15 @@ void GAMEPROG_ResetHighScores(struct GameProgress* gameProg)
 	{
 		track = &gameProg->highScoreTracks[i];
 		
+		#if 0
+		// all but two tracks
+		if(i > 1)
+		{
+			// temporary test
+			track->timeTrialFlags = 7;
+		}
+		#endif
+		
 		// for time trial and relic
 		for(j = 0; j < 2; j++)
 		{
