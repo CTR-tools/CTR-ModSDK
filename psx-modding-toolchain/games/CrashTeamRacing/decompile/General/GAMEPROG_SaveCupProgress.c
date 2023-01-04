7a8a5a281b17a0cfcef626c0e8ac6ab781dfdd8d
Niko <a>
2023-01-04 11:46:04 -0500

    progress

---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/GAMEPROG_SaveCupProgress.c
@@ -0,0 +1,28 @@
+#include <common.h>
+
+// same as the one in GAMEPROG_AdvPercent
+#define CHECK_PROG_BIT(rewards, bitIndex) \
+	((rewards[bitIndex>>5] >> (bitIndex & 0x1f)) & 1) != 0
+
+void GAMEPROG_SaveCupProgress()
+{
+	int i;
+	int* prog;
+	int bitIndex1;
+	int bitIndex2;
+	
+	prog = &sdata->gameProgress.Unlocks.characters_tracks;
+	
+	// 4 cups, 3 difficulties
+	for(i = 0; i < 12; i++)
+	{
+		// if cup is "currently" beaten
+		bitIndex1 = i + 0xC;
+		if(CHECK_PROG_BIT(prog, bitIndex1))
+		{
+			// set if cup was "previously" beaten
+			bitIndex2 = bitIndex1 + 0xC;
+			prog[bitIndex2>>5] |= 1 << (bitIndex2 & 0x1f);
+		}
+	}
+}
