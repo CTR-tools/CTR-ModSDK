494aafe1276e5bd44d7a1277ae43a04e68637ff7
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-02-25 16:48:13 -0400

    PaintJobs is now fully functional

--- psx-modding-toolchain/games/CrashTeamRacing/mods/Modules/PaintJobs_OnlyUSA/src/paints/PAINTP_R.c
+++ psx-modding-toolchain/games/CrashTeamRacing/mods/Modules/PaintJobs_OnlyUSA/src/paints/PAINTP_R.c
@@ -81,70 +81,70 @@ RECT floor_coco_pos __attribute__ ((section (".data"))) = {
 };
 
 RECT floor_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
-	.y = 250,
+	.x = 960,
+	.y = 232,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
-	.y = 254,
+	.x = 960,
+	.y = 234,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 251,
+	.x = 960,
+	.y = 236,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 255,
+	.x = 960,
+	.y = 238,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 224,
-	.y = 253,
+	.x = 960,
+	.y = 239,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 224,
-	.y = 255,
+	.x = 960,
+	.y = 244,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 240,
-	.y = 251,
+	.x = 960,
+	.y = 245,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 257,
+	.x = 960,
+	.y = 246,
 	.w = 16,
 	.h = 1
 };
 RECT floor_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 261,
+	.x = 960,
+	.y = 249,
 	.w = 16,
 	.h = 1
 };
 
 RECT floor_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 128,
-	.y = 253,
+	.x = 960,
+	.y = 250,
 	.w = 16,
 	.h = 1
 };
@@ -169,73 +169,73 @@ RECT motortop_coco_pos __attribute__ ((section (".data"))) = {
 	.h = 1
 };
 RECT motortop_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 320,
-	.y = 260,
+	.x = 160,
+	.y = 254,
 	.w = 16,
 	.h = 1
 };
 
 RECT motortop_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 336,
-	.y = 257,
+	.x = 176,
+	.y = 254,
 	.w = 16,
 	.h = 1
 };
 RECT motortop_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 336,
-	.y = 263,
+	.x = 192,
+	.y = 249,
 	.w = 16,
 	.h = 1
 };
 RECT motortop_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 368,
-	.y = 260,
+	.x = 192,
+	.y = 255,
 	.w = 16,
 	.h = 1
 };
 
 RECT motortop_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 368,
-	.y = 263,
+	.x = 208,
+	.y = 250,
 	.w = 16,
 	.h = 1
 };
 
 RECT motortop_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 384,
-	.y = 256,
+	.x = 224,
+	.y = 248,
 	.w = 16,
 	.h = 1
 };
 
 RECT motortop_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 400,
-	.y = 258,
+	.x = 224,
+	.y = 252,
 	.w = 16,
 	.h = 1
 };
 
 RECT motortop_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 416,
-	.y = 263,
+	.x = 240,
+	.y = 248,
 	.w = 16,
 	.h = 1
 };
 RECT motortop_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 432,
-	.y = 259,
+	.x = 240,
+	.y = 252,
 	.w = 16,
 	.h = 1
 };
 RECT motortop_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 928,
-	.y = 250,
+	.x = 240,
+	.y = 255,
 	.w = 16,
 	.h = 1
 };
 RECT motortop_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 944,
-	.y = 245,
+	.x = 448,
+	.y = 260,
 	.w = 16,
 	.h = 1
 };
@@ -259,74 +259,74 @@ RECT brown_coco_pos __attribute__ ((section (".data"))) = {
 	.h = 1
 };
 RECT brown_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 176,
-	.y = 252,
+	.x = 992,
+	.y = 228,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
-	.y = 252,
+	.x = 992,
+	.y = 229,
 	.w = 16,
 	.h = 1
 };
 RECT brown_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
-	.y = 253,
+	.x = 992,
+	.y = 230,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 252,
+	.x = 992,
+	.y = 231,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 253,
+	.x = 992,
+	.y = 232,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 224,
-	.y = 250,
+	.x = 992,
+	.y = 234,
 	.w = 16,
 	.h = 1
 };
 RECT brown_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 240,
-	.y = 249,
+	.x = 992,
+	.y = 236,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 240,
-	.y = 253,
+	.x = 992,
+	.y = 237,
 	.w = 16,
 	.h = 1
 };
 RECT brown_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 256,
+	.x = 992,
+	.y = 238,
 	.w = 16,
 	.h = 1
 };
 RECT brown_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 259,
+	.x = 992,
+	.y = 239,
 	.w = 16,
 	.h = 1
 };
 
 RECT brown_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 128,
-	.y = 255,
+	.x = 992,
+	.y = 240,
 	.w = 16,
 	.h = 1
 };
@@ -350,123 +350,123 @@ RECT front_coco_pos __attribute__ ((section (".data"))) = {
 	.h = 1
 };
 RECT front_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 231,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 232,
-	.w = 16,
-	.h = 1
-};
-RECT front_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 234,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 236,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 238,
-	.w = 16,
-	.h = 1
-};
-RECT front_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 239,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 244,
-	.w = 16,
-	.h = 1
-};
-RECT front_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 245,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 246,
-	.w = 16,
-	.h = 1
-};
-RECT front_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 249,
-	.w = 16,
-	.h = 1
-};
-
-RECT front_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 960,
-	.y = 250,
-	.w = 16,
-	.h = 1
-};
-
-RECT bridge_penta_pos __attribute__ ((section (".data"))) = {
 	.x = 160,
-	.y = 251,
-	.w = 16,
-	.h = 1
-};
-
-RECT bridge_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 160,
-	.y = 254,
-	.w = 16,
-	.h = 1
-};
-
-RECT bridge_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 176,
-	.y = 254,
-	.w = 16,
-	.h = 1
-};
-
-RECT bridge_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
-	.y = 249,
-	.w = 16,
-	.h = 1
-};
-
-RECT bridge_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 192,
 	.y = 255,
 	.w = 16,
 	.h = 1
 };
 
-RECT bridge_pinstripe_pos __attribute__ ((section (".data"))) = {
+RECT front_dingo_pos __attribute__ ((section (".data"))) = {
+	.x = 176,
+	.y = 253,
+	.w = 16,
+	.h = 1
+};
+RECT front_polar_pos __attribute__ ((section (".data"))) = {
+	.x = 192,
+	.y = 251,
+	.w = 16,
+	.h = 1
+};
+
+RECT front_pura_pos __attribute__ ((section (".data"))) = {
 	.x = 208,
+	.y = 248,
+	.w = 16,
+	.h = 1
+};
+
+RECT front_pinstripe_pos __attribute__ ((section (".data"))) = {
+	.x = 208,
+	.y = 249,
+	.w = 16,
+	.h = 1
+};
+RECT front_papu_pos __attribute__ ((section (".data"))) = {
+	.x = 208,
+	.y = 254,
+	.w = 16,
+	.h = 1
+};
+
+RECT front_roo_pos __attribute__ ((section (".data"))) = {
+	.x = 224,
+	.y = 251,
+	.w = 16,
+	.h = 1
+};
+RECT front_joe_pos __attribute__ ((section (".data"))) = {
+	.x = 224,
+	.y = 254,
+	.w = 16,
+	.h = 1
+};
+
+RECT front_tropy_pos __attribute__ ((section (".data"))) = {
+	.x = 240,
 	.y = 250,
+	.w = 16,
+	.h = 1
+};
+RECT front_penta_pos __attribute__ ((section (".data"))) = {
+	.x = 240,
+	.y = 254,
+	.w = 16,
+	.h = 1
+};
+
+RECT front_fakecrash_pos __attribute__ ((section (".data"))) = {
+	.x = 448,
+	.y = 258,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_penta_pos __attribute__ ((section (".data"))) = {
+	.x = 256,
+	.y = 246,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_fakecrash_pos __attribute__ ((section (".data"))) = {
+	.x = 256,
+	.y = 251,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_dingo_pos __attribute__ ((section (".data"))) = {
+	.x = 320,
+	.y = 259,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_polar_pos __attribute__ ((section (".data"))) = {
+	.x = 336,
+	.y = 258,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_pura_pos __attribute__ ((section (".data"))) = {
+	.x = 352,
+	.y = 256,
+	.w = 16,
+	.h = 1
+};
+
+RECT bridge_pinstripe_pos __attribute__ ((section (".data"))) = {
+	.x = 368,
+	.y = 258,
 	.w = 16,
 	.h = 1
 };
 
 RECT bridge_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 224,
-	.y = 248,
+	.x = 368,
+	.y = 262,
 	.w = 16,
 	.h = 1
 };
@@ -486,8 +486,8 @@ RECT bridge_coco_pos __attribute__ ((section (".data"))) = {
 };
 
 RECT bridge_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 240,
-	.y = 252,
+	.x = 432,
+	.y = 258,
 	.w = 16,
 	.h = 1
 };
@@ -500,21 +500,21 @@ RECT bridge_cortex_pos __attribute__ ((section (".data"))) = {
 };
 
 RECT bridge_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 260,
+	.x = 928,
+	.y = 249,
 	.w = 16,
 	.h = 1
 };
 
 RECT bridge_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 128,
-	.y = 252,
+	.x = 928,
+	.y = 253,
 	.w = 16,
 	.h = 1
 };
 RECT bridge_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 144,
-	.y = 251,
+	.x = 944,
+	.y = 248,
 	.w = 16,
 	.h = 1
 };
@@ -540,124 +540,124 @@ RECT motorside_coco_pos __attribute__ ((section (".data"))) = {
 };
 
 RECT motorside_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 228,
+	.x = 176,
+	.y = 249,
 	.w = 16,
 	.h = 1
 };
 
 RECT motorside_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 229,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 230,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 231,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 232,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 234,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 236,
-	.w = 16,
-	.h = 1
-};
-RECT motorside_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 237,
-	.w = 16,
-	.h = 1
-};
-RECT motorside_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 238,
-	.w = 16,
-	.h = 1
-};
-
-RECT motorside_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 239,
-	.w = 16,
-	.h = 1
-};
-RECT motorside_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 992,
-	.y = 240,
-	.w = 16,
-	.h = 1
-};
-
-RECT exhaust_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 160,
-	.y = 249,
-	.w = 16,
-	.h = 1
-};
-
-RECT exhaust_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 160,
+	.x = 176,
 	.y = 255,
 	.w = 16,
 	.h = 1
 };
 
-RECT exhaust_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 176,
-	.y = 253,
+RECT motorside_polar_pos __attribute__ ((section (".data"))) = {
+	.x = 192,
+	.y = 250,
 	.w = 16,
 	.h = 1
 };
 
-RECT exhaust_polar_pos __attribute__ ((section (".data"))) = {
+RECT motorside_pura_pos __attribute__ ((section (".data"))) = {
 	.x = 192,
+	.y = 254,
+	.w = 16,
+	.h = 1
+};
+
+RECT motorside_pinstripe_pos __attribute__ ((section (".data"))) = {
+	.x = 208,
 	.y = 251,
 	.w = 16,
 	.h = 1
 };
 
-RECT exhaust_pura_pos __attribute__ ((section (".data"))) = {
+RECT motorside_papu_pos __attribute__ ((section (".data"))) = {
 	.x = 208,
-	.y = 248,
+	.y = 255,
+	.w = 16,
+	.h = 1
+};
+
+RECT motorside_roo_pos __attribute__ ((section (".data"))) = {
+	.x = 224,
+	.y = 253,
+	.w = 16,
+	.h = 1
+};
+RECT motorside_joe_pos __attribute__ ((section (".data"))) = {
+	.x = 224,
+	.y = 255,
+	.w = 16,
+	.h = 1
+};
+RECT motorside_tropy_pos __attribute__ ((section (".data"))) = {
+	.x = 240,
+	.y = 251,
+	.w = 16,
+	.h = 1
+};
+
+RECT motorside_penta_pos __attribute__ ((section (".data"))) = {
+	.x = 448,
+	.y = 257,
+	.w = 16,
+	.h = 1
+};
+RECT motorside_fakecrash_pos __attribute__ ((section (".data"))) = {
+	.x = 448,
+	.y = 261,
+	.w = 16,
+	.h = 1
+};
+
+RECT exhaust_penta_pos __attribute__ ((section (".data"))) = {
+	.x = 256,
+	.y = 247,
+	.w = 16,
+	.h = 1
+};
+
+RECT exhaust_fakecrash_pos __attribute__ ((section (".data"))) = {
+	.x = 256,
+	.y = 252,
+	.w = 16,
+	.h = 1
+};
+
+RECT exhaust_dingo_pos __attribute__ ((section (".data"))) = {
+	.x = 320,
+	.y = 260,
+	.w = 16,
+	.h = 1
+};
+
+RECT exhaust_polar_pos __attribute__ ((section (".data"))) = {
+	.x = 336,
+	.y = 257,
+	.w = 16,
+	.h = 1
+};
+
+RECT exhaust_pura_pos __attribute__ ((section (".data"))) = {
+	.x = 336,
+	.y = 263,
 	.w = 16,
 	.h = 1
 };
 
 RECT exhaust_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 249,
+	.x = 368,
+	.y = 260,
 	.w = 16,
 	.h = 1
 };
 
 RECT exhaust_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 208,
-	.y = 254,
+	.x = 368,
+	.y = 263,
 	.w = 16,
 	.h = 1
 };
@@ -681,27 +681,27 @@ RECT exhaust_coco_pos __attribute__ ((section (".data"))) = {
 	.h = 1
 };
 RECT exhaust_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 240,
-	.y = 254,
+	.x = 432,
+	.y = 259,
 	.w = 16,
 	.h = 1
 };
 
 RECT exhaust_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 448,
-	.y = 258,
+	.x = 928,
+	.y = 250,
 	.w = 16,
 	.h = 1
 };
 RECT exhaust_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 128,
-	.y = 254,
+	.x = 944,
+	.y = 245,
 	.w = 16,
 	.h = 1
 };
 
 RECT exhaust_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 144,
+	.x = 944,
 	.y = 249,
 	.w = 16,
 	.h = 1
@@ -729,74 +729,74 @@ RECT back_coco_pos __attribute__ ((section (".data"))) = {
 };
 
 RECT back_ngin_pos __attribute__ ((section (".data"))) = {
-	.x = 336,
-	.y = 258,
+	.x = 176,
+	.y = 248,
 	.w = 16,
 	.h = 1
 };
 RECT back_dingo_pos __attribute__ ((section (".data"))) = {
-	.x = 352,
-	.y = 256,
+	.x = 176,
+	.y = 252,
 	.w = 16,
 	.h = 1
 };
 
 RECT back_polar_pos __attribute__ ((section (".data"))) = {
-	.x = 368,
-	.y = 258,
+	.x = 192,
+	.y = 252,
 	.w = 16,
 	.h = 1
 };
 RECT back_pura_pos __attribute__ ((section (".data"))) = {
-	.x = 368,
-	.y = 262,
-	.w = 16,
-	.h = 1
-};
-
-RECT back_pinstripe_pos __attribute__ ((section (".data"))) = {
-	.x = 400,
-	.y = 257,
-	.w = 16,
-	.h = 1
-};
-RECT back_papu_pos __attribute__ ((section (".data"))) = {
-	.x = 416,
-	.y = 262,
-	.w = 16,
-	.h = 1
-};
-
-RECT back_roo_pos __attribute__ ((section (".data"))) = {
-	.x = 432,
-	.y = 258,
-	.w = 16,
-	.h = 1
-};
-
-RECT back_joe_pos __attribute__ ((section (".data"))) = {
-	.x = 496,
-	.y = 263,
-	.w = 16,
-	.h = 1
-};
-RECT back_tropy_pos __attribute__ ((section (".data"))) = {
-	.x = 928,
-	.y = 249,
-	.w = 16,
-	.h = 1
-};
-
-RECT back_penta_pos __attribute__ ((section (".data"))) = {
-	.x = 928,
+	.x = 192,
 	.y = 253,
 	.w = 16,
 	.h = 1
 };
 
+RECT back_pinstripe_pos __attribute__ ((section (".data"))) = {
+	.x = 208,
+	.y = 252,
+	.w = 16,
+	.h = 1
+};
+RECT back_papu_pos __attribute__ ((section (".data"))) = {
+	.x = 208,
+	.y = 253,
+	.w = 16,
+	.h = 1
+};
+
+RECT back_roo_pos __attribute__ ((section (".data"))) = {
+	.x = 224,
+	.y = 250,
+	.w = 16,
+	.h = 1
+};
+
+RECT back_joe_pos __attribute__ ((section (".data"))) = {
+	.x = 240,
+	.y = 249,
+	.w = 16,
+	.h = 1
+};
+RECT back_tropy_pos __attribute__ ((section (".data"))) = {
+	.x = 240,
+	.y = 253,
+	.w = 16,
+	.h = 1
+};
+
+RECT back_penta_pos __attribute__ ((section (".data"))) = {
+	.x = 448,
+	.y = 256,
+	.w = 16,
+	.h = 1
+};
+
 RECT back_fakecrash_pos __attribute__ ((section (".data"))) = {
-	.x = 944,
-	.y = 248,
+	.x = 448,
+	.y = 259,
 	.w = 16,
 	.h = 1
 };
