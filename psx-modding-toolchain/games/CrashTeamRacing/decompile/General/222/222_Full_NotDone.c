e5330ee30390989d22575943fd62ecea310dc148
Niko <a>
2023-03-04 09:13:49 -0500

    222 progress

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/222/222_Full_NotDone.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/222/222_Full_NotDone.c
@@ -13,6 +13,14 @@ void AA_EndEvent_DrawMenu()
 	struct HudElement* hudArray;
 	struct AdvProgress* adv;
 	int bitIndex;
+	short posXY[2];
+	
+	short lerpStartX;
+	short lerpStartY;
+	short lerpEndX;
+	short lerpEndY;
+	short lerpFrames;
+	short currFrame;
 	
 	gGT = sdata->gGT;
 	driver = gGT->drivers[0];
@@ -47,13 +55,50 @@ void AA_EndEvent_DrawMenu()
 			bitIndex = gGT->levelID + 0x4C;
 			if(CHECK_ADV_BIT(adv->rewards, bitIndex))
 			{
+				// first 10 seconds, lerp closer to center of screen
+				if(sdata->framesSinceRaceEnded < 301)
+				{
+					lerpStartX = data.hud_1P_P1[0x24].x;
+					lerpStartY = data.hud_1P_P1[0x24].y;
+					lerpEndX = data.hud_1P_P1[0x24].x + 0x10;
+					lerpEndY = data.hud_1P_P1[0x24].y + 0x10;
+					lerpFrames = 8;
+					currFrame = sdata->framesSinceRaceEnded;
+				}
 				
+				// past 10 seconds, lerp off screen
+				else
+				{
+					lerpStartX = data.hud_1P_P1[0x24].x + 0x10;
+					lerpStartY = data.hud_1P_P1[0x24].y + 0x10;
+					lerpEndX = -400;
+					lerpEndY = lerpStartY;
+					lerpFrames = 10;
+					currFrame = sdata->framesSinceRaceEnded - 300;
+				}
+				
+				InterpolatePosition2D_Linear(
+					&posXY[0], lerpStartX, lerpStartY, 
+					lerpEndX, lerpEndY, currFrame, lerpFrames);
+					
+				sdata->ptrHudC->matrix.t[0] = UI_ConvertX_2(posXY[0], 0x200);
+				sdata->ptrHudC->matrix.t[1] = UI_ConvertY_2(posXY[1], 0x200);
+				
+				sdata->ptrHudT->matrix.t[0] = UI_ConvertX_2(posXY[0]+0x1d, 0x200);
+				sdata->ptrHudT->matrix.t[1] = UI_ConvertY_2(posXY[1]-1, 0x200);
+				
+				sdata->ptrHudR->matrix.t[0] = UI_ConvertX_2(posXY[0]+0x3a, 0x200);
+				sdata->ptrHudR->matrix.t[1] = UI_ConvertY_2(posXY[1], 0x200);
 			}
 			
 			// if won for the first time
 			else
 			{
+				// what on earth does it do with this?
+				// sdata->ptrHudC->scale[0] - 0x800
 				
+				// copy HudElement X and Y
+				*(int*)&posXY[0] = *(int*)&data.hud_1P_P1[0x24];
 			}
 		}
 		
