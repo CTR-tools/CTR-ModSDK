6474b18449f7dd2a1019e382fec71e98f5a53039
Niko <a>
2023-03-12 16:45:46 -0400

    BaronLInB

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_054_055_RB_Baron.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_054_055_RB_Baron.c
@@ -1,6 +1,7 @@
 #include <common.h>
 
-void RB_Baron_ThTick(struct Thread* t)
+#if 0
+void DECOMP_RB_Baron_ThTick(struct Thread* t)
 {
 	struct Instance* baronInst;
 	struct Baron* baronObj;
@@ -37,4 +38,48 @@ void RB_Baron_ThTick(struct Thread* t)
 	if(level1->numSpawnType2_PosRot == 0) return;
 
 	
+}
+#endif
+
+void RB_Baron_ThTick();
+
+void DECOMP_RB_Baron_LInB(struct Instance* inst)
+{
+	struct Baron* baronObj;
+	
+	struct Thread* t = 
+		THREAD_BirthWithObject
+		(
+			// creation flags
+			SIZE_RELATIVE_POOL_BUCKET
+			(
+				sizeof(struct Baron), 
+				NONE, 
+				SMALL, 
+				STATIC
+			), 
+			
+			RB_Baron_ThTick,	// behavior
+			0,						// debug name
+			0						// thread relative
+		);
+		
+	if(t == 0) return;
+	inst->thread = t;
+	t->inst = inst;
+	
+	inst->scale[0] = 0x1000;
+	inst->scale[1] = 0x1000;
+	inst->scale[2] = 0x1000;
+	
+	#if 0
+	// Removed, Baron Plane would spawn two
+	// planes, one starts at pointIndex=1,
+	// the other one at pointIndex=NumPoints/2
+	#endif
+	
+	baronObj = ((struct Baron*)t->object);
+	baronObj->otherInst = 0;
+	baronObj->pointIndex = 1;
+	baronObj->soundID_flags = 0;
 }
