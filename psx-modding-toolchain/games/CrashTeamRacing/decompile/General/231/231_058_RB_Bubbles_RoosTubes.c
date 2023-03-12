4198d183e00dec674e9c347490af6e1f09381f45
Niko <a>
2023-03-12 13:35:31 -0400

    progress

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_058_RB_Bubbles_RoosTubes.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/231/231_058_RB_Bubbles_RoosTubes.c
@@ -1,5 +1,7 @@
 #include <common.h>
 
+extern struct ParticleEmitter emSet_TubeBubbles[7];
+
 void DECOMP_RB_Bubbles_RoosTubes()
 {
 	struct GameTracker* gGT;
@@ -66,8 +68,7 @@ void DECOMP_RB_Bubbles_RoosTubes()
 		
 		// == spawn particle ==
 		
-		// 0x800b3bc0 is under RB_Bubbles_RoosTubes, in the same file
-		p = Particle_CreateInstance(0, gGT->iconGroup[7], 0x800b3bc0);
+		p = Particle_CreateInstance(0, gGT->iconGroup[7], &emSet_TubeBubbles[0]);
 		
 		if(p == 0) return;
 		
@@ -81,4 +82,137 @@ void DECOMP_RB_Bubbles_RoosTubes()
 	}
 }
 
-// [Spawn Data Here]
+struct ParticleEmitter emSet_TubeBubbles[7] =
+{
+	[0] =
+	{
+		.flags = 1,
+		
+		// invalid axis, assume FuncInit
+		.initOffset = 0xC,
+		
+		.InitTypes.FuncInit =
+		{	
+			.particle_funcPtr = 0,
+			.particle_colorFlags = 0x4A0,
+			.particle_lifespan = 0x32,
+			.particle_Type = 0,
+		}
+		
+		// last 0x10 bytes are blank
+	},
+	
+	[1] =
+	{
+		.flags = 0x1B,
+		
+		// posX
+		.initOffset = 0,
+		
+		.InitTypes.AxisInit =
+		{
+			.baseValue =
+			{
+				.startVal = -0x200,
+				.velocity = -0x200,
+				.accel = 0,
+			},
+			
+			.rngSeed =
+			{
+				.startVal = 0x400,
+				.velocity = 0x400,
+				.accel = 0,
+			}
+		}
+		
+		// last 0x10 are blank
+	},
+	
+	[2] =
+	{
+		.flags = 0x24,
+		
+		// posY
+		.initOffset = 1,
+		
+		.InitTypes.AxisInit =
+		{
+			.baseValue =
+			{
+				.startVal = 0,
+				.velocity = 0,
+				.accel = 0x64,
+			},
+			
+			.rngSeed =
+			{
+				.startVal = 0,
+				.velocity = 0,
+				.accel = 0xC8,
+			}
+		}
+		
+		// last 0x10 are blank
+	},
+	
+	[3] =
+	{
+		.flags = 0x1B,
+		
+		// posZ
+		.initOffset = 2,
+		
+		.InitTypes.AxisInit =
+		{
+			.baseValue =
+			{
+				.startVal = -0x200,
+				.velocity = -0x200,
+				.accel = 0,
+			},
+			
+			.rngSeed =
+			{
+				.startVal = 0x400,
+				.velocity = 0x400,
+				.accel = 0,
+			}
+		}
+		
+		// last 0x10 are blank
+	},
+	
+	[4] =
+	{
+		.flags = 1,
+		
+		// Scale
+		.initOffset = 5,
+		
+		// 20% scale
+		.InitTypes.AxisInit.baseValue.startVal = 0x200,
+		
+		// all the rest is untouched
+	},
+	
+	[5] =
+	{
+		.flags = 1,
+		
+		// RotX?
+		.initOffset = 4,
+		
+		.InitTypes.AxisInit.baseValue.startVal = 1,
+		
+		// all the rest is untouched
+	},
+	
+	// null terminator
+	[6] = {}
+	
+	#if 0
+	// original had [6], [7], [8], and [9] 
+	// all null terminators, would bubbles change color?
+	#endif
+};
