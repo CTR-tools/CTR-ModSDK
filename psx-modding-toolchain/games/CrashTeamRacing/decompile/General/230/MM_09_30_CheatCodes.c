794474a9dbf600723167b7209f75dd0a777aa1af
Niko <a>
2023-03-18 12:41:02 -0400

    22%

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/230/MM_09_30_CheatCodes.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/230/MM_09_30_CheatCodes.c
@@ -472,7 +472,7 @@ void DECOMP_MM_ParseCheatCodes()
 	DecalFont_DrawLine(info, 		5, 197, FONT_SMALL, 0xffff0000);
 	DecalFont_DrawLine(__DATE__, 	5, 206, FONT_SMALL, 0xffff0000);
 	DecalFont_DrawLine(__TIME__, 	170, 206, FONT_SMALL, 0xffff0000);
-	DecalFont_DrawLine("20%", 		285, 206, FONT_SMALL, 0xffff0000);
+	DecalFont_DrawLine("22%", 		285, 206, FONT_SMALL, 0xffff0000);
 	#endif
 	
 	gpad = &sdata->gGamepads->gamepad[0];
