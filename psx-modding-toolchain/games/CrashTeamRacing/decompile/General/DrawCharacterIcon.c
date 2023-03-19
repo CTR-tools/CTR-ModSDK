7acf03522456bfcce2e2ece7c361bdcb9123c81a
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-03-19 01:00:26 -0400

    revise DecalHUD_DrawPolyGT4 and DrawCharacterIcon

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DrawCharacterIcon.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DrawCharacterIcon.c
@@ -1,24 +1,14 @@
 #include <common.h>
 
-void DECOMP_DrawCharacterIcon(struct Icon* icon, short posX, int posY, struct PrimMem* primMem, void *ot, u_char semitransparencyEnabled, short scale, u_int vcolorAndCode)
+void DECOMP_DrawCharacterIcon(struct Icon* icon, short posX, short posY, struct PrimMem* primMem, u_long* ot, u_char semitransparencyEnabled, short scale, u_int vcolorAndCode)
 {
-	u_char x1;
-	u_char x0;
-	u_char y2;
-	u_char y0;
-	u_short bottomRightCorner;
-	int iVar6;
-	int height;
-	int width;
-	POLY_FT4 *p;
+	short iVar6;
+	short height;
+	short width;
+	POLY_FT4* p;
 
-	// PrimMem->curr
 	p = (POLY_FT4*)primMem->curr;
-
-	x1 = icon->X2;
-	x0 = icon->X1;
-	y2 = icon->Y3;
-	y0 = icon->Y1;
+	setPolyFT4(p);
 
 	// vertex color and code
 	*(u_int*)&p->r0 = vcolorAndCode;
@@ -29,76 +19,60 @@ void DECOMP_DrawCharacterIcon(struct Icon* icon, short posX, int posY, struct Pr
 	// UVs and texpage
 	*(u_int*)&p->u1 = *(u_int*)&icon->X2;
 
-	width = (u_int)x1 - (u_int)x0;
+	width = icon->X2 - icon->X1;
 
 	// UVs and... pad1?
 	*(u_int*)&p->u2 = *(u_int*)&icon->X3;
 
-	bottomRightCorner = *(u_short*)&icon->X4;
 	p->x0 = posX;
-	*(u_short*)&p->u3 = bottomRightCorner;
+	p->u3 = icon->X4;
 
-	height = (u_int)y2 - (u_int)y0;
+	height = icon->Y3 - icon->Y1;
 
-	if (posY < 0xa6)
-	{
-		p->y0 = (short)posY;
-	}
-
+	if (posY < 166)
+		p->y0 = posY;
 	else
-	{
-		p->y0 = 0xa5;
-	}
+		p->y0 = 165;
 
-	p->x1 = posX + (short)(width * scale >> 0xc);
+	p->x1 = posX + (short)(width * scale >> 12);
 
-	if (posY < 0xa6)
-	{
-		p->y1 = (short)posY;
-	}
-
+	if (posY < 166)
+		p->y1 = posY;
 	else
-	{
-		p->y1 = 0xa5;
-	}
+		p->y1 = 165;
 
-	iVar6 = posY + (height * scale >> 0xc);
+	iVar6 = posY + (height * scale >> 12);
 	p->x2 = posX;
 
-	if (iVar6 < 0xa6)
-	{
-		p->y2 = (short)iVar6;
-	}
-
+	if (iVar6 < 166)
+		p->y2 = iVar6;
 	else
-	{
-		p->y2 = 0xa5;
-	}
+		p->y2 = 165;
 
-	p->x3 = posX + (short)(width * scale >> 0xc);
-	height = posY + (height * scale >> 0xc);
+	p->x3 = posX + (short)(width * scale >> 12);
+	height = posY + (height * scale >> 12);
 
-	if (height < 0xa6)
-	{
+	if (height < 166)
 		p->y3 = (short)height;
-	}
-
 	else
-	{
-		p->y3 = 0xa5;
-	}
-
-	setPolyFT4(p);
+		p->y3 = 165;
 
 	p->v2 = (p->v0 + *(u_char*)&p->y2) - (char)posY;
 	p->v3 = (p->v0 + *(u_char*)&p->y3) - (char)posY;
 
 	if (semitransparencyEnabled != 0)
 	{
-		p->tpage = p->tpage & 0xff9f | ((u_short)semitransparencyEnabled - 1) * 0x20;
+		// disable blending mode bits of the texpage using AND, then set them using OR
+		// blending mode bits on most Icon images are set to 11 (Mode 3, which is no blending)
+		// this function is always called with this parameter set to 1 (which is Mode 0, equivalent to 50% transparency)
+		// uh, I think so anyway, feel free to double check I guess
+
+		// note that these blending modes are different from those used in Map_DrawMap
+		p->tpage = p->tpage & 0xff9f | ((u_short)semitransparencyEnabled - 1) << 5;
 		p->code = p->code | 2;
 	}
 
+	// could also use the psn00bsdk macro, there is no difference
 	AddPrim(ot, p);
 
 	// POLY_FT4 is 0x28 bytes large
