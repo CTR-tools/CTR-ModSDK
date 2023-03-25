bd6278f1ae691a4ee178f207cb064a7d1450cab6
Agung Firdaus <agung.frds7@gmail.com>
2023-03-25 15:18:46 +0700

    [Unfinished, missing defs] UI_ThTick

---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/WorkInProgress/src/UI/UI_ThTick_CountPickup.c
@@ -0,0 +1,73 @@
+#include <common.h>
+
+void DECOMP_UI_ThTick_CountPickup(struct Thread * bucket) {
+
+  struct GameTracker * gGT;
+  short rotSpd;
+  u_int flags;
+  int mat;
+
+  gGT = sdata->gGT;
+
+  // object from thread
+  void * obj = bucket->object;
+
+  // instance from thread
+  struct Instance * inst = bucket->inst;
+
+  // instance color
+  inst->colorRGBA = 0xffff0000;
+
+  // if numPlyrCurrGame is 1
+  if (gGT->numPlyrCurrGame == 1) {
+    // if instance->model->modelID is not timebox
+    if (inst->model->id != 0x5c) {
+      // If player's wumpa is less than 10
+      if (gGT->drivers[0]->numWumpas < 10) {
+        // no shine
+        inst->alphaScale = 0;
+      } else {
+        // wumpaShineResult
+        inst->alphaScale = ((short) sdata->wumpaShineResult + -0x80) * 0x10;
+      }
+      goto LAB_8004c7a4;
+    }
+  }
+
+  // if numPlyrCurrGame is not 1
+  else {
+    LAB_8004c7a4:
+
+      // if HUD item is not timecrate
+      if (inst->model->id != 0x5c) {
+        // rotation speed 0x80
+        rotSpd = obj.rot[1] + 0x80;
+        goto LAB_8004c7d4;
+      }
+  }
+
+  // if wumpa or crystal,
+  // rotation speed 0x40
+  rotSpd = obj.rot[1] + 0x40;
+
+  LAB_8004c7d4:
+
+  obj.rot[1] = rotSpd;
+  mat = inst->matrix.m[0][0];
+
+  // convert 3 rotation shorts into rotation matrix
+  CovertRotToMatrix(mat, obj);
+  // then rotate
+  MatrixRotate(mat, obj.rot[4], mat);
+
+  // if hud is enabled, and this is not demo mode
+  if ((gGT->bool_DrawOTag_InProgress & 0xff0100) == 0x100) {
+    // make visible
+    flags = inst->flags & 0xffffff7f;
+  } else {
+    // make invisible
+    flags = inst->flags | 0x80;
+  }
+  inst->flags = flags;
+  return;
+}
---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/WorkInProgress/src/UI/UI_ThTick_Reward.c
@@ -0,0 +1,51 @@
+#include <common.h>
+
+// Draw various objects, like relic,
+// key trophy, token, crystal, etc
+void DECOMP_UI_ThTick_Reward(struct Thread * bucket)
+
+{
+  u_int flags;
+  void * obj;
+  MATRIX mat;
+  struct Instance * inst;
+
+  // Get instance
+  inst = bucket->inst;
+
+  // Get object
+  obj = bucket->object;
+
+  // Spin on the Y axis
+  obj.rot[1] += 0x40;
+
+  // instance, ptr rot[6], cop registers
+  Vector_SpecLightSpin2D(inst,obj,(obj + 0x28));
+
+  // pointer to matrix
+  mat = inst->matrix;
+
+   // convert 3 rotation shorts into rotation matrix
+  CovertRotToMatrix(mat,obj);
+
+  // MatrixRotate (param_1 = param_2 matrix rotated by param_3 matrix)
+  MatrixRotate(mat,obj.rot[4],mat);
+  
+  if (
+		((sdata->gGT->bool_DrawOTag_InProgress & 0xff0100) == 0x100) &&
+
+		// if any fade-in-from-black transition is over
+		(0xfff < sdata->gGT->tileView_UI.fadeFromBlack_currentValue)
+	 )
+  {
+	// make visible
+    flags = inst->flags & 0xffffff7f;
+  }
+  else
+  {
+	// make invisible
+    flags = inst->flags | 0x80;
+  }
+  inst->flags = flags;
+  return;
+}
