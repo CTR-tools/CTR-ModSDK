38c1e7c371a9d4de0a43f2230a9cb2b2ef1b0463
Niko <a>
2023-04-07 12:30:56 -0400

    4x UI funcs

---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/UI/UI_DrawNumCrystal.c
@@ -0,0 +1,18 @@
+#include <common.h>
+
+void UI_DrawNumCrystal(int posX, int posY, struct Driver* d)
+{
+	int numCrystalsOwned;
+	int numCrystalsTotal;
+	struct GameTracker* gGT;
+	char string[8];
+
+	DecalFont_DrawLine(&sdata->s_x[0], posX, posY+4, FONT_SMALL, ORANGE);
+	
+	gGT = sdata->gGT;
+	numCrystalsOwned = d->numCrystals;
+	numCrystalsTotal = gGT->numCrystalsInLEV;
+	
+	sprintf(&string[0], &rdata.s_lapString[0], numCrystalsOwned, numCrystalsTotal);
+	DecalFont_DrawLine(&string[0], posX+0xD, posY, FONT_BIG, ORANGE);
+}
