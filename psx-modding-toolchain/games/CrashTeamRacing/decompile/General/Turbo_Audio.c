6fffddfdfe7b277408adadd6819c4a6d4cbb8ce8
Niko <a>
2023-04-08 12:11:12 -0400

    progress

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/Turbo_Audio.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/Turbo_Audio.c
@@ -2,42 +2,58 @@
 
 void Voiceline_RequestPlay(int, short, int);
 
-void DECOMP_Turbo_Audio(struct Driver* driver, int speed_cap) {
+void DECOMP_Turbo_Audio(struct Driver* driver, int speed_cap) 
+{
     u_int distortion;
     u_int volume;
+	u_int extraFlags;
 
     // if turbo audio cooldown is not done
     if (driver->turbo_audioCooldown != 0) {
         return;
     }
 
-    // half volume
-    volume = 0x80 << 0x10;
-    // distort
-    distortion = 0x94 << 8;
-
-    if (speed_cap > 0x7f) {
+    if (speed_cap > 0x7f) 
+	{
         // max volume
-        volume = 0xff0000;
+        volume = 0xff << 0x10;
 
         // distort
         distortion = 0x6c << 8;
 
         // Make driver talk
         Voiceline_RequestPlay(0x10,data.characterIDs[driver->driverID],0x10);
-
-    } else if (speed_cap > 0x40) {
-        
+		
+		goto Skip;
+    }
+	
+	if (speed_cap > 0x40) 
+	{    
         // 3/4 volume
         volume = 0xc0 << 0x10;
+		
         // no distort
         distortion = 0x80 << 8;
+		
+		goto Skip;
     }
 
+    // half volume
+    volume = 0x80 << 0x10;
+	
+    // distort
+    distortion = 0x94 << 8;
+	
+Skip:
+
+	// 50% L/R
+	extraFlags = 0x80;
+
     // if echo is required
-    if ((driver->actionsFlagSet & 0x10000) != 0) {
+    if ((driver->actionsFlagSet & 0x10000) != 0) 
+	{
         // add echo
-        distortion |= 0x1000000;
+        extraFlags |= 0x1000000;
     }
 
     // 0xD = Turbo Boost Sound
