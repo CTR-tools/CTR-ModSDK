ef6741ac607c9d8a85f8f1daa146c449bfbb403c
Niko <a>
2023-04-10 14:00:00 -0400

    Faradise MM_MenuBox_NewLoad

---
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/230/230_MM_38_MenuBox_NewLoad.c
@@ -0,0 +1,24 @@
+#include <common.h>
+
+void DECOMP_MM_MenuBox_NewLoad(struct MenuBox* mb)
+{
+  short row;
+  
+  // row number
+  row = mb->rowSelected;
+  
+  if(row == -1)
+	  mb->ptrPrevMenuBox_InHierarchy->state &= 0xffffffeb;
+  
+  if((unsigned char)row > 1) return;
+  
+  // if Load was chosen
+  *(int*)0x800b59e0 = row;
+  
+  // MM_Title transitioning out
+  *(int*)0x800b5a1c = 2;
+  
+  mb->state |= 4;
+  return;
+}
+ 
