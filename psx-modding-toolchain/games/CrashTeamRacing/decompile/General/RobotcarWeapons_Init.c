0e7595b6d39a8d297a24d7a9395ccdea38c90811
Agung Firdaus <agung.frds7@gmail.com>
2023-04-11 20:19:24 +0700

    BossWeapon works

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/RobotcarWeapons_Init.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/RobotcarWeapons_Init.c
@@ -8,16 +8,16 @@ void DECOMP_RobotcarWeapons_Init(void)
   // get hubID of level
   hub = data.metaDataLEV[lev].hubID;
 
-  // If Level ID == 13
   // If Level ID is Oxide Station
   if (lev == 0xd) {
     hub = 0;
   }
 
-  if (hub != (unsigned int)-1)
+  if (hub > -1)
   {
 	// set pointer to boss weapon meta
-    *(unsigned int*)0x8008d8e8 = data.bossWeaponMetaPtr[hub];
+  // sdata->bossWeaponMeta = data.bossWeaponMetaPtr[hub]
+    *(unsigned int*)0x8008d8e8 = ((unsigned int*)0x800859d0)[hub];
   }
   return;
 }
