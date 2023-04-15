169e1b7b1413b7e4041d9ad8dbd217151405d1c3
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-04-15 14:47:55 -0400

    update test mod

--- psx-modding-toolchain/games/CrashTeamRacing/mods/Tutorials/test/src/hello.c
+++ psx-modding-toolchain/games/CrashTeamRacing/mods/Tutorials/test/src/hello.c
@@ -6,6 +6,14 @@ int Hello_Main2()
 {
 	if (!(sdata->gGT->gameMode1 & LOADING))
 	{
+		RECT r;
+		r.x = -10;
+		r.y = 28;
+		r.w = 64;
+		r.h = 64;
+
+		u_int color = 0xFFFFFF;
+
 		DecalHUD_DrawPolyGT4
 		(
 			sdata->gGT->ptrIcons[1],
@@ -18,5 +26,7 @@ int Hello_Main2()
 			data.ptrColor[GRAY][3],
 			SUBTRACT, 0x1000
 		);
+
+		CTR_Box_DrawSolidBox(&r, &color, sdata->gGT->tileView_UI.ptrOT, &sdata->gGT->backBuffer->primMem);
 	}
 }
