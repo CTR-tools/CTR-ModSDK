7d05173595a0a1e83ca7bfc5acfc2e85573b6d65
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-04-16 17:23:36 -0400

    so apparently DecalHUD_DrawPolyFT4 doesn't have that issue to begin with

--- psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DecalHUD/DecalHUD_DrawPolyFT4.c
+++ psx-modding-toolchain/games/CrashTeamRacing/decompile/General/DecalHUD/DecalHUD_DrawPolyFT4.c
@@ -1,7 +1,5 @@
 #include <common.h>
 
-#define EDUCATIONAL_BUG_IF 0
-
 void DECOMP_DecalHUD_DrawPolyFT4(struct Icon* icon, short posX, short posY, struct PrimMem* primMem, u_long* ot, char transparency, int scale)
 {
 	if (!icon) return;
@@ -14,29 +12,11 @@ void DECOMP_DecalHUD_DrawPolyFT4(struct Icon* icon, short posX, short posY, stru
 
 	unsigned int width = icon->texLayout.u1 - icon->texLayout.u0;
 	unsigned int height = icon->texLayout.v2 - icon->texLayout.v0;
+	unsigned int rightX = posX + (width * scale / 0x1000);
 	unsigned int bottomY = posY + (height * scale / 0x1000);
-	
-	#if EDUCATIONAL_BUG_IF
-		// The original compiled version of DecalHUD_DrawPolyFT4 has a bug where the Y coordinate of the top
-		// right vertex of the primitive (henceforth referred to as y1) can overflow by 1 if the X offset of
-		// the primitive (centered on the top left vertex of the primitive, henceforth referred to as x0) is
-		// set to a negative value. This is due to an optimization in the code: with x0 being a 32-bit value
-		// truncated to 16 bits, the value for the X coordinate of the top right vertex (henceforth referred to
-		// as x1), itself the offset of x0 by the width of the texture, is stored in a 32-bit variable; as the
-		// function sets the primitive's X and Y coordinate values by dereferencing both of them as a singular
-		// 32-bit integer, with x1 and y1's bits being added together using bitwise OR, the bottom 16 bits of
-		// x1 are added onto y1's bits, resulting in y1's value being altered by x1's overflow.
-		// This also affects the bottom right vertex.
 
-		unsigned int rightX = (posX & 0xFFFF) + (width * scale / 0x1000);
-		unsigned int rightXOverflow = rightX >> 16;
-		setXY4(p, posX, posY, rightX, posY | rightXOverflow, posX, bottomY, rightX, bottomY | rightXOverflow);
-	#else
-		unsigned int rightX = posX + (width * scale / 0x1000);
-		setXY4(p, posX, posY, rightX, posY, posX, bottomY, rightX, bottomY);
-	#endif
+	setXY4(p, posX, posY, rightX, posY, posX, bottomY, rightX, bottomY);
 	setIconUV(p, icon);
-
 	if (transparency)
 	{
 		setTransparency(p, transparency);
--- psx-modding-toolchain/games/CrashTeamRacing/mods/Tutorials/test/src/hello.c
+++ psx-modding-toolchain/games/CrashTeamRacing/mods/Tutorials/test/src/hello.c
@@ -1,6 +1,6 @@
 #include <common.h>
 
-void DECOMP_DecalHUD_DrawPolyGT4(struct Icon* icon, short posX, short posY, struct PrimMem* primMem, u_long* ot, u_int color0, u_int color1, u_int color2, u_int color3, char transparency, int scale);
+void DECOMP_DecalHUD_DrawPolyFT4(struct Icon* icon, short posX, short posY, struct PrimMem* primMem, u_long* ot, char transparency, int scale);
 
 int Hello_Main2()
 {
@@ -14,16 +14,12 @@ int Hello_Main2()
 
 		u_int color = 0xFFFFFF;
 
-		DecalHUD_DrawPolyGT4
+		DecalHUD_DrawPolyFT4
 		(
 			sdata->gGT->ptrIcons[1],
 			-10, 32,
 			&sdata->gGT->backBuffer->primMem,
 			sdata->gGT->tileView_UI.ptrOT,
-			data.ptrColor[COCO_MAGENTA][0],
-			data.ptrColor[GRAY][1],
-			data.ptrColor[GRAY][2],
-			data.ptrColor[GRAY][3],
 			SUBTRACT, 0x1000
 		);
 
