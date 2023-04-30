0758aded55419fd50c1488e368b4324528fcb59a
Niko <a>
2023-04-30 15:23:20 -0400

    bugfix for 3D sound

--- decompile/General/HOWL/Channel_SetVolume.c
+++ decompile/General/HOWL/Channel_SetVolume.c
@@ -9,8 +9,8 @@ void Channel_SetVolume(
 	
 	if(sdata->boolStereoEnabled == 1)
 	{
-		attr->audioL = (volume * data.volumeLR[0x00+LR]) >> 8;
-		attr->audioR = (volume * data.volumeLR[0xff-LR]) >> 8;
+		attr->audioL = (volume * data.volumeLR[0xFF-LR]) >> 8;
+		attr->audioR = (volume * data.volumeLR[0x00+LR]) >> 8;
 		return;
 	}
 	
