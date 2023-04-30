e4ffbe108444fc670f9a8f51b6e1af536e2e41fe
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2023-04-30 03:00:06 +0300

    feat(decomp): add LinkedCollide_Hitbox_Desc function (#49)
    * feat(decomp): add LinkedCollide_Hitbox_Desc function
    
    * fix/typo: inverted comparison operators

---
+++ decompile/General/LinkedCollide_Hitbox_Desc.c
@@ -0,0 +1,35 @@
+#include <common.h>
+
+struct Instance* LinkedCollide_Hitbox_Desc(struct HitboxDesc* objBoxDesc) {
+  struct Instance* thInst;
+  int diff_y;
+  MATRIX thInstMatrix;
+  SVECTOR thInstPos;
+  VECTOR outVec;
+  long flags[2];
+
+  // Loop over thBucket Linked List
+  for (struct Thread* thBucket = objBoxDesc->bucket; thBucket != 0; thBucket = thBucket->siblingThread){
+    thInst = thBucket->inst;
+
+    thInstPos.vx = thInst->matrix.t[0];
+    thInstPos.vy = thInst->matrix.t[1];
+    thInstPos.vz = thInst->matrix.t[2];
+
+    diff_y = thInst->matrix.t[1] - objBoxDesc->inst->matrix.t[1] ;
+
+    MATH_HitboxMatrix(&thInstMatrix, objBoxDesc->inst->matrix);
+
+    SetRotMatrix(&thInstMatrix);
+    SetTransMatrix(&thInstMatrix);
+
+    RotTrans(&thInstPos,&outVec, flags);
+
+    if ( (objBoxDesc->bbox.min[0] > outVec.vx) && (objBoxDesc->bbox.max[0] < outVec.vx) &&
+         (objBoxDesc->bbox.min[2] > outVec.vz) && (objBoxDesc->bbox.max[2] < outVec.vz) &&
+         (objBoxDesc->bbox.min[1] <= diff_y)   && (objBoxDesc->bbox.max[1] > diff_y) ) {
+        return thInst; //collision thread instance
+      }
+    }
+  return 0;
+}
