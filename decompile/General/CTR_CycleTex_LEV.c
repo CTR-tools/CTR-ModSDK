5ac0e6ce9691af107c24caa3e9b7375f24db7dd6
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-05-22 13:46:28 -0400

    update CTR_CycleTex_LEV and add to decomp buildList

--- decompile/General/CTR_CycleTex_LEV.c
+++ decompile/General/CTR_CycleTex_LEV.c
@@ -14,8 +14,8 @@ void DECOMP_CTR_CycleTex_LEV(struct AnimTex *animtex, int timer)
 		iVar3 = timer + psVar4->shrug >> ((int)psVar4->lottashortshuh & 0x1fU);
 		currentFrame = iVar3 % psVar4->numFrames;
 		psVar4->frameIndex = (u_short)currentFrame;
-		psVar4->ptrNext = *(struct AnimTex **)((int)psVar4->ptrarray + ((currentFrame << 0x10) >> 0xe));
-		psVar4 = (struct AnimTex *)(psVar4->ptrarray + psVar4->numFrames);
+		psVar4->ptrNext = psVar4->ptrarray[currentFrame];
+		psVar4 = &psVar4->ptrarray[psVar4->numFrames];
 		psVar1 = psVar4->ptrNext;
 	}
 	return;
