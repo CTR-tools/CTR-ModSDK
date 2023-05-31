fc474db0820243635ab8be40e97192d5f5aa12d8
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-05-30 20:42:48 -0400

    get rid of bat that only works for me

--- mods/Levels/WorkingLapTest/copy level to ctrviewer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-copy .\output\Lev221c.bin ..\..\..\..\..\..\ctrviewer-r15prev\levels\lap_test\test.lev
-copy .\src\dataVRAM.bin ..\..\..\..\..\..\ctrviewer-r15prev\levels\lap_test\test.vrm
-pause
