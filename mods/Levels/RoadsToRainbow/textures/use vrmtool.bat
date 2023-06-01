6158e0c74b7a779cc29e8959ecf20ba8e7bd8f82
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-05-31 21:41:26 -0400

    delete NikoLevelUSF data folder and all model_reader and vrmtool to all level texture folders
    and change bats to suit this stuff better

--- mods/Levels/RoadsToRainbow/textures/use vrmtool.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-"C:\CTR\ctr-tools-source\most recently compiled ctr-tools stuff\vrmtool.exe" .\data.vrm
-pause
--- mods/Levels/WorkingLapTest/textures/use model reader.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-"C:\CTR\ctr-tools-source\most recently compiled ctr-tools stuff\model_reader.exe" .\data.lev
-pause
