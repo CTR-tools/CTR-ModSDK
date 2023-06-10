bf64533edbb842b2cd04ac6f92776af30d878e13
Niko <a>
2023-06-10 18:09:00 -0400

    case changes

---
+++ decompile/General/230/230_36_MM_MENUBOX_Difficulty.c
@@ -0,0 +1,31 @@
+#include <common.h>
+
+void DECOMP_MM_MENUBOX_Difficulty(struct MenuBox* mb)
+{
+  short row;
+  
+  row = mb->rowSelected;
+  
+  // if uninitialized
+  if (row == -1) {
+    mb->ptrPrevBox_InHierarchy->state &= 0xffffffeb;
+  }
+  
+  else 
+  {
+	// if you are on a valid row
+    if ((unsigned char)row < 3) 
+	{
+	  // set difficulty to value, from array of fixed difficulty values
+      sdata->gGT->arcadeDifficulty = OVR_230.cupDifficultySpeed[row];
+      
+	  *(int*)0x800b5a1c = 2;
+	  *(int*)0x800b59e0 = 2;
+      
+	  mb->state |= 4;
+      return;
+    }
+  }
+  return;
+}
+ 
