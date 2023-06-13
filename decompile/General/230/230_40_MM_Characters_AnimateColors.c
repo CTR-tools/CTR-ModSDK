5c8aa4d18cd6890a8ad6fa655705ed894ecce7c9
Nonunknown <nonunknown777@gmail.com>
2023-06-13 18:01:18 -0300

    Give Meaningful names for animateColors

--- decompile/General/230/230_40_MM_Characters_AnimateColors.c
+++ decompile/General/230/230_40_MM_Characters_AnimateColors.c
@@ -2,40 +2,40 @@
 
 void DECOMP_MM_Characters_AnimateColors(unsigned char *colorData, short playerID, short flag)
 {
-  unsigned char bVar1;
-  unsigned int uVar3;
-  unsigned int iVar4;
+  u_char colorAdjustmentValue;
+  u_int trigApproximationIndex;
+  u_int trigApprox;
   
   // access int RGBA as a char array,
   // for editing components of color
-  unsigned char* ptrColor;
-  ptrColor = (unsigned char*)data.ptrColor[playerID + PLAYER_BLUE];
+  u_char* ptrColor;
+  ptrColor = (u_char*)data.ptrColor[playerID + PLAYER_BLUE];
 
-  iVar4 = 0;
+  trigApprox = 0;
 
   if (flag == 0) {
-    uVar3 = sdata->frameCounter * 0x100 + playerID * 0x400;
+    trigApproximationIndex = sdata->frameCounter * 0x100 + playerID * 0x400;
 
     // approximate trigonometry
-    iVar4 = data.trigApprox[uVar3 & 0x3ff];
+    trigApprox = data.trigApprox[trigApproximationIndex & 0x3ff];
 
-    if ((uVar3 & 0x400) == 0) {
-      iVar4 = iVar4 << 0x10;
+    if ((trigApproximationIndex & 0x400) == 0) {
+      trigApprox = trigApprox << 0x10;
     }
-    iVar4 = iVar4 >> 0x10;
+    trigApprox = trigApprox >> 0x10;
 
-    if ((uVar3 & 0x800) != 0)
-      iVar4 = -(int)iVar4;
+    if ((trigApproximationIndex & 0x800) != 0)
+      trigApprox = -(int)trigApprox;
   }
 
-  bVar1 = 0;
-  if (0xc00 < (int)iVar4) {
-    bVar1 = ((iVar4 << 7) >> 0xc);
+  colorAdjustmentValue = 0;
+  if (0xc00 < (int)trigApprox) {
+    colorAdjustmentValue = ((trigApprox << 7) >> 0xc);
   }
   
-  colorData[0] = ptrColor[0] | bVar1;
-  colorData[1] = ptrColor[1] | bVar1;
-  colorData[2] = ptrColor[2] | bVar1;
+  colorData[0] = ptrColor[0] | colorAdjustmentValue;
+  colorData[1] = ptrColor[1] | colorAdjustmentValue;
+  colorData[2] = ptrColor[2] | colorAdjustmentValue;
   colorData[3] = 0;
   
   return;
