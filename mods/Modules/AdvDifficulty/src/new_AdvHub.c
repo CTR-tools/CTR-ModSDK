6ed4ee8e7475c3773c99fac29fa625857af00139
Agung Firdaus <agung.frds7@gmail.com>
2023-06-16 23:25:29 +0700

    AdvDifficulty Update

---
+++ mods/Modules/AdvDifficulty/src/new_AdvHub.c
@@ -0,0 +1,5 @@
+#include <common.h>
+
+extern struct MenuRow new_advHub;
+
+struct MenuRow* hub = &new_advHub;
---
+++ mods/Modules/AdvDifficulty/src/new_AdvRace.c
@@ -0,0 +1,5 @@
+#include <common.h>
+
+extern struct MenuRow new_advRace;
+
+struct MenuRow* adv = &new_advRace;
