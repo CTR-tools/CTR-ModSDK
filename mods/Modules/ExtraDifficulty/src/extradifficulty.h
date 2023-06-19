2508114c1df19c924b641275ae4010dbbfcda9be
Nonunknown <nonunknown777@gmail.com>
2023-06-19 15:29:51 -0300

    Boss Battle Mode - WIP

--- mods/Modules/ExtraDifficulty/src/extradifficulty.h
+++ mods/Modules/ExtraDifficulty/src/extradifficulty.h
@@ -1,20 +1,5 @@
-#define MENU_ROW(NAME, STRING_INDEX, UP, DOWN, LEFT, RIGHT) \
-    [NAME] = { \
-        .stringIndex = STRING_INDEX, \
-        .rowOnPressUp = UP, \
-        .rowOnPressDown = DOWN, \
-        .rowOnPressLeft = LEFT, \
-        .rowOnPressRight = RIGHT \
-    }
+#include "..\..\..\mods\include\helper_menu_box.h"
 
-#define FINALIZER_ROW \
-    { \
-        .stringIndex = -1, \
-        .rowOnPressUp = 0, \
-        .rowOnPressDown = 0, \
-        .rowOnPressLeft = 0, \
-        .rowOnPressRight = 0 \
-    }
 
 #define ROW_EASY 0
 #define ROW_MEDIUM 1
