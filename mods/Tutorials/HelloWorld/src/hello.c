0e9dda02611ac9b8eef2fb970bd952819e5a1983
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-06-28 12:00:18 -0400

    HelloWorld: updated for more clarity

--- mods/Tutorials/HelloWorld/src/hello.c
+++ mods/Tutorials/HelloWorld/src/hello.c
@@ -1,36 +1,43 @@
 #include <common.h>
 
-int Hello_Main()
+/*
+BUILD is a variable assigned during compilation time.
+It takes the value build_id defined in config.json
+You can also see what these aliases for BUILD equal to
+in include/gccHeaders.h
+*/
+char RegionString[] =
+#if BUILD == UsaRetail
+	"NTSC-U";
+#elif BUILD == EurRetail
+	"PAL";
+#elif BUILD == JpnRetail
+	"NTSC-J";
+#elif BUILD == SepReview
+	"September 3 Prototype";
+#elif BUILD == JpnTrial
+	"Japan Store Trial";
+#else
+	"Unknown build";
+#endif
+
+// The PAL version has a bigger screen display, so the Y position needs to be lower (by making its value higher)
+int height =
+#if BUILD == EurRetail
+	220;
+#else
+	200;
+#endif
+
+void Hello_Main()
 {
 	if (!(sdata->gGT->gameMode1 & LOADING))
 	{
 		/*
-			BUILD is a variable assigned during compilation time.
-			It takes the value build_id defined in config.json
-			You can also see what these aliases for BUILD equal to
-			in include/gccHeaders.h
-			For the last two parameters in DecalFont_DrawLine, also
+			For the last two parameters in DecalFont_DrawLine,
 			check include/namespace_Decal.h
 		*/
-		#if BUILD == UsaRetail
-			DecalFont_DrawLine("NTSC-U", 10, 200, FONT_SMALL, ORANGE);
-		#elif BUILD == EurRetail
-			DecalFont_DrawLine("PAL", 10, 220, FONT_SMALL, ORANGE);
-		#elif BUILD == JpnRetail
-			DecalFont_DrawLine("NTSC-J", 10, 200, FONT_SMALL, ORANGE);
-		#elif BUILD == SepReview
-			DecalFont_DrawLine("September 3 Prototype", 10, 200, FONT_SMALL, ORANGE);
-		#elif BUILD == JpnTrial
-			DecalFont_DrawLine("Japan Store Trial", 10, 200, FONT_SMALL, ORANGE);
-		#else
-			DecalFont_DrawLine("Unknown build", 10, 200, FONT_SMALL, ORANGE);
-		#endif
-		
-		#if BUILD == EurRetail
-		// The PAL version has a bigger screen display, so the Y position needs to be lower (by making its value higher)
-			DecalFont_DrawLine("Hello World!", 10, 210, FONT_SMALL, ORANGE);
-		#else
-			DecalFont_DrawLine("Hello World!", 10, 190, FONT_SMALL, ORANGE);
-		#endif
+		DecalFont_DrawLine(RegionString, 10, height, FONT_SMALL, ORANGE);
+		DecalFont_DrawLine("Hello Worm!", 10, height - 10, FONT_SMALL, ORANGE);
 	}
 }
