88234a6844c07fee408c1c3e9b1cc017fd6f4b2b
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-06-30 15:49:24 -0400

    Update math.h by Redhot

--- include/math.h
+++ include/math.h
@@ -2,7 +2,23 @@
 
 #define CTR_MATH_H
 
-// fixed point macros
+// trigonometry //
+
+#define ANG_TWO_PI 0x1000        // 360
+#define ANG_PI (ANG_TWO_PI / 2)  // 180
+#define ANG_HALF_PI (ANG_PI / 2) // 90
+
+#define ANG_MODULO_TWO_PI(x) (x & (ANG_TWO_PI - 1))   // ang % 360
+#define ANG_MODULO_PI(x) (x & (ANG_PI - 1))           // ang % 180
+#define ANG_MODULO_HALF_PI(x) (x & (ANG_HALF_PI - 1)) // ang % 90
+
+#define ANG(x) ANG_MODULO_TWO_PI(((short)((((float) x) * ANG_TWO_PI) / 360))) // works for any float, pos or neg
+
+#define IS_ANG_FIRST_OR_THIRD_QUADRANT(x) ((x & ANG_HALF_PI) == 0) // [0, 90[ \/ [180, 270[
+#define IS_ANG_THIRD_OR_FOURTH_QUADRANT(x) (x & ANG_PI)            // [180, 360[
+
+// fixed point //
+
 #define FRACTIONAL_BITS 12
 #define FP_ONE (1 << FRACTIONAL_BITS)
 #define FP(x) ((short)(((float)x) * FP_ONE))
@@ -16,6 +32,8 @@ force_inline short FP_Mult(short x, short y)
 	int result = x;
 	return (result * y) >> FRACTIONAL_BITS;
 }
+
+// misc //
 
 force_inline int abs(int value)
 {
