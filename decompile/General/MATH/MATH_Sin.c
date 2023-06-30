819c420a94e255b9426af4ffce48db5b9d323086
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-06-30 15:55:24 -0400

    Update MATH_Sin.c by Redhot

--- decompile/General/MATH/MATH_Sin.c
+++ decompile/General/MATH/MATH_Sin.c
@@ -2,14 +2,14 @@
 
 int DECOMP_MATH_Sin(unsigned int angle)
 {
-    int sine = data.trigApprox[angle & 0x3FF];
+    int sine;
+    
+    if (IS_ANG_FIRST_OR_THIRD_QUADRANT(angle))
+        sine = data.trigApprox[ANG_MODULO_HALF_PI(angle)].sin;
+    else
+        sine = data.trigApprox[ANG_MODULO_HALF_PI(angle)].cos;
 
-    if ((angle & 0x400) == 0)
-        sine = sine << 0x10;
-
-    sine = sine >> 0x10;
-
-    if ((angle & 0x800) != 0)
+    if (IS_ANG_THIRD_OR_FOURTH_QUADRANT(angle))
         sine = -sine;
 
     return sine;
