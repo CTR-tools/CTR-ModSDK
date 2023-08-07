bc9b2c322145d41ceb9a0320c8a1d3b940b23f3a
Niko <a>
2023-08-06 21:44:26 -0400

    progress

--- decompile/WorkInProgress/src/GHOST/GhostBuffer_RecordBoosts.c
+++ decompile/WorkInProgress/src/GHOST/GhostBuffer_RecordBoosts.c
@@ -5,7 +5,7 @@
 // param3 - speed cap
 void GhostBuffer_RecordBoosts(int addReserve,char type,int speedCap)
 {
-  void *puVar1;
+  char *puVar1;
 
   puVar1 = sdata->GhostRecording.ptrCurrOffset;
 
@@ -22,28 +22,21 @@ void GhostBuffer_RecordBoosts(int addReserve,char type,int speedCap)
 	// 0x82-style chunks are 6 bytes long (including 0x82)
 
 	// Write to recording buffer
-    *sdata->GhostRecording.ptrCurrOffset = 0x82;
+    puVar1[0] = 0x82;
 
-	// increment recording offset by 1 byte
-    sdata->GhostRecording.ptrCurrOffset = (int)sdata->GhostRecording.ptrCurrOffset + 1;
-
-	// This holds information about turbo pads:
-	//	- Regular Turbo vs Super Turbo
-	//  - Amount of boost gained from turbo
-
-	// short, reserve count
-    *sdata->GhostRecording.ptrCurrOffset = (char)((u_int)addReserve >> 8);
+	// big endian reserve
+    puVar1[1] = (char)((u_int)addReserve >> 8);
     puVar1[2] = (char)addReserve;
 
 	// char, add type (increment or set)
     puVar1[3] = type;
 
-	// short, speed cap
+	// big endian speedCcap
     puVar1[4] = (char)((u_int)speedCap >> 8);
     puVar1[5] = (char)speedCap;
 
 	// increment recording offset by 5 bytes
-	sdata->GhostRecording.ptrCurrOffset = (int)sdata->GhostRecording.ptrCurrOffset + 5;
+	sdata->GhostRecording.ptrCurrOffset = (int)sdata->GhostRecording.ptrCurrOffset + 6;
   }
   return;
 }
