ca4d2d5ce564b980ce32324690b702fa4d8144a4
Niko <a>
2023-08-10 22:12:18 -0400

    GARAGE_HOWL progress

--- decompile/General/HOWL/Garage_LerpFX.c
+++ decompile/General/HOWL/Garage_LerpFX.c
@@ -2,11 +2,12 @@
 
 void DECOMP_Garage_LerpFX(void)
 {
-  char cVar1;
-  short sVar3;
+  int cVar1;
   struct GarageFX* garageSounds;
   short sVar5;
   short sVar6;
+  short volume;
+  short LR;
   int i;
 
   for (i = 0; i < 8; i++) 
@@ -39,60 +40,54 @@ void DECOMP_Garage_LerpFX(void)
       sVar5 = garageSounds->LR;
     }
 
+	LR = garageSounds->LR;
+	volume = garageSounds->volume;
+
 	// if change is desired, lerp properties
-    if ((sVar5 != garageSounds->LR) || (sVar6 != garageSounds->volume)) 
+    if ((sVar5 != LR) || (sVar6 != volume)) 
 	{
 	  // desired audio change
-      if (sVar6 != garageSounds->volume) 
+      if (sVar6 != volume) 
 	  {
 		// lerp up
-        if (garageSounds->volume < sVar6) 
+        if (volume < sVar6) 
 		{
-		  sVar3 = garageSounds->volume + 8;
-          garageSounds->volume = sVar3;
-          cVar1 = sVar6 < sVar3;
+		  volume += 8;
+          if(volume > sVar6)
+			  volume = sVar6;
         }
         
 		// lerp down
 		else 
 		{
-          sVar3 = garageSounds->volume + -8;
-          garageSounds->volume = sVar3;
-          cVar1 = sVar3 < sVar6;
-        }
-        
-		// lock on
-		if (cVar1) 
-		{
-          garageSounds->volume = sVar6;
+          volume -= 8;
+          if(volume < sVar6)
+			  volume = sVar6;
         }
 		
+		garageSounds->volume = volume;
       }
       
 	  // desired LR change
-	  if (sVar5 != garageSounds->LR) 
+	  if (sVar5 != LR) 
 	  {
 		// lerp up
-        if (garageSounds->LR < sVar5) 
+        if (LR < sVar5) 
 		{
-		  sVar3 = garageSounds->LR + 2;
-          garageSounds->LR = sVar3;
-          cVar1 = (sVar5 < sVar3);
+		  LR += 2;
+          if(LR > sVar5)
+			  LR = sVar5;
         }
 		
 		// lerp down
         else 
 		{
-          sVar3 = garageSounds->LR + -2;
-          garageSounds->LR = sVar3;
-          cVar1 = (sVar3 < sVar5);
+          LR -= 2;
+          if(LR < sVar5)
+			  LR = sVar5;
         }
-        
-		// lock on
-		if (cVar1) 
-		{
-          garageSounds->LR = sVar5;
-        }
+		
+		garageSounds->LR = LR;
       }
 	  
       if (sdata->garageSoundIDs[i] != 0) 
@@ -100,12 +95,12 @@ void DECOMP_Garage_LerpFX(void)
         OtherFX_RecycleNew(
             garageSounds->audioPtr,
             sdata->garageSoundIDs[i],
-            (int)garageSounds->volume << 0x10 | (int)garageSounds->LR | 0x8000U
+            (int)volume << 0x10 | (int)LR | 0x8000U
             );
       }
 	  
 	  // if desired properties have been reached
-      if (((sVar5 == garageSounds->LR) && (sVar6 == garageSounds->volume)) &&
+      if (((sVar5 == LR) && (sVar6 == volume)) &&
         (
             garageSounds->gsp_prev = garageSounds->gsp_curr,
             garageSounds->gsp_curr == GSP_GONE
