61f61ec696617f1916cc39fddfc5e8e12efc6f06
Niko <a>
2023-09-08 11:47:34 -0400

    progress

--- decompile/WorkInProgress/src/GhostReplayThTick.c
+++ decompile/WorkInProgress/src/GhostReplayThTick.c
@@ -85,6 +85,8 @@ void GhostReplay_ThTick(struct Thread *t) {
     opcodePos = 0;
     packetPtr = tape->ptrCurr;
     short tmpPos[3] = {0};
+	
+	char* lastByteAfterPrevPacket = tape->ptrCurr;
 
     tape->packetID = -1;
     tape->timeInPacket01 = tape->timeInPacket32_backup;
@@ -146,8 +148,10 @@ void GhostReplay_ThTick(struct Thread *t) {
           // count position opcodes
           opcodePos++;
 
-          packet->bufferPacket = packetPtr;
+          packet->bufferPacket = lastByteAfterPrevPacket;
           packetPtr += 11;
+		  lastByteAfterPrevPacket = packetPtr;
+		
           packet++;
 
           break;
@@ -173,8 +177,10 @@ void GhostReplay_ThTick(struct Thread *t) {
           packet[0].rot[0] = packet[-1].rot[0];
           packet[0].rot[1] = packet[-1].rot[1];
 
-          packet->bufferPacket = packetPtr;
+		  packet->bufferPacket = lastByteAfterPrevPacket;
           packetPtr += 1;
+		  lastByteAfterPrevPacket = packetPtr;
+		
           packet++;
           break;
         }
@@ -193,8 +199,10 @@ void GhostReplay_ThTick(struct Thread *t) {
         packet->rot[1] = packetPtr[3] << 4;
         packet->rot[0] = packetPtr[4] << 4;
 
-        packet->bufferPacket = packetPtr;
+        packet->bufferPacket = lastByteAfterPrevPacket;
         packetPtr += 5;
+		lastByteAfterPrevPacket = packetPtr;
+		
         packet++;
       }
     }
@@ -284,8 +292,11 @@ void GhostReplay_ThTick(struct Thread *t) {
 
   unsigned char* buffer = tape->packets[packetIdx].bufferPacket;
 
-  //printf("%d %d\n", tape->packetID, packetIdx);
-  while (tape->packetID < packetIdx) {
+  while (tape->packetID < packetIdx) 
+  {
+	printf("%d %d, %08x\n", tape->packetID, packetIdx, buffer);
+	
+	  
     if (tape->ptrEnd <= buffer)
       break;
 
