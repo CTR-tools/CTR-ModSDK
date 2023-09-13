065378beead58a43d1923f1eb943c510a2c8a286
Niko <a>
2023-09-12 20:39:06 -0400

    bugfix

--- decompile/WorkInProgress/src/MainFreeze_IfPressStart.c
+++ decompile/WorkInProgress/src/MainFreeze_IfPressStart.c
@@ -2,28 +2,24 @@
 
 void DECOMP_MainFreeze_IfPressStart(void)
 {
-    struct GameTracker *gGT = sdata->gGT;
-    u_int gameMode = gGT->gameMode1;
+    struct GameTracker* gGT = sdata->gGT;
 
     // Check conditions for pausing the game
     if (
-        // if flag is not fully on screen
         (TitleFlag_IsFullyOnScreen() == 0) &&
 
         // if you are not drawing loading screen (after fully off screen)
         ((gGT->renderFlags & 0x1000) == 0) &&
 
-        // if aku is not giving a hint
         (sdata->AkuAkuHintState == 0) &&
-
-        // if not in a menu, time trial, and not paused yet
-        (sdata->ptrActiveMenuBox == 0 && (gGT->gameMode1 & (END_OF_RACE | PAUSE_ALL)) == 0) &&
-
-        // not in Main Menu and not in Demo Mode
-        (gGT->levelID != MAIN_MENU_LEVEL && ((gGT->gameMode1 & GAME_CUTSCENE) == 0 && gGT->boolDemoMode == 0)) &&
-
-        // if game is not loading and not in a cutscene where you can't move
-        ((1 < (gGT->levelID - OXIDE_ENDING) && (sdata->load_inProgress == 0 && (!(gGT->gameMode2 & 4)))) == 0))
+        (sdata->ptrActiveMenuBox == 0) &&
+		((gGT->gameMode1 & (END_OF_RACE | PAUSE_ALL | GAME_CUTSCENE)) == 0) &&
+        (gGT->levelID != MAIN_MENU_LEVEL) &&
+		(gGT->boolDemoMode == 0) &&
+		((gGT->levelID - OXIDE_ENDING) > 1) &&
+		(sdata->load_inProgress == 0) &&
+		((gGT->gameMode2 & 4) == 0)
+	   )
     {
         // pause the game
         gGT->gameMode1 |= PAUSE_1;
