ff3caabd03d6d697a9094eb589c1082677b05392
Niko <a>
2023-09-13 14:23:58 -0400

    progress

--- decompile/General/231/231_018_RB_TNT_ThTick_ThrowOffHead.c
+++ decompile/General/231/231_018_RB_TNT_ThTick_ThrowOffHead.c
@@ -16,31 +16,30 @@ void DECOMP_RB_TNT_ThTick_ThrowOffHead(struct Thread* t)
 
   if (mw->stopFallAtY == 0x3fff) 
     mw->stopFallAtY = mw->driverTarget->instSelf->matrix.t[1];
-
+  
+  if (inst->matrix.t[1] <= mw->stopFallAtY)
+  {	  
+    // plays tnt explosion sound 3D
+    PlaySound3D(0x3d,inst);
+    
+    RB_Blowup_Init(inst);
+    
+    // Set scale (x, y, z) to zero
+    inst->scale[0] = 0;
+    inst->scale[1] = 0;
+    inst->scale[2] = 0;
+    
+    // make invisible
+    inst->flags |= 0x80;
+    
+    // this thread is now dead
+    t->flags |= 0x800;
+    
+    mw->driverTarget->instTntRecv = 0;
+  }
+  
   // decrease velocity (artificial gravity)
   mw->velocity[1] -= ((gGT->elapsedTimeMS << 2) >> 5);
   if (mw->velocity[1] < -0x60) mw->velocity[1] = -0x60;
-  
-  if (inst->matrix.t[1] > mw->stopFallAtY) return; 
-  
-  // plays tnt explosion sound 3D
-  PlaySound3D(0x3d,inst);
-  
-  RB_Blowup_Init(inst);
-  
-  // Set scale (x, y, z) to zero
-  inst->scale[0] = 0;
-  inst->scale[1] = 0;
-  inst->scale[2] = 0;
-  
-  // make invisible
-  inst->flags |= 0x80;
-  
-  // this thread is now dead
-  t->flags |= 0x800;
-  
-  mw->driverTarget->instTntRecv = 0;
-  
-  return;
 }
  
