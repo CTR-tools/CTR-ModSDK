677f8fb1e3c997ce0ac5279990e35878e53e2d59
Niko <a>
2023-09-17 20:27:08 -0400

    progress

---
+++ decompile/General/LOAD/LOAD_A16_ReadDirectory.c
@@ -0,0 +1,32 @@
+#include <common.h>
+
+void* DECOMP_LOAD_ReadDirectory(char* filename)
+{
+	CdlFILE cdlFile;
+	char buf[8];
+	
+	CDSYS_SetMode_StreamData();
+	
+	if(CdSearchFile(&cdlFile, filename) == 0) return 0;
+	
+	struct BigHeader* bh = MEMPACK_AllocMem(0x4000/*, filename*/);
+	
+	CdControl(CdlSetloc, &cdlFile, buf);
+	
+	// read bigfile header
+	if(CdRead(8, bh, 0x80) == 0) return 0;
+	
+	// wait for read to finish
+	if(CdReadSync(0,0) != 0) return 0;
+
+	bh->cdpos = CdPosToInt(&cdlFile);
+	
+	// undo allocation of 0x4000, only use "needed" size
+	MEMPACK_ReallocMem(
+		offsetof(struct BigHeader, entry) + 
+		sizeof(struct BigEntry) * bh->numEntry
+	);
+	
+	sdata->ptrBigfileCdPos_2 = bh;
+	return bh;
+}
