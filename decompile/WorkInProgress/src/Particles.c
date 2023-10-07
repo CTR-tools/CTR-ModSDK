2a194780c1222a186c0c3bf85fd82fbd8cece23a
Agung Firdaus <agung.frds7@gmail.com>
2023-10-07 08:12:12 +0700

    Particle Progress

---
+++ decompile/WorkInProgress/src/Particles.c
@@ -0,0 +1,157 @@
+#include <common.h>
+
+void Particle_FuncPtr_PotionShatter(struct Particle *p)
+{
+  short sVar2;
+  int rng;
+
+  if (p->axis[1].velocity < 0x578)
+  {
+    if (p->axis[0].velocity != 0)
+      goto LAB_8003ebc8;
+
+    // Get random number
+    rng = MixRNG_Scramble();
+
+    p->axis[0].velocity = rng + (rng / 800) * -800 + -400;
+
+    // Get random number
+    rng = MixRNG_Scramble();
+
+    p->axis[2].velocity = rng + (rng / 800) * -800 + -400;
+
+    // Get random number
+    rng = MixRNG_Scramble();
+
+    sVar2 = (rng >> 8);
+    if (rng < 0)
+    {
+      sVar2 = ((rng + 0xff) >> 8);
+    }
+    p->axis[5].velocity = rng + sVar2 * -0x100 + 0x100;
+  }
+  if (p->axis[0].velocity == 0) return;
+
+LAB_8003ebc8:
+
+  if (*(int*)(p + 0x20) == 0x45) // ???
+  {
+    if (0 < p->axis[8].startVal)
+    {
+      p->axis[8].startVal = p->axis[8].startVal + -0x1200;
+    }
+  }
+  else
+  {
+    if (0 < p->axis[7].startVal)
+    {
+      p->axis[7].startVal = p->axis[7].startVal + -0x1200;
+    }
+  }
+}
+
+void Particle_FuncPtr_SpitTire(struct Particle *p)
+{
+  int rng;
+  int iVar2;
+
+  if (p->axis[1].startVal >> 8 < *(int *)(p->driverInst->matrix.t[1]) + 0x10)
+  {
+    // Get random number
+    rng = MixRNG_Scramble();
+
+    p->axis[0].velocity = rng + (rng / 0x1640) * -0x1640 + -0xb20;
+
+    // Get random number
+    rng = MixRNG_Scramble();
+
+    iVar2 = p->axis[5].startVal;
+
+    p->axis[2].velocity = rng + (rng / 0x1640) * -0x1640 + -0xb20;
+    if (iVar2 == 0x1000)
+    {
+      // Get random number
+      rng = MixRNG_Scramble();
+
+      p->axis[1].velocity = rng + (rng / 0x12c0) * -0x12c0 + 0x1900;
+      iVar2 = p->driverInst->matrix.t[1];
+      p->axis[5].startVal = 0xfff;
+    }
+    else
+    {
+      if (iVar2 == 0xfff)
+      {
+        rng = MixRNG_Scramble();
+        p->axis[1].velocity = rng + (rng / 800) * -800 + 8000;
+        iVar2 = p->driverInst->matrix.t[1];
+        p->axis[5].startVal = 0xffe;
+      }
+      else
+      {
+        if (iVar2 != 0xffe)
+        {
+          return;
+        }
+
+        // Get random number
+        rng = MixRNG_Scramble();
+
+        p->axis[1].velocity = rng + (rng / 800) * -800 + 6000;
+        rng = p->driverInst->matrix.t[1];
+        p->axis[5].velocity = 0xf801;
+      }
+    }
+    p->axis[1].startVal = (iVar2 + 0x10) * 0x100;
+  }
+}
+
+void Particle_FuncPtr_ExhaustUnderwater(struct Particle *p)
+{
+  struct IconGroup *icon;
+
+  if ((3 < (p->axis[1].startVal >> 8) + p->driverInst->matrix.t[1]) &&
+      (p->framesLeftInLife < 27))
+  {
+    // bubblepop
+    icon = sdata->gGT->iconGroup[8];
+
+    if (icon != NULL)
+    {
+      // save icons
+      p->ptrIconGroup = icon;
+      p->ptrIconArray = icon->icons;
+    }
+
+    p->axis[4].startVal = MixRNG_Scramble() & 0xfff;
+    p->framesLeftInLife = 0;
+  }
+}
+
+void Particle_OnDestroy(struct Particle* p)
+{
+  struct Particle *pp;
+
+  pp = p->prev;
+
+  while (pp != NULL)
+  {
+    pp = pp->next;
+	// free list of Oscillator Pool
+    LIST_AddFront(sdata->gGT->JitPools.oscillator.free);
+  }
+}
+
+void Particle_UpdateAllParticles(void)
+{
+  struct GameTracker* gGT = sdata->gGT;
+  // if PauseAllThreads is diabled
+  if ((gGT->gameMode1 & PAUSE_THREADS) == 0)
+  {
+	// world-space particles (exhaust on cars)
+  Particle_UpdateList(gGT->particleList_ordinary,gGT->particleList_ordinary);
+
+	// Draw "heat" particles that warp the screen
+	// (above fire in tiger temple flamejet and player missiles)
+	Particle_UpdateList(gGT->particleList_heatWarp,gGT->particleList_heatWarp);
+  }
+}
