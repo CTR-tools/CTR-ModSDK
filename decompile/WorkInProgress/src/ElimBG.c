c360b708e8fef5da976e3f386f26401d739b31c1
Agung Firdaus <agung.frds7@gmail.com>
2023-10-08 10:58:39 +0700

    ElimBG Progress(1)

--- decompile/WorkInProgress/src/ElimBG.c
+++ decompile/WorkInProgress/src/ElimBG.c
@@ -134,32 +134,37 @@ void DECOMP_ElimBG_ToggleInstance(struct Instance* inst, char boolGameIsPaused)
 		inst->flags &= ~(INVISIBLE_DURING_PAUSE | HIDE_MODEL);
 }
 
-void DECOMP_ElimBG_ToggleAllInstances(struct Instance* i, char boolGameIsPaused)
+void DECOMP_ElimBG_ToggleAllInstances(struct GameTracker* gGT, char boolGameIsPaused)
 {
-	int iVar1;
-	struct Instance* inst;
-	u_int uVar2;
-	u_int uVar3;
-	
-	iVar1 = *(int *)((int)&i->idpp[1].lodIndex + 2);
-	uVar2 = *(u_int *)(iVar1 + 0x10);
-	uVar3 = uVar2 + *(int *)(iVar1 + 0xc) * 0x40;
-	for (; uVar2 < uVar3; uVar2 += 0x40)
-	{
-		if (*(struct Instance **)(uVar2 + 0x2c) != 0)
-		{
-			DECOMP_ElimBG_ToggleInstance(*(struct Instance **)(uVar2 + 0x2c), boolGameIsPaused);
-		}
-	}
-	for
-	(
-		inst = (struct Instance *)LIST_GetFirstItem((struct LinkedList *)&i[9].idpp[2].unkD4);
-		inst != NULL;
-		inst = (struct Instance *)LIST_GetNextItem((struct Item *)inst)
-	)
-	{
-		DECOMP_ElimBG_ToggleInstance(inst, boolGameIsPaused);
-	}
+  struct Item* item;
+  struct InstDef* instdef;
+  u_int last;
+
+  // first lev instance
+    instdef = gGT->level1->ptrInstDefs;
+
+  // last lev instance (first + count * 0x40)
+    last = instdef + (gGT->level1->numInstances * 0x40);
+
+  // loop through all LEV instances
+    while((u_int)instdef < last) 
+    {
+      if (instdef->ptrInstance != NULL) {
+        DECOMP_ElimBG_ToggleInstance(instdef->ptrInstance, boolGameIsPaused);
+      }
+      instdef += 0x40;
+    }
+
+  // first taken item in InstancePool
+  item = LIST_GetFirstItem(gGT->JitPools.instance.taken.first);
+
+  // loop through everything in InstancePool,
+  // Driver, Weapons, everything non-lev
+  while (item != NULL)
+  {
+    DECOMP_ElimBG_ToggleInstance(item, boolGameIsPaused);
+	  item = LIST_GetNextItem(item);
+  }
 }
 
 void DECOMP_ElimBG_HandleState(struct GameTracker *gGT)
