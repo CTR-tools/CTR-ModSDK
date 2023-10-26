f5d90255bbf5859c9b524b5cb4aa0ac4a4bcb6e3
Agung Firdaus <agung.frds7@gmail.com>
2023-10-26 15:24:18 +0700

    MainFrame_VisMemFullFrame

--- decompile/WorkInProgress/src/[4. Doesn't compile] MainFrame_VisMemFullFrame.c
+++ decompile/WorkInProgress/src/[4. Doesn't compile] MainFrame_VisMemFullFrame.c
@@ -2,15 +2,13 @@
 
 void DECOMP_MainFrame_VisMemFullFrame(struct GameTracker* gGT, struct Level* level)
 {
-	void* ptr_add_tex;
-	void* unk20;
-	void* iVar3;
-	u_int uVar4;
-	void* uVar5;
-	struct cameraDC* cameraDC;
+	int visSrc;
+	int dest;
 	int iVar7;
+	struct PVS * pvs;
+	struct QuadBlock* qb;
+	struct CameraDC* camDC;
 	struct Driver* driver;
-	int i;
 
 	if
 	(
@@ -28,55 +26,45 @@ void DECOMP_MainFrame_VisMemFullFrame(struct GameTracker* gGT, struct Level* lev
 	)
 	{
 		// loop through all four cameras
-		for(i = 0; i < numPlyrCurrGame; i++)
+		for(char i = 0; i < gGT->numPlyrCurrGame; i++)
 		{
 			// CameraDC
-			cameraDC = gGT->cameraDC[i];
+			camDC = gGT->CameraDC[i];
 
 			// pointer to structure of each player
 			driver = gGT->drivers[i];
 
-			// CameraDC 0x20 (vismem->0x40)
-			unk20 = cameraDC->unk20;
-
 			// remove flag 0x4000
-			cameraDC->flags &= 0xffffbfff;
+			camDC->flags &= ~(0x4000);
+
+			visSrc = camDC->visLeafSrc[i];
 
 			// if camera can't figure out what to draw on it's own,
 			// if not following warpball path like track select videos,
-			if (unk20 == 0)
+			if (visSrc == 0)
 			{
 				if
 				(
 					(
 						// driver -> quadblock under driver
-						(driver->underDriver != 0) &&
+						(driver->underDriver != NULL) &&
 
-						// quadblock -> ptr_add_tex
-						(ptr_add_tex = driver->underDriver->ptr_add_tex, ptr_add_tex != 0)
+						(pvs = driver->underDriver->pvs, pvs != NULL)
 					) &&
 
 					// pull vismem data from quadblock + 0x44
-					(unk20 = ptr_add_tex->0x0, unk20 != 0)
+					(visSrc = pvs->visLeafSrc[i], visSrc != NULL)
 				)
 				{
-					// if cameraDC->0x20 is nullptr,
-					// grab from quadblock->0x440->0x0
-					
-					// VisMem 0x40-0x4F
-					gGT->visMem1->array4[i] = unk20;
+					gGT->visMem1->visLeafSrc[i] = visSrc;
 
-					// quadblock -> ptr_add_tex -> 0x0
-					uVar5 = driver->underDriver->ptr_add_tex->0x0;
-
-					if ((uVar5 & 1) == 0) goto LAB_80035900;
-LAB_800358e0:
+					if ((visSrc & 1) == 0) goto LAB_80035900;
+				
+				LAB_800358e0:
 					// copy until nullptr
-					FUN_80021da0
-					(
-						// VisMem 0x0-0xF
+					FUN_80021da0(
 						gGT->visMem1->visLeafList[i],
-						uVar5 & 0xfffffffc
+						visSrc & ~(1 | 2)
 					);
 				}
 			}
@@ -85,20 +73,15 @@ LAB_800358e0:
 			// if following warpball path like track select videos,
 			else
 			{
-				// VisMem 0x40-0x4F
-				iVar3 = gGT->visMem1;
-
 				// if this changed from previous frame
-				if (iVar3->array4[i] != unk20)
+				if (gGT->visMem1->visLeafSrc[i] != visSrc)
 				{
-					iVar3->array4[i] = unk20;
-
-					// CameraDC 0x20
-					uVar5 = cameraDC->unk20;
+					gGT->visMem1->visLeafSrc[i] = visSrc;
 
 					// if size is not specified
-					if ((uVar5 & 1) != 0) goto LAB_800358e0;
-LAB_80035900:
+					if ((visSrc & 1) != 0) goto LAB_800358e0;
+					
+				LAB_80035900:
 
 					// called VISMEM in prototypes
 					memcpy
@@ -108,7 +91,7 @@ LAB_80035900:
 						gGT->visMem1->visLeafList[i],
 
 						// source
-						uVar5,
+						visSrc,
 
 						// unk size
 						// get number of bits
@@ -124,12 +107,11 @@ LAB_80035900:
 				}
 			}
 
-			// CameraDC 0x24
-			unk20 = cameraDC->unk24;
+			visSrc = camDC->visFaceSrc[i];
 
 			// if camera can't figure out what to draw on it's own,
 			// if not following warpball path like track select videos,
-			if (unk20 == 0)
+			if (visSrc == 0)
 			{
 				if
 				(
@@ -138,43 +120,36 @@ LAB_80035900:
 						(driver->underDriver != 0) &&
 
 						(
-							// quadblock -> ptr_add_tex
-							unk20 = driver->underDriver->ptr_add_tex,
-							unk20 != 0
+							pvs = driver->underDriver->pvs,
+							pvs != 0
 						)
 					) &&
 
-					(unk20 = unk20->0x4, unk20 != 0)
+					(visSrc = pvs->visFaceSrc[i], visSrc != 0)
 				)
 				{
-					// if camDC->0x24 is nullptr,
-					// take from quadblock->0x44->0x4
+					// if camDC->visFaceSrc is nullptr,
+					// take from quadblock->0x44
 					
 					// VisMem 0x50-0x5F
-					gGT->visMem1->array5[i] = unk20;
+					gGT->visMem1->visFaceSrc[i] = visSrc;
 
-					// quadblock -> ptr_add_tex -> 0x4
-					uVar5 = driver->underDriver->ptr_add_tex->0x4;
-
-					if ((uVar5 & 1) == 0)
+					if ((visSrc & 1) == 0)
 					{
 						// called VISMEM in prototypes
 						memcpy
 						(
-							// VisMem 0x0-0xF
 							gGT->visMem1->visLeafList[i],
-							uVar5,
+							visSrc,
 							// unk size
 						);
 					}
 					else 
 					{
-						// VisMem 0x10-0x1F
 						// copy until nullptr
-						FUN_80021da0
-						(
+						FUN_80021da0(
 							gGT->visMem1->visFaceList[i],
-							uVar5 & 0xfffffffc
+							visSrc & 0xfffffffc
 						);
 					}
 				}
@@ -184,92 +159,72 @@ LAB_80035900:
 			// if following warpball path like track select videos,
 			else
 			{
-				// VisMem 0x50-0x5F
-				iVar3 = gGT->visMem1;
-
 				// if this changed from the previous frame
-				if ((void*)((int)iVar3->0x50[i]) != unk20)
+				if (gGT->visMem1->visFaceSrc[i] != visSrc)
 				{
-					(void*)((int)iVar3->0x50[i]) = unk20;
+					gGT->visMem1->visFaceSrc[i] = visSrc;
 
-					// CameraDC 0x24
-					if ((cameraDC->unk24 & 1) == 0)
+					if ((visSrc & 1) == 0)
 					{
 						memcpy
 						(
-							// VisMem 0x10-0x1F
-							gGT->visMem1->visFaceList[i]
+							gGT->visMem1->visFaceList[i],
 					
-							cameraDC->unk24,
+							visSrc,
 							// unk size
 						);
 					}
-
 					else
 					{
 						// copy until nullptr
-						FUN_80021da0
-						(
-
-							// VisMem 0x10-0x1F
+						FUN_80021da0(
 							gGT->visMem1->visFaceList[i],
-
-							// CameraDC 0x24
-							cameraDC->unk24 & 0xfffffffc
+							visSrc & 0xfffffffc
 						);
 					}
 
 					// quadblock under driver
-					unk20 = driver->underDriver;
+					qb = driver->underDriver;
 
 					if
 					(
-						// if no quadblock exists, or quadblock -> ptr_add_tex does not exist
-						((unk20 == 0) || (ptr_add_tex = unk20->ptr_add_tex, ptr_add_tex == 0)) ||
-						(
-							// data from quadblock -> ptr_add_tex is invalid
+						((qb == NULL) || (pvs = qb->pvs, pvs == NULL)) ||
+						
 							(
-								ptr_add_tex->0x0 == 0 ||
+								pvs->visLeafSrc[i] == NULL ||
 								
 								(
 									(
-										(ptr_add_tex->0x4 == 0 || (ptr_add_tex->0x8 == 0)) ||
+										(pvs->visFaceSrc[i] == 0 || (pvs->visInstSrc[i] == NULL)) ||
 
 										// LEV -> mesh_info -> ptrQuadBlockArray
-										(unk20 = (unk20 - gGT->level1->ptr_mesh_info->ptrQuadBlockArray) * -0x1642c859,
+										(iVar7 = ((int)qb - gGT->level1->ptr_mesh_info->ptrQuadBlockArray) * -0x1642c859,
 
-										// VisMem 0x10-0x1F
-										(*(u_int* )((unk20 >> 7) * 4 + gGT->visMem1->visFaceList[i]) &
-										1 << (unk20 >> 2 & 0x1fU)) != 0)
+										(*(u_int* )((iVar7 >> 7) * 4 + gGT->visMem1->visFaceList[i]) & 1 << (iVar7 >> 2 & 0x1fU)) != 0)
 									)
 								)
 							)
 						)
-					)
 					{
-						uVar5 = cameraDC->flags & 0xffffdfff;
+						cameraDC->flags &= ~(0x2000);
 					}
 
 					else
 					{
 						// camera is moving on path, not followinig driver
-						uVar5 = cameraDC->flags | 0x2000;
+						camDC->flags |= 0x2000;
 					}
 
-					cameraDC->flags = uVar5;
-
 					// if driver quadblock is wall, therefore need to improvise?
-					if ((cameraDC->flags & 0x2000) != 0)
+					if ((camDC->flags & 0x2000) != 0)
 					{
-						// param1 is PTR_DAT_8008d2ac
-						// i is screen ID (0, 1, 2, 3)
 						FUN_80035684(gGT, i);
-						cameraDC->flags = cameraDC->flags | 0x4000;
+						camDC->flags |= 0x4000;
 					}
 				}
 			}
 
-			if ((cameraDC->flags & 0x5000) == 0x1000)
+			if ((camDC->flags & 0x5000) == 0x1000)
 			{
 				// param1 is PTR_DAT_8008d2ac
 				// i is screen ID (0, 1, 2, 3)
@@ -280,99 +235,84 @@ LAB_80035900:
 			(
 				(
 					// if camera is following driver "normally"
-					(cameraDC->cameraMode == 0) &&
+					(camDC->cameraMode == 0) &&
 					// and not wall
-					((cameraDC->flags & 0x2000) != 0)
+					((camDC->flags & 0x2000) != 0)
 				) &&
 				(
 					(
 						// quadblock under driver
-						driver->underDriver != 0 &&
+						driver->underDriver != NULL &&
 
 						(
 							// driver -> quadblock -> ptr_add_tex
-							(unk20 = driver->underDriver->ptr_add_tex, unk20 != 0 &&
+							(pvs = driver->underDriver->pvs, pvs != 0 &&
 
-							(unk20 = unk20->0x8, unk20 != 0))
+							(visSrc = pvs->visInstSrc[i], visSrc != 0))
 						)
 					)
 				)
 			)
 			{
-				// quadblock->0x44->0x8
-		
 				// CameraDC 0x28
 				// which instances are visible
-				cameraDC->unk28 = unk20;
+				camDC->visInstSrc[i] = visSrc;
 			}
-		
+
+			visSrc = camDC->visOVertSrc[i];
+
 			// if no animated vertices
 			if ((level->configFlags & 4) == 0)
 			{
-				// VisMem 0x60-0x6F
-				unk20 = gGT->visMem1;
-				driver = unk20->0x60[i];
 
-				// CameraDC 0x2C
-				if (driver == cameraDC->unk2c)
+				if (gGT->visMem1->visOVertSrc[i] == visSrc)
 				{
-					if (driver == 0) goto LAB_80035ce0;
+					if (gGT->visMem1->visOVertSrc[i] == NULL) goto LAB_80035ce0;
 				}
 
 				// if change since last frame
 				else
 				{
-					// camera 0x2C
-					unk20->0x60[i] = cameraDC->unk2c;
+					gGT->visMem1->visOVertSrc[i] = visSrc;
+					dest = gGT->visMem1->visOVertList[i];
 
-					uVar5 = cameraDC->unk2c;
-
-					if ((uVar5 & 1) == 0) goto LAB_80035ce0;
-
-					// VisMem 0x20-0x2F (visOVertList)
-					uVar4 = gGT->visMem1->visOVertList[i];
+					if ((visSrc & 1) == 0) goto LAB_80035ce0;
 LAB_80035c98:
 					// copy until nullptr
-					FUN_80021da0(uVar4, uVar5 & 0xfffffffc);
+					FUN_80021da0(dest, visSrc & 0xfffffffc);
 				}
 			}
 		
 			// if animated lev vertices
 			else
 			{
-				// VisMem 0x0-0xF
-				unk20 = gGT->visMem1->visLeafList[i];
-				driver = unk20->0x70;
+				visSrc = camDC->visSCVertSrc[i];
 
 				// CameraDC 0x30
-				if (driver == cameraDC->unk30)
+				if (gGT->visMem1->visSCVertSrc[i] == visSrc)
 				{
-					if (driver != 0) continue;
+					if (gGT->visMem1->visSCVertSrc[i] != 0) continue;
 				}
 
 				// if change since previous frame
 				else
 				{
-					// VisMem + playerIndex*4 + 0x70 = CameraDC 0x30
-					unk20->0x70[playerIndex] = cameraDC->unk30;
-					uVar5 = cameraDC->unk30;
+					gGT->visMem1->visSCVertSrc[i] = visSrc;
 
-					if ((uVar5 & 1) != 0)
+					if ((visSrc & 1) != 0)
 					{
-						// VisMem 0x30-0x3F
-						uVar4 = gGT->visMem1->visSCVertList[i];
+						dest = gGT->visMem1->visSCVertList[i];
 						goto LAB_80035c98;
 					}
 				}
 LAB_80035ce0:
 				memcpy
 				(
-					uVar4,
+					dest,
 					// camDC offset 0x2c or 0x30
 					// unk size
 				);
 			}
 		}
 	}
-	return;
 }
