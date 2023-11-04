9cbed18f4e14b0ec1adc56137890d476e0ccd7ec
Agung Firdaus <agung.frds7@gmail.com>
2023-11-04 17:09:13 +0700

    PAL lng func research

--- decompile/WorkInProgress/src/230/MM_Menubox_Language.c
+++ decompile/WorkInProgress/src/230/MM_Menubox_Language.c
@@ -12,10 +12,11 @@ void MM_MENUBOX_Language(struct MenuBox *mb)
   {
     // Language id in EurRetail gGT
     *(char *)(sdata->gGT + 0x1d58) =
-        // OVR_230 global array of language ID
-        ((short *)0x800b4e28)[row];
+        
+		// EurRetail OVR_230 global array of language ID
+        OVR_230.langIndex[row];
 
-    sdata->gGT->gameMode2 |= 0x10000000; // Flag only foound in EurRetail
+    sdata->gGT->gameMode2 |= 0x10000000; // Flag only foound in EurRetail related to lang change
 
     mb->ptrPrevBox_InHierarchy->state &= ~(DRAW_NEXT_MENU_IN_HIERARCHY | ONLY_DRAW_TITLE);
   }
