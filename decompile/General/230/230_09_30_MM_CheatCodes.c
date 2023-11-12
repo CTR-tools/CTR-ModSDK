dd75b4a6b1c965833e0d8ecdda43faabe1ff5176
Niko <a>
2023-11-12 15:49:47 -0500

    50%

--- decompile/General/230/230_09_30_MM_CheatCodes.c
+++ decompile/General/230/230_09_30_MM_CheatCodes.c
@@ -469,7 +469,7 @@ void DECOMP_MM_ParseCheatCodes()
 	DecalFont_DrawLine(info, 		5, 197, FONT_SMALL, ORANGE);
 	DecalFont_DrawLine(__DATE__, 	5, 206, FONT_SMALL, ORANGE);
 	DecalFont_DrawLine(__TIME__, 	170, 206, FONT_SMALL, ORANGE);
-	DecalFont_DrawLine("49%", 		285, 206, FONT_SMALL, ORANGE);
+	DecalFont_DrawLine("50%", 		285, 206, FONT_SMALL, ORANGE);
 	#endif
 	
 	gpad = &sdata->gGamepads->gamepad[0];
