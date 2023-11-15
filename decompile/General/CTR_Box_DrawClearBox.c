96b97805c1707d6f173bd3ac90611e79a280c3ce
Niko <a>
2023-11-15 18:43:11 -0500

    fix ClearBox

--- decompile/General/CTR_Box_DrawClearBox.c
+++ decompile/General/CTR_Box_DrawClearBox.c
@@ -1,48 +1,38 @@
 #include <common.h>
 
-// Nicolas Noble: this is a composed primitive. aka, there's more than one command in it
-
-// spicyjpeg: most of sony's docs conflate dma packets and actual gpu commands under the term "primitive", but in actual
-// fact they are two completely independent things. psyq is written with the expectation that each packet contains exactly
-// one command (+ any parameters required for it), but games frequently violated that rule for better efficiency. the gpu
-// does not "see" packet headers and boundaries, all it see is a continuous stream of the contents of each packet so you
-// can have multiple commands in the same packet, or half of a command in one packet and the other half in the next
-// packet, or empty packets with no commands (which is how ordering tables are implemented). the first 4 bytes are your
-// usual dma packet header [editor's note: this is referring to p->tag]. the next 4 bytes are the tpage command, which is a
-// single 32bit word [editor's note: this is referring to p->tpage]. then there's a nop and then a 0x2a000000 command which
-// is a flat shaded, semitransparent quad [editor's note: this is referring to p->nop, and p->f4 as well as the setcode call]
-
 void DECOMP_CTR_Box_DrawClearBox(RECT* r, u_int* colorPtr, int transparency, u_long* ot, struct PrimMem* primMem)
 {
 	typedef struct
 	{
 		u_int tag;
 		u_int tpage;
-		u_int nop;
-		POLY_F4_T f4;
-	} compoundPrim;
+		POLY_F4 f4;
+	} multiCmdPacket;
 
 	void* primMem_curr = primMem->curr;
-	compoundPrim* p = 0;
+	multiCmdPacket* p = 0;
 
 	if (primMem_curr <= primMem->endMin100)
 	{
 		primMem->curr = (void *)((int)primMem_curr + 0x20);
-		p = (compoundPrim*)primMem_curr;
+		p = (multiCmdPacket*)primMem_curr;
 	}
 	if (p != 0)
 	{
 		setlen(p, 7);
-		setColor0(&p->f4, *colorPtr);
-		setcode_T(&p->f4, 0x28);
-		setSemiTrans_T(&p->f4, true);
-
 		p->tpage = (transparency << 5) | 0xe1000a00;
 
-		unsigned int rightX = r->x + r->w;
-		unsigned int bottomY = r->y + r->h;
+		*(int*)&p->f4.tag = 0;
+		*(int*)&p->f4.r0 = *colorPtr;
+		p->f4.code = 0x2A;
 
-		setXY4(&p->f4, r->x, r->y, rightX, r->y, r->x, bottomY, rightX, bottomY);
+		short rightX = r->x + r->w;
+		short bottomY = r->y + r->h;
+
+		*(int*)&p->f4.x0 = r->x | (r->y<<16);
+		*(int*)&p->f4.x1 = rightX | (r->y<<16);
+		*(int*)&p->f4.x2 = r->x | (bottomY<<16);
+		*(int*)&p->f4.x3 = rightX | (bottomY<<16);
 
 		AddPrim(ot, p);
 	}
