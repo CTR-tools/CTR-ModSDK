a1d5bf0ed38514bb6efa652fe48f130699d0c0d5
Niko <a>
2023-11-17 17:11:23 -0500

    switch to SDL keys

--- rebuild_PC/CrashTeamRacingPC.c
+++ rebuild_PC/CrashTeamRacingPC.c
@@ -25,6 +25,64 @@
 #include "../decompile/General/zGlobal_DATA.c"
 #include "../decompile/General/zGlobal_SDATA.c"
 
+enum SdlKeys
+{
+	SDL_SCANCODE_RIGHT = 79,
+	SDL_SCANCODE_LEFT = 80,
+	SDL_SCANCODE_DOWN = 81,
+	SDL_SCANCODE_UP = 82,
+	SDL_SCANCODE_C = 6,
+	SDL_SCANCODE_X = 27,
+};
+static int btnFlags = 0;
+
+int WIN_GetPressUp()
+{
+	return btnFlags & 0x1;
+}
+
+int WIN_GetPressDown()
+{
+	return btnFlags & 0x2;
+}
+
+int WIN_GetPressLeft()
+{
+	return btnFlags & 0x4;
+}
+
+int WIN_GetPressRight()
+{
+	return btnFlags & 0x8;
+}
+
+int WIN_GetPressCross()
+{
+	return btnFlags & 0x10;
+}
+
+int WIN_GetPressTriangle()
+{
+	return btnFlags & 0x20;
+}
+
+void PsyXKeyboardHandler(int key, char down)
+{
+	// here goes your SDL_SCANCODE_* debug keys
+	if (key == SDL_SCANCODE_UP)
+		btnFlags = down ? (btnFlags | 0x1) : (btnFlags & ~0x1);
+	if (key == SDL_SCANCODE_DOWN)
+		btnFlags = down ? (btnFlags | 0x2) : (btnFlags & ~0x2);
+	if (key == SDL_SCANCODE_LEFT)
+		btnFlags = down ? (btnFlags | 0x4) : (btnFlags & ~0x4);
+	if (key == SDL_SCANCODE_RIGHT)
+		btnFlags = down ? (btnFlags | 0x8) : (btnFlags & ~0x8);
+	if (key == SDL_SCANCODE_X)
+		btnFlags = down ? (btnFlags | 0x10) : (btnFlags & ~0x10);
+	if (key == SDL_SCANCODE_C)
+		btnFlags = down ? (btnFlags | 0x20) : (btnFlags & ~0x20);
+}
+
 int main()
 {
 	PsyX_Initialise("CTRPC", 800, 600, 0);
@@ -33,6 +91,7 @@ int main()
 	// set to 30 FPS VSync
 	PsyX_SetSwapInterval(2);
 	PsyX_EnableSwapInterval(1);
+	g_dbg_gameDebugKeys = PsyXKeyboardHandler;
 
 	return DECOMP_main();
 }
