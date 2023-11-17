e276347610058c90cf867e35874bee4190ab3654
Niko <a>
2023-11-17 17:40:33 -0500

    sopay fixes

--- include/gpu.h
+++ include/gpu.h
@@ -1,128 +1,134 @@
 #pragma once
 
+#ifdef REBUILD_PC
+#define ADD_PSX_ADDRESS
+#else
+#define ADD_PSX_ADDRESS ^ 0x80000000
+#endif
+
 force_inline void addPolyF3(u_long* ot, POLY_F3* p)
 {
 	p->tag = 0x4000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x20;
 }
 
 force_inline void addPolyFT3(u_long* ot, POLY_FT3* p)
 {
 	p->tag = 0x7000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x24;
 }
 
 force_inline void addPolyG3(u_long* ot, POLY_G3* p)
 {
 	p->tag = 0x6000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x30;
 }
 
 force_inline void addPolyGT3(u_long* ot, POLY_GT3* p)
 {
 	p->tag = 0x9000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x34;
 }
 
 force_inline void addPolyF4(u_long* ot, POLY_F4* p)
 {
 	p->tag = 0x5000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x28;
 }
 
 force_inline void addPolyFT4(u_long* ot, POLY_FT4* p)
 {
 	p->tag = 0x9000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x2c;
 }
 
 force_inline void addPolyG4(u_long* ot, POLY_G4* p)
 {
 	p->tag = 0x8000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x38;
 }
 
 force_inline void addPolyGT4(u_long* ot, POLY_GT4* p)
 {
 	p->tag = 0xc000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x3c;
 }
 
 force_inline void addSprt8(u_long* ot, SPRT* p)
 {
 	p->tag = 0x3000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x74;
 }
 
 force_inline void addSprt16(u_long* ot, SPRT* p)
 {
 	p->tag = 0x3000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x7c;
 }
 
 force_inline void addSprt(u_long* ot, SPRT* p)
 {
 	p->tag = 0x4000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x64;
 }
 
 force_inline void addTile1(u_long* ot, TILE* p)
 {
 	p->tag = 0x2000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x68;
 }
 
 force_inline void addTile8(u_long* ot, TILE* p)
 {
 	p->tag = 0x2000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x70;
 }
 
 force_inline void addTile16(u_long* ot, TILE* p)
 {
 	p->tag = 0x2000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x78;
 }
 
 force_inline void addTile(u_long* ot, TILE* p)
 {
 	p->tag = 0x3000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x60;
 }
 
 force_inline void addLineF2(u_long* ot, LINE_F2* p)
 {
 	p->tag = 0x3000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x40;
 }
 
 force_inline void addLineG2(u_long* ot, LINE_G2* p)
 {
 	p->tag = 0x4000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x50;
 }
 
 force_inline void addLineF3(u_long* ot, LINE_F3* p)
 {
 	p->tag = 0x5000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x48;
 	p->pad = 0x55555555;
 }
@@ -130,7 +136,7 @@ force_inline void addLineF3(u_long* ot, LINE_F3* p)
 force_inline void addLineG3(u_long* ot, LINE_G3* p)
 {
 	p->tag = 0x7000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x58;
 	p->pad = 0x55555555;
 	p->p1, p->p2 = 0;
@@ -139,7 +145,7 @@ force_inline void addLineG3(u_long* ot, LINE_G3* p)
 force_inline void addLineF4(u_long* ot, LINE_F4* p)
 {
 	p->tag = 0x6000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x4c;
 	p->pad = 0x55555555;
 }
@@ -147,7 +153,7 @@ force_inline void addLineF4(u_long* ot, LINE_F4* p)
 force_inline void addLineG4(u_long* ot, LINE_G4* p)
 {
 	p->tag = 0x9000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 0x5c;
 	p->pad = 0x55555555;
 	p->p1, p->p2 = 0;
@@ -157,7 +163,7 @@ force_inline void addLineG4(u_long* ot, LINE_G4* p)
 force_inline void addFill(u_long* ot, FILL* p)
 {
 	p->tag = 0x3000000 | *ot;
-	*ot = ((u_int) p) ^ 0x80000000;
+	*ot = ((u_int) p) ADD_PSX_ADDRESS;
 	p->code = 2;
 }
 #endif
