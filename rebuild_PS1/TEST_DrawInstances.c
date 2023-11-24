c9cf8d7c87667612456d5de8ec459fbaa7f1d4a1
Niko <a>
2023-11-24 17:15:30 -0500

    model progress -- remove Cheese

--- rebuild_PS1/TEST_DrawInstances.c
+++ rebuild_PS1/TEST_DrawInstances.c
@@ -1,246 +1,5 @@
 #include <common.h>
 
-struct ModelHeader_Custom
-{
-	char name[16];
-	int unk; // probably name is 20 bytes, idk
-
-	// distance till next model is drawn
-	short lodDistance;
-
-	// 0 for 3D
-	// 1 for 2D
-	short renderMode;
-
-	// X, Y, Z, padding
-	short modelScale[4];
-
-	unsigned int* ptrCmds;
-	unsigned int* ptrVert;
-	unsigned int* ptrTex;
-	unsigned int* ptrColor;
-
-	// leave these all zeros
-	int unk3;
-	int numAnim;
-	int ptrAnim;
-	int unk4;
-};
-
-// multiple headers usually means multiple LODs, but I want multiple headers for multiple models,
-// that all share vertex data and command data, I am not sure if CTR ever intended this, so it
-// might get messy. Also I change scale and color in the model header, not the instance, because instance
-// color isn't used when I try by default, and the CTR engine fights me when I try to change instance scale
-
-struct Model_GiveToInst
-{
-	char nameModel[16];
-	short modelID;
-
-	short numHeaders;
-	unsigned int* ptrHeaders;
-
-	// can be an array of headers
-	struct ModelHeader_Custom h;
-};
-
-struct Model_Custom
-{
-	struct Model_GiveToInst mgti[4];
-
-	// 4 bytes per command,
-	// must end in 0xFFFFFFFF
-	unsigned int cmd[0x16];
-
-	// some data in
-	// beginning for translation
-	// each point is 3 ints
-	unsigned char vert[0x58];
-
-	// each pointer goes to 3 ints,
-	// each holding VRAM coordinates,
-
-	// if 6 pointers are all nullptr,
-	// then it just uses colors
-	unsigned int texPtrs[6];
-
-	unsigned int colorsYellow[3];
-	unsigned int colorsCyan[3];
-};
-
-struct Model_Custom mCheese =
-{
-	// four models, one for each player
-	.mgti =
-	{
-		// team 1 - player 1
-		//.[0] =
-		{
-			.nameModel = "team1-player1",
-
-			// no label on this model
-			.modelID = 0xFFFF,
-
-			.numHeaders = 1,
-
-			.ptrHeaders = (unsigned int*)(OFFSETOF(struct Model_GiveToInst, h) + sizeof(struct Model_GiveToInst) * 0),
-
-			.h =
-			{
-				.name = "t1p1_low",
-				.unk = 0,
-
-				// render no matter how far away
-				.lodDistance = 0xFFFF,
-
-				.renderMode = 0,
-
-				.modelScale = { 0x400, 0x600, 0x6D3, 0 },
-
-				.ptrCmds = (unsigned int*)OFFSETOF(struct Model_Custom, cmd),
-				.ptrVert = (unsigned int*)OFFSETOF(struct Model_Custom, vert),
-				.ptrTex = (unsigned int*)OFFSETOF(struct Model_Custom, texPtrs),
-				.ptrColor = (unsigned int*)OFFSETOF(struct Model_Custom, colorsYellow),
-			},
-		},
-	},
-
-	.cmd =
-	{
-		3,
-
-		// side 1
-		0x98570001,
-		0x18570001,
-		0x18570001,
-		0x38570002,
-
-		// side 2
-		0x98570201,
-		0x18570201,
-		0x18570201,
-		0x38570202,
-
-		// side 3
-		0x98570201,
-		0x18570201,
-		0x18570201,
-		0x38570202,
-
-		// side 4
-		0x98570403,
-		0x18570403,
-		0x18570403,
-
-		// top
-		0x38570404,
-		0x18570405,
-
-		// bottom
-		0x38570406,
-		0x18570401,
-
-		// end geometry
-		0x38570402,
-		0xFFFFFFFF,
-	},
-
-	.vert =
-	{
-		// offset from center of model
-		0x81, 0xFF, 0x00, 0x00, // X
-		0x81, 0xFF, 0x00, 0x00, // Z
-		0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-
-		// 0x1c is the offset of the "vert"
-		// data where the positions start
-		0x1C, 0x00, 0x00, 0x00,
-
-		// 3 bytes per point (x, y, z)
-
-		// Z is forward, so to make arrow,
-		// X needs to shrink on +Z side
-
-		// XX, ZZ, YY
-		0x00, 0x00, 0x00, // (000)
-		0x80, 0xFF, 0x00, // (010)
-		0xFF, 0x00, 0x00, // (100)
-		0x80, 0xFF, 0x00, // (110)
-		0x80, 0xFF, 0xFF, // (111)
-		0xFF, 0x00, 0xFF, // (101)
-		0x80, 0xFF, 0x00, // (110)
-		0xFF, 0x00, 0x00, // (100)
-		0x00, 0x00, 0xFF, // (001)
-		0x80, 0xFF, 0xFF, // (011)
-		0x00, 0x00, 0x00, // (000)
-		0x80, 0xFF, 0x00, // (010)
-		0x80, 0xFF, 0x00, // (110)
-		0x80, 0xFF, 0x00, // (010)
-		0x80, 0xFF, 0xFF, // (111)
-		0x80, 0xFF, 0xFF, // (011)
-		0xFF, 0x00, 0xFF, // (101)
-		0x00, 0x00, 0xFF, // (001)
-		0xFF, 0x00, 0x00, // (100)
-		0x00, 0x00, 0x00, // (000)
-	},
-
-	//.texPtrs =
-	//{
-	//},
-
-	// yellow
-	.colorsYellow =
-	{
-		// bottom
-		0x2244,
-
-		// side
-		0x4080,
-
-		// top
-		0x80FF
-	},
-
-	// cyan
-	.colorsCyan =
-	{
-		// bottom
-		0x442200,
-
-		// side
-		0x804000,
-
-		// top
-		0xFF8000
-	},
-};
-
-int run = 0;
-
-void RunEntryHook()
-{
-	if (run == 1) return;
-	run = 1;
-
-#define FIX_OFFSET(x) \
-		x = (unsigned int*)((unsigned char*)&mCheese + (unsigned int)x)
-
-	int i;
-
-	for (i = 0; i < 4; i++)
-	{
-		// initialize models
-		FIX_OFFSET(mCheese.mgti[i].ptrHeaders);
-		FIX_OFFSET(mCheese.mgti[i].h.ptrCmds);
-		FIX_OFFSET(mCheese.mgti[i].h.ptrVert);
-		FIX_OFFSET(mCheese.mgti[i].h.ptrTex);
-		FIX_OFFSET(mCheese.mgti[i].h.ptrColor);
-	}
-}
-
 
 void TEST_DrawInstances(struct GameTracker* gGT)
 {
@@ -253,7 +12,6 @@ void TEST_DrawInstances(struct GameTracker* gGT)
 
 	struct OTMem* otMem = &gGT->backBuffer->otMem;
 	struct PrimMem* primMem = &gGT->backBuffer->primMem;
-	RunEntryHook();
 
 	for (
 			struct Instance* curr = gGT->JitPools.instance.taken.first;
@@ -261,9 +19,6 @@ void TEST_DrawInstances(struct GameTracker* gGT)
 			curr = curr->next
 		)
 	{
-		// temporary test
-		//curr->model = &mCheese.mgti[0];
-
 		for (int i = 0; i < gGT->numPlyrCurrGame; i++)
 		{
 			if ((curr->flags & 0x80) != 0) continue;
@@ -306,64 +61,6 @@ void TEST_DrawInstances(struct GameTracker* gGT)
 
 			// 3FF is background, 3FE is next depth slot
 			void* ot = &view->ptrOT[0x3FE];
-
-#if 0
-			POLY_F3* p = primMem->curr;
-			primMem->curr = p + 1;
-
-			// set Poly_LineF3 len, code, and padding
-			setPolyF3(p);
-
-			// RGB
-			setRGB0(p,
-				data.ptrColor[PLAYER_BLUE + i][0],
-				data.ptrColor[PLAYER_BLUE + i][0] >> 8,
-				data.ptrColor[PLAYER_BLUE + i][0] >> 16
-			);
-
-			posWorld1[0] = ((mh->ptrFrameData->pos[0] + 0) * mh->scale[0]) >> 8;
-			posWorld1[1] = ((mh->ptrFrameData->pos[1] + 0) * mh->scale[1]) >> 8;
-			posWorld1[2] = ((mh->ptrFrameData->pos[2] - 0x200) * mh->scale[2]) >> 8;
-			posWorld1[3] = 0;
-			gte_ldv0(&posWorld1[0]);
-			gte_rtps();
-			gte_stsxy(&posScreen1[0]);
-
-			posWorld2[0] = ((mh->ptrFrameData->pos[0] + 0x80) * mh->scale[0]) >> 8;
-			posWorld2[1] = ((mh->ptrFrameData->pos[1] + 0xFF) * mh->scale[1]) >> 8;
-			posWorld2[2] = ((mh->ptrFrameData->pos[2] - 0x200) * mh->scale[2]) >> 8;
-			posWorld2[3] = 0;
-			gte_ldv0(&posWorld2[0]);
-			gte_rtps();
-			gte_stsxy(&posScreen2[0]);
-
-			posWorld3[0] = ((mh->ptrFrameData->pos[0] + 0xFF) * mh->scale[0]) >> 8;
-			posWorld3[1] = ((mh->ptrFrameData->pos[1] +    0) * mh->scale[1]) >> 8;
-			posWorld3[2] = ((mh->ptrFrameData->pos[2] - 0x200) * mh->scale[2]) >> 8;
-			posWorld3[3] = 0;
-			gte_ldv0(&posWorld3[0]);
-			gte_rtps();
-			gte_stsxy(&posScreen3[0]);
-
-			// to be in viewport, coordinates must be
-			// X: [0, 0x40]
-			// Y: [0, 0xA0]
-			setXY3(p,
-				posScreen1[0], posScreen1[1],	// XY0
-				posScreen2[0], posScreen2[1],	// XY1
-				posScreen3[0], posScreen3[1]);	// XY2
-
-			AddPrim(ot, p);
-
-
-
-			// ============================
-
-			continue;
-
-			// ============================
-#endif
-
 
 
 			//helper type, kinda same as RGB
@@ -471,11 +168,11 @@ void TEST_DrawInstances(struct GameTracker* gGT)
 
 					*/
 
-					POLY_F3* p = primMem->curr;
+					LINE_F3* p = primMem->curr;
 					primMem->curr = p + 1;
 
 					// set Poly_LineF3 len, code, and padding
-					setPolyF3(p);
+					setLineF3(p);
 
 					// RGB
 					setRGB0(p,
