068a28cc3653e2d204ba674cec3008a6c6598494
Niko <a>
2023-11-30 04:50:17 -0800

    bugfix

--- decompile/General/231/231_006_RB_Hazard_HurtDriver.c
+++ decompile/General/231/231_006_RB_Hazard_HurtDriver.c
@@ -6,6 +6,8 @@ void RB_Hazard_HurtDriver(
 	struct Driver* driverAttacker,
 	int reason)
 {
+	struct GameTracker* gGT = sdata->gGT;
+	
 	// if not an AI
 	if ((driverVictim->actionsFlagSet & 0x100000) == 0)
 	{
@@ -13,34 +15,16 @@ void RB_Hazard_HurtDriver(
 		return;
 	}
 	
-	// if not an AI...
-	
-	// original code
-	#if 0
-	
 	// if boss mode
-	if ((sdata->gGT->gameMode1 & ADVENTURE_BOSS) != 0)
+	if ((gGT->gameMode1 & ADVENTURE_BOSS) != 0)
 	{
 		// if oxide station
-		if(sdata->gGT->levelID == OXIDE_STATION)
+		if(gGT->levelID == OXIDE_STATION)
 		{
 			// override to spin out
 			damageType = 1;
 		}
 	}
-	
-	// optimized code
-	#else
-	
-	// if bossID is oxide's challenge (4), 
-	// or oxide's final challenge (5)
-	if ((sdata->gGT->bossID & 4) != 0)
-	{
-		// override to spin out
-		damageType = 1;
-	}
-	
-	#endif
 	
 	BOTS_ChangeState(driverVictim, damageType, driverAttacker, reason);
 }
