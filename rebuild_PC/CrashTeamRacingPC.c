ea5f7d9839ad42ee7033d2a0208e3ada4fb160fd
Niko <a>
2023-12-08 16:38:46 -0800

    fix pc port

--- rebuild_PC/CrashTeamRacingPC.c
+++ rebuild_PC/CrashTeamRacingPC.c
@@ -12,14 +12,32 @@
 #include "PsyX/PsyX_public.h"
 #include "PsyX/PsyX_globals.h"
 #include "PsyX/PsyX_render.h"
+
+#define BUILD 926
 #define u_char unsigned char
 #define u_short unsigned short
 #define u_int unsigned int
 #define u_long unsigned int
-#define _Static_assert(x) ;
-#define __attribute__(x) ;
-#define BUILD 926
+
+// these two should do nothing
+#define _Static_assert(x) 
+#define __attribute__(x)
+
+// Since we dont have psn00b in PC
 #define RECT RECT16
+typedef enum {
+	PAD_ID_MOUSE = 0x1, // Sony PS1 mouse
+	PAD_ID_NEGCON = 0x2, // Namco neGcon
+	PAD_ID_IRQ10_GUN = 0x3, // "Konami" lightgun without composite video passthrough
+	PAD_ID_DIGITAL = 0x4, // Digital pad or Dual Analog/DualShock in digital mode
+	PAD_ID_ANALOG_STICK = 0x5, // Flight stick or Dual Analog in green LED mode
+	PAD_ID_GUNCON = 0x6, // Namco Guncon (lightgun with composite video passthrough)
+	PAD_ID_ANALOG = 0x7, // Dual Analog/DualShock in analog (red LED) mode
+	PAD_ID_MULTITAP = 0x8, // Multitap adapter (when tap_mode == 1)
+	PAD_ID_JOGCON = 0xe, // Namco Jogcon
+	PAD_ID_CONFIG_MODE = 0xf, // Dual Analog/DualShock in config mode (if len == 0x3)
+	PAD_ID_NONE = 0xf  // No pad connected (if len == 0xf)
+} PadTypeID;
 
 #include "../rebuild_PS1/main.c"
 #include "../decompile/General/zGlobal_DATA.c"
