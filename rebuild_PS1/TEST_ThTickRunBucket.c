26573e8c9c164032c81cbb99f2a8a643b58c174c
Niko <a>
2023-12-08 22:22:45 -0800

    bugfix

--- rebuild_PS1/TEST_ThTickRunBucket.c
+++ rebuild_PS1/TEST_ThTickRunBucket.c
@@ -4,7 +4,11 @@ void TEST_ThTickRunBucket(struct Thread* t)
 {
 	while(t != 0)
 	{
-		t->funcThTick(t);
+		// if thread wasn't killed in a menu -- this is required cause dead threads
+		// aren't cleaned until after bucket execution, and menu is before bucket execution
+		if((t->flags & 0x800) == 0)
+			t->funcThTick(t);
+		
 		t = t->siblingThread;
 	}
 }
