dc731aa1d7f4781155ab85f75e72e1c7ffb828ab
Niko <a>
2023-12-19 00:31:05 -0800

    progress

--- decompile/General/231/231_023_RB_Hazard_InterpolateValue.c
+++ decompile/General/231/231_023_RB_Hazard_InterpolateValue.c
@@ -6,18 +6,18 @@ int DECOMP_RB_Hazard_InterpolateValue(short currRot, short desiredRot, short rot
 
 	int delta = (rotSpeed * sdata->gGT->elapsedTimeMS) >> 5;
 
+	// adjust for range of "short" [0-0xffff]
+	// compared to range of degrees [0-0xfff]
 	int diff = ((desiredRot - currRot) & 0xfff);
 	if (diff == 0) return currRot & 0xfff;
 	if (diff > 0x7ff) diff -= 0x1000;
-	
-	if (desiredRot < 0) 
-		desiredRot = -desiredRot;
+	if (diff < 0) diff = -diff;
 
-	if (rotSpeed > desiredRot)
-		return desiredRot & 0xfff;
+	// skip to end, if close enough
+	if (diff < rotSpeed) return desiredRot & 0xfff;
 
+	// interpolate
 	if (diff < 0) currRot -= delta;
 	else currRot += delta;
-
 	return currRot & 0xfff;
 }
