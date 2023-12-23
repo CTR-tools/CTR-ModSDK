63246e1a826af4ac8ad574f32d4ae6d550e943e6
Niko <a>
2023-12-23 10:38:42 -0500

    progress

--- decompile/General/zGlobal_DATA.c
+++ decompile/General/zGlobal_DATA.c
@@ -300,7 +300,9 @@ struct Data data =
 	
 	.MetaDataModels =
 	{
-		// turn off for rebuild_ps1
+		// turn off for rebuild_ps1,
+		// leave on for rebuild_pc,
+		// leave on for decomp/General
 		#if 1
 		
 		// 0x00 - NO_FUNC
