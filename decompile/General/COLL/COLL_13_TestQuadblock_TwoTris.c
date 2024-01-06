829fcf762e5f3b6a0f0c175786486aea54d5bb86
Niko <a>
2024-01-06 15:47:29 -0500

    COLL progress

--- decompile/General/COLL/COLL_13_TestQuadblock_TwoTris.c
+++ decompile/General/COLL/COLL_13_TestQuadblock_TwoTris.c
@@ -9,14 +9,18 @@ void DECOMP_COLL_TestQuadblock_TwoTris(struct ScratchpadStruct* sps,struct QuadB
   
   sps->dataOutput[6] = quad->triNormalVecBitShift;
   
+  // TriNormalVecDividend
+  short* SPS_TNVD = &sps->dataOutput[4];
+  short* QBL_TNVD = &quad->triNormalVecDividend[0];
+  
   // calculate normal vectors for two triangles,
   // no collision detection here
   struct BspSearchVertex* bsv = &sps->bspSearchVert[0];
   
   if (sps->unk4C[0xA0] != sps->unk4C[0xA2]) {
-    sps->dataOutput[4] = quad->triNormalVecDividend[9];
+    *SPS_TNVD = QBL_TNVD[9];
     COLL_TestTriangle_GetNormalVector(sps, &bsv[1], &bsv[3], &bsv[2]); // 1, 3, 2
   }
-  sps->dataOutput[4] = quad->triNormalVecDividend[8];
+  *SPS_TNVD = QBL_TNVD[8];
   COLL_TestTriangle_GetNormalVector(sps, &bsv[0], &bsv[1], &bsv[2]); // 0, 1, 2
 }
--- decompile/General/COLL/COLL_14_TestQuadblock_EightTris.c
+++ decompile/General/COLL/COLL_14_TestQuadblock_EightTris.c
@@ -9,26 +9,38 @@ void DECOMP_COLL_TestQuadblock_EightTris(struct ScratchpadStruct* sps, struct Qu
   
   sps->dataOutput[6] = quad->triNormalVecBitShift;
   
+  // TriNormalVecDividend
+  short* SPS_TNVD = &sps->dataOutput[4];
+  short* QBL_TNVD = &quad->triNormalVecDividend[0];
+  
   // calculate normal vectors for eight triangles,
   // no collision detection here
   struct BspSearchVertex* bsv = &sps->bspSearchVert[0];
   
-  if (sps->unk4C[0xA0] != sps->unk4C[0xA2]) {  // Do we hit two quads? if then, check two quads.
-    sps->dataOutput[4] = quad->triNormalVecDividend[4]; // triangle 4
+  if (sps->unk4C[0xA0] != sps->unk4C[0xA2]) 
+  {
+    *SPS_TNVD = QBL_TNVD[4]; // triangle 4
     COLL_TestTriangle_GetNormalVector(sps, &bsv[8], &bsv[6], &bsv[7]); // 8, 6, 7
-    sps->dataOutput[4] = quad->triNormalVecDividend[5]; // triangle 5
+    
+	*SPS_TNVD = QBL_TNVD[5]; // triangle 5
     COLL_TestTriangle_GetNormalVector(sps, &bsv[7], &bsv[3], &bsv[8]); // 7, 3, 8
-    sps->dataOutput[4] = quad->triNormalVecDividend[6]; // triangle 6
+    
+	*SPS_TNVD = QBL_TNVD[6]; // triangle 6
     COLL_TestTriangle_GetNormalVector(sps, &bsv[1], &bsv[7], &bsv[6]); // 1, 7, 6
-    sps->dataOutput[4] = quad->triNormalVecDividend[7]; // triangle 7
+    
+	*SPS_TNVD = QBL_TNVD[7]; // triangle 7
     COLL_TestTriangle_GetNormalVector(sps, &bsv[2], &bsv[6], &bsv[8]); // 2, 6, 8
   }
-  sps->dataOutput[4] = quad->triNormalVecDividend[0]; // triangle 0
+  
+  *SPS_TNVD = QBL_TNVD[0]; // triangle 0
   COLL_TestTriangle_GetNormalVector(sps, &bsv[0], &bsv[4], &bsv[5]); // 0, 4, 5
-  sps->dataOutput[4] = quad->triNormalVecDividend[1]; // triangle 1
+  
+  *SPS_TNVD = QBL_TNVD[1]; // triangle 1
   COLL_TestTriangle_GetNormalVector(sps, &bsv[4], &bsv[6], &bsv[5]); // 4, 6, 5
-  sps->dataOutput[4] = quad->triNormalVecDividend[2]; // triangle 2
+  
+  *SPS_TNVD = QBL_TNVD[2]; // triangle 2
   COLL_TestTriangle_GetNormalVector(sps, &bsv[6], &bsv[4], &bsv[1]); // 6, 4, 1
-  sps->dataOutput[4] = quad->triNormalVecDividend[3]; // triangle 3
+  
+  *SPS_TNVD = QBL_TNVD[3]; // triangle 3
   COLL_TestTriangle_GetNormalVector(sps, &bsv[5], &bsv[6], &bsv[2]); // 5, 6, 2
 }
