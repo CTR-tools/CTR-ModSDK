448324c55b231d02bf94e0fefbd5c97aaa1b44c1
Niko <a>
2024-01-13 13:54:30 -0500

    UI improvement

---
+++ decompile/General/UI/UI_AAA_BigBlock1.c
@@ -0,0 +1,19 @@
+#include <common.h>
+
+#include "UI_DrawNumWumpa.c" 
+#include "UI_DrawNumKey.c"
+#include "UI_DrawNumRelic.c"
+#include "UI_DrawNumTrophy.c"
+#include "UI_DrawNumCrystal.c"
+#include "UI_DrawNumTimebox.c"
+#include "UI_DrawSpeedBG.c"
+#include "UI_DrawSpeedNeedle.c" 
+#include "UI_JumpMeter_Draw.c"
+#include "UI_JumpMeter_Update.c" 
+#include "UI_DrawSlideMeter.c"
+
+#ifdef USE_60FPS
+#endif
+
+#ifdef USE_16BY9
+#endif
