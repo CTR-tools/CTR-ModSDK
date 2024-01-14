b02d7453ce3fe0b65e5f9db8de36c2957cf53f0e
Niko <a>
2024-01-14 01:55:05 -0500

    Decomp timestamp widescreen fix

--- decompile/General/230/230_09_30_MM_CheatCodes.c
+++ decompile/General/230/230_09_30_MM_CheatCodes.c
@@ -466,10 +466,10 @@ void DECOMP_MM_ParseCheatCodes()
 	// so store this code here
 	#if 1
 	char* info = "Date/Time in CheatCodes.c";
-	DECOMP_DecalFont_DrawLine(info, 		5, 197, FONT_SMALL, ORANGE);
-	DECOMP_DecalFont_DrawLine(__DATE__, 	5, 206, FONT_SMALL, ORANGE);
-	DECOMP_DecalFont_DrawLine(__TIME__, 	170, 206, FONT_SMALL, ORANGE);
-	DECOMP_DecalFont_DrawLine("57%", 		285, 206, FONT_SMALL, ORANGE);
+	DECOMP_DecalFont_DrawLine(info, 	WIDE_PICK(5, 100-50), 197, FONT_SMALL, ORANGE);
+	DECOMP_DecalFont_DrawLine(__DATE__, WIDE_PICK(5, 100-50), 206, FONT_SMALL, ORANGE);
+	DECOMP_DecalFont_DrawLine(__TIME__, WIDE_PICK(170, 228-50), 206, FONT_SMALL, ORANGE);
+	DECOMP_DecalFont_DrawLine("57%", 	WIDE_PICK(285, 315-50), 206, FONT_SMALL, ORANGE);
 	#endif
 	
 	gpad = &sdata->gGamepads->gamepad[0];
