52ebdb4fc3c0c234cebd8e5d2a33d194a2c13bc3
Niko <a>
2024-01-24 14:04:10 -0500

    podium fix

--- decompile/General/Tawna_Init.c
+++ decompile/General/Tawna_Init.c
@@ -11,6 +11,9 @@ void DECOMP_Tawna_Init(struct GameTracker *gGT)
   gGT->podium_modelIndex_First = 0;
   gGT->podium_modelIndex_Second = 0;
   gGT->podium_modelIndex_Third = 0;
+    
+  unsigned char* podiumModelIndexArr =
+	&gGT->podium_modelIndex_First;
 
   for (i = 0; i < 8; i++)
   {
@@ -19,58 +22,45 @@ void DECOMP_Tawna_Init(struct GameTracker *gGT)
 
     if (driver == NULL) continue;
     
-    // 0 = 1st place, 1 = 2nd place, 2 = 3rd place, etc
     rank = driver->driverRank;
-    index = data.characterIDs[driver->driverID];
-
-	if (rank == 0)
-    {
-      // First place
-      gGT->podium_modelIndex_First = index + 0x7e;
-      
-	  // Set podium_modelIndex_tawna based on char index
-      switch (index)
-      {
-		// Crash + Coco
-        case 0:
-        case 3:
-          // STATIC_TAWNA2 (Isabella)
-          gGT->podium_modelIndex_tawna = 0x90;
-          break;
-        
-		// Polar + Pura
-		case 6:
-        case 7:
-          // STATIC_TAWNA3 (Liz)
-          gGT->podium_modelIndex_tawna = 0x91;
-          break;
-		
-		// Cortex + NGin
-		case 1:
-        case 4:
-          // STATIC_TAWNA4 (Megumi)
-          gGT->podium_modelIndex_tawna = 0x92;
-		  break;
-		
-	    default:
-		  // STATIC_TAWNA1 (Ami)
-		  gGT->podium_modelIndex_tawna = 0x8f;
-          break;
-      }
-	
-	  continue;
-    }
-
-    if (rank == 1)
-    {
-      gGT->podium_modelIndex_Second = index + 0x7e;
-	  continue;
-	}
     
-    else if (rank == 2)
+	if (rank < 3)
     {
-      gGT->podium_modelIndex_Third = index + 0x7e;
-	  continue;
-	}
+	  index = data.characterIDs[driver->driverID];
+      podiumModelIndexArr[rank] = index + 0x7e;
+	  
+	  if(rank != 0) continue;
+	  
+	  // === Rank0, first place ===
+	  
+	  switch (index)
+	  {
+	  	// Crash + Coco
+	  	case 0:
+	  	case 3:
+	  	// STATIC_TAWNA2 (Isabella)
+	  	gGT->podium_modelIndex_tawna = 0x90;
+	  	break;
+	  	
+	  	// Polar + Pura
+	  	case 6:
+	  	case 7:
+	  	// STATIC_TAWNA3 (Liz)
+	  	gGT->podium_modelIndex_tawna = 0x91;
+	  	break;
+	  
+	  	// Cortex + NGin
+	  	case 1:
+	  	case 4:
+	  	// STATIC_TAWNA4 (Megumi)
+	  	gGT->podium_modelIndex_tawna = 0x92;
+	  	break;
+	  
+	  	default:
+	  	// STATIC_TAWNA1 (Ami)
+	  	gGT->podium_modelIndex_tawna = 0x8f;
+	  	break;
+	  }
+    }
   }
 }
