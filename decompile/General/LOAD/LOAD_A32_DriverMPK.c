ac123988f136f84ff18b00d1513349b4944a0d99
Niko <a>
2024-02-14 09:47:03 -0500

    USE_GPU1P

--- decompile/General/LOAD/LOAD_A32_DriverMPK.c
+++ decompile/General/LOAD/LOAD_A32_DriverMPK.c
@@ -1,6 +1,5 @@
 #include <common.h>
 
-
 void DECOMP_LOAD_DriverMPK(unsigned int param_1,int levelLOD,unsigned int param_3)
 {
 	int i;
@@ -17,6 +16,24 @@ void DECOMP_LOAD_DriverMPK(unsigned int param_1,int levelLOD,unsigned int param_
 	// 3P/4P
 	if(levelLOD - 3U < 2)
 	{
+		#ifdef USE_GPU1P
+		
+		for(i = 0; i < 3; i++)
+		{
+			// high LOD models
+			DECOMP_LOAD_AppendQueue(param_1,LT_DRAM,
+				data.characterIDs[i] + (base-0xf2)+0xf2,
+				&data.driverModel_lowLOD[i],0xfffffffe);
+		}
+
+		// Time Trial MPK (with high LOD weapons),
+		// dont use adv mpk, it has no weapons
+		DECOMP_LOAD_AppendQueue(param_1,LT_DRAM,
+			data.characterIDs[i] + (base-0xf2)+0x124,
+			0,param_3);
+			
+		#else
+
 		for(i = 0; i < 3; i++)
 		{
 			// low lod CTR model
@@ -29,6 +46,8 @@ void DECOMP_LOAD_DriverMPK(unsigned int param_1,int levelLOD,unsigned int param_
 		DECOMP_LOAD_AppendQueue(param_1,LT_DRAM,
 			data.characterIDs[i] + (base-0xf2)+0x15c,
 			0,param_3);
+			
+		#endif
 
 		return;
 	}
@@ -107,10 +126,9 @@ void DECOMP_LOAD_DriverMPK(unsigned int param_1,int levelLOD,unsigned int param_
 
 	if(levelLOD == 2)
 	{
-		// med models
 		for(i = 0; i < 2; i++)
 		{
-			// low lod CTR model
+			// med lod CTR model
 			DECOMP_LOAD_AppendQueue(param_1,LT_DRAM,
 				data.characterIDs[i] + (base-0xf2)+0x134,
 				&data.driverModel_lowLOD[i],0xfffffffe);
