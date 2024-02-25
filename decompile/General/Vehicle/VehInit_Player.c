624a26d04c8ac81fbe3b10f75982d11011e27ead
Niko <a>
2024-02-25 14:45:17 -0500

    progress

--- decompile/General/Vehicle/VehInit_Player.c
+++ decompile/General/Vehicle/VehInit_Player.c
@@ -8,6 +8,13 @@ struct Driver* DECOMP_VehInit_Player(int index)
 	struct Driver* d = t->object;
 	memset(d, 0, 0x62c);
 	
+	// My bad, this wasn't safe to relocate -- Niko
+	// Patch call to VehPtr_Crashing_Init from COLL
+	#ifndef REBUILD_PC
+	#define JAL(dest) (((unsigned long)dest & 0x3FFFFFF) >> 2 | 0xC000000)
+	*(int*)0x800214bc = JAL(DECOMP_VehPtr_Crashing_Init);
+	#endif
+	
 	DECOMP_VehInit_NonGhost(t, index);
 	
 	d->funcPtrs[0] = DECOMP_VehPtr_Driving_Init;
