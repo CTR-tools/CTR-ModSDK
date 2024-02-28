95d9c819b29ae9145caacc7c681b119c33c587e6
Niko <a>
2024-02-28 01:23:39 -0500

    ElimBG WIP progress

--- decompile/General/ElimBG/ElimBG_SaveScreenshot_Full.c
+++ decompile/General/ElimBG/ElimBG_SaveScreenshot_Full.c
@@ -42,6 +42,10 @@ void DECOMP_ElimBG_SaveScreenshot_Full(struct GameTracker* gGT)
   local_38.w = 0x200;
   local_38.x = 0;
   local_38.h = 8;
+  
+  local_30.x = 0x200;
+  local_30.w = 0x80;
+  local_30.h = 8;
 
   // swapchain index * Y-offset of second swapchain image
   sVar5 = gGT->swapchainIndex * 0x128;
@@ -51,11 +55,10 @@ void DECOMP_ElimBG_SaveScreenshot_Full(struct GameTracker* gGT)
   iVar2 = 0;
   do {
     iVar4 = 1 - iVar4;
+	
     local_38.y = sVar5 + iVar2 + 8;
-    local_38.x = 0;
-    local_38.w = 0x200;
-    local_38.h = 8;
-    DrawSync(0);
+    
+	DrawSync(0);
     StoreImage(&local_38,sdata->PausePtrsVRAM[2+iVar4]);
 
     DECOMP_ElimBG_SaveScreenshot_Chunk(
@@ -63,11 +66,9 @@ void DECOMP_ElimBG_SaveScreenshot_Full(struct GameTracker* gGT)
 		sdata->PausePtrsVRAM[2+(1-iVar4)],
 		0x1000);
 
-	local_30.x = 0x200;
-    local_30.w = 0x80;
-    local_30.h = 8;
     iVar3 = iVar2 + 8;
     local_30.y = iVar2;
+
     LoadImage(&local_30,sdata->PausePtrsVRAM[4+(1-iVar4)]);
 
 	iVar1 = iVar2 + 0x10;
@@ -81,15 +82,11 @@ void DECOMP_ElimBG_SaveScreenshot_Full(struct GameTracker* gGT)
 		sdata->PausePtrsVRAM[2+(iVar4)],
 	0x1000);
 
-  local_30.x = 0x200;
   local_30.y = iVar3;
-  local_30.w = 0x80;
-  local_30.h = 8;
   LoadImage(&local_30,sdata->PausePtrsVRAM[4+(iVar4)]);
 
   local_30.y = 0xff;
   local_30.w = 0x10;
-  local_30.x = 0x200;
   local_30.h = 1;
   LoadImage(&local_30,&data.pauseScreenStrip[0]);
 }
