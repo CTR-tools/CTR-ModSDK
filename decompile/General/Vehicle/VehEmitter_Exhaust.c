35e2b62f065ecb3077a325d705f2abf9bf7f4d17
Niko <a>
2024-03-13 20:20:01 -0400

    VehEmitter_Exhaust

---
+++ decompile/General/Vehicle/VehEmitter_Exhaust.c
@@ -0,0 +1,92 @@
+#include <common.h>
+
+struct Particle* DECOMP_VehEmitter_Exhaust(struct Driver *d, VECTOR *param_2, VECTOR *param_3)
+{
+    int exhaustType;
+
+    struct ParticleEmitter *emSet;
+    struct Particle *p = NULL;
+	struct GameTracker* gGT = sdata->gGT;
+	struct Instance* dInst = d->instSelf;
+
+    if (d->invisibleTimer != 0)
+		return 0;
+	
+	if ((dInst->flags & HIDE_MODEL) != 0)
+		return 0;
+
+	// low LOD exhaust (4p or ai car)
+    exhaustType = 1;
+	emSet = &data.emSet_Exhaust_Low[0];
+
+    char numPlyr = gGT->numPlyrCurrGame;
+
+	if (dInst->thread->modelIndex != DYNAMIC_ROBOT_CAR)
+	{
+		switch (numPlyr)
+		{
+		case 1:
+			// 1P mode, high LOD exhaust
+			emSet = &data.emSet_Exhaust_High[0];
+			break;
+		case 2:
+			// 2P mode, med LOD exhaust
+			emSet = &data.emSet_Exhaust_Med[0];
+			break;
+		}
+	}
+
+    if (
+			((dInst->flags & SPLIT_LINE) != 0) && 
+			((param_2->vy - param_3->vy) + d->posCurr[1] < 256)
+		)
+    {
+		// bubble texture
+        exhaustType = 7;
+        emSet = &data.emSet_Exhaust_Water[0];
+    }
+
+    p = Particle_CreateInstance(0, gGT->iconGroup[exhaustType], emSet);
+
+    if (p == NULL)
+        return p;
+
+    for (char i = 0; i < 3; i++)
+	{
+        p->axis[i].startVal += (((int*)param_2)[i] - ((int*)param_3)[i]);
+        p->axis[i].velocity = ((int*)param_3)[i];
+    }
+
+    p->driverInst = dInst;
+    p->unk18 = dInst->unk50;
+
+    if (exhaustType == 7)
+    {
+		void Particle_FuncPtr_ExhaustUnderwater(struct Particle *p);
+        p->funcPtr = Particle_FuncPtr_ExhaustUnderwater;
+    }
+
+    // if engine revving
+    if (d->kartState == KS_ENGINE_REVVING)
+    {
+        if (d->engineRevState != 1)
+        {
+            return p;
+        }
+    }
+	
+    // if not engine revving
+    else
+    {
+        short meterLeft = d->turbo_MeterRoomLeft;
+        if ((meterLeft < 129) || (((d->const_turboLowRoomWarning + 2) * 32) < meterLeft))
+        {
+            return p;
+        }
+    }
+	
+    p->flagsSetColor &= ~(0x60); 
+	p->flagsSetColor |= 0x40;
+	
+    return p;
+}
