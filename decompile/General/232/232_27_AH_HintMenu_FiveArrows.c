04cee814f31e235657a716d68a3439a6fd9e85f5
Niko <a>
2024-03-28 15:17:33 -0700

    HintMenu_FiveArrows

---
+++ decompile/General/232/232_27_AH_HintMenu_FiveArrows.c
@@ -0,0 +1,25 @@
+#include <common.h>
+
+void DECOMP_AH_HintMenu_FiveArrows(int param_1,short rotation)
+{
+  int *ptrColor;
+  char i;
+  
+  ptrColor = &D232.fiveArrow_col1[0];
+  if (((sdata->frameCounter >> FPS_RIGHTSHIFT(0)) & 2) != 0)
+  	ptrColor = &D232.fiveArrow_col2[0];
+  
+  for (i = 0; i < 5; i++)
+  {	
+	AH_Map_HubArrow(
+        // posX
+        (i * 0x32 + 0x95),
+		
+		// posY
+		(param_1 + 4), 
+        
+		&D232.fiveArrow_pos[0],
+		
+        ptrColor,0x800,(int)rotation);
+  }
+}
