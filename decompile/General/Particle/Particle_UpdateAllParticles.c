6546794d2a94a8ba4a0a50416f976dd6428f2fcf
Niko <a>
2024-03-30 16:34:44 -0700

    crashing bugfix

--- decompile/General/Particle/Particle_UpdateAllParticles.c
+++ decompile/General/Particle/Particle_UpdateAllParticles.c
@@ -5,8 +5,12 @@ void DECOMP_Particle_UpdateAllParticles(void)
   int* ptrToList;
   struct GameTracker* gGT = sdata->gGT;
   
+  // Pretty sure this is only for debugging,
+  // not an actual game feature, and we need bytes
+#if 0
   if ((gGT->gameMode1 & PAUSE_THREADS) != 0)
 	  return;
+#endif
   
   // world-space particles (exhaust on cars)
   ptrToList = &gGT->particleList_ordinary;
