55bd08c09437f69a08be31363c6f0acc2d734824
Niko <a>
2024-03-30 11:23:21 -0700

    pc port fix

--- rebuild_PS1/main.c
+++ rebuild_PS1/main.c
@@ -554,4 +554,5 @@
 
 #include "../decompile/General/232/232_Block1.c"
 
-#include "../decompile/General/233/233_37_40_CS_Garage.c"
+#include "../decompile/General/233/233_37_39_CS_Garage.c"
+#include "../decompile/General/233/233_40_CS_Garage_GetMenuBox.c"
