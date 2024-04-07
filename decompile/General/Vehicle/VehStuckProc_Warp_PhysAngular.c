5592276ab405ea09fd054cafc614620e0f4c0d87
Niko <a>
2024-04-07 09:20:15 -0700

    60fps

--- decompile/General/Vehicle/VehStuckProc_Warp_PhysAngular.c
+++ decompile/General/Vehicle/VehStuckProc_Warp_PhysAngular.c
@@ -45,14 +45,12 @@ void DECOMP_VehStuckProc_Warp_PhysAngular(struct Thread *th, struct Driver *d)
         // interpolate until scale is [0x12c0, 0x960, 0x12c0],
         // car is wide and short
 
-		#ifdef USE_60FPS
-		if(halfTime)
-		#endif
-			for (char i = 0 ; i < 3;i++)
-				inst->scale[i] = 
-					VehCalc_InterpBySpeed(
-						inst->scale[i], 
-						120, 4800 >> (i & 1));
+		for (char i = 0 ; i < 3;i++)
+			inst->scale[i] = 
+				VehCalc_InterpBySpeed(
+					inst->scale[i], 
+					FPS_HALF(120), 
+					4800 >> (i & 1));
 
         if (d->posCurr[1] < d->quadBlockHeight + 0x8000)
             d->posCurr[1] += FPS_HALF(0x800);
@@ -67,15 +65,12 @@ void DECOMP_VehStuckProc_Warp_PhysAngular(struct Thread *th, struct Driver *d)
         // interpolate until scale is [0, 24000, 0],
         // car is tall and thin
 
-		#ifdef USE_60FPS
-		if(halfTime)
-		#endif
-			for (char i = 0 ; i < 3;i++)
-				inst->scale[i] = 
-					VehCalc_InterpBySpeed(
-						inst->scale[i], 
-						(i == 1) ? 3200 : 600,
-						24000 * (i & 1));
+		for (char i = 0 ; i < 3;i++)
+			inst->scale[i] = 
+				VehCalc_InterpBySpeed(
+					inst->scale[i], 
+					(i == 1) ? FPS_HALF(3200) : FPS_HALF(600),
+					24000 * (i & 1));
 
         // if scale shrinks to zero
         if (inst->scale[0] == 0)
