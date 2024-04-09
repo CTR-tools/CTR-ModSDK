998673e9569ab5446285645812b6f00aa972e455
PedroHLC <root@pedrohlc.com>
2024-04-09 08:48:51 -0300

    add CI building them all

--- flake.nix
+++ flake.nix
@@ -1,15 +1,29 @@
 {
   inputs.nixpkgs.url = https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz;
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; {
+  outputs = { self, nixpkgs }: rec {
+    packages.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; rec {
       native32 = with pkgsi686Linux; {
         gcc = callPackage ./rebuild_PC { ctrModSDK = self; };
         clang = callPackage ./rebuild_PC { ctrModSDK = self; stdenv = clangStdenv;  };
       };
-      mingw32 = with mingw32; {
+      mingw32 = with pkgsCross.mingw32; {
         gcc = callPackage ./rebuild_PC { ctrModSDK = self; };
         clang = callPackage ./rebuild_PC { ctrModSDK = self; stdenv = clangStdenv;  };
       };
+      all = stdenvNoCC.mkDerivation {
+        name = "ctr-join";
+        dontUnpack = true;
+        dontBuild = true;
+        installPhase = ''
+          mkdir $out
+          ln -s ${native32.gcc} $out/native32-gcc
+          ln -s ${native32.clang} $out/native32-clang
+          ln -s ${mingw32.gcc} $out/mingw32-gcc
+          ln -s ${mingw32.clang} $out/mingw32-clang
+        '';
+      };
     };
+    # default
+    package.x86_64-linux = packages.x86_64-linux.native32.gcc;
   };
 }
