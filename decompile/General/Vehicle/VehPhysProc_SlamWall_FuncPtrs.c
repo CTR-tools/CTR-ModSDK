ac87850f20ef45ec12e1937df9316b211b3074d1
Niko <a>
2024-04-12 03:38:24 -0700

    undo last commit

--- decompile/General/Vehicle/VehPhysProc_SlamWall_FuncPtrs.c
+++ decompile/General/Vehicle/VehPhysProc_SlamWall_FuncPtrs.c
@@ -20,30 +20,21 @@ void DECOMP_VehPhysProc_SlamWall_PhysAngular(struct Thread* t, struct Driver* d)
 	
 	d->rotCurr.y = d->unk3D4[0] + d->angle + d->turnAngleCurr;
 	
-	#ifdef USE_60FPS
-	if(sdata->gGT->timer&1)
-	{
-	#endif
-	
-		d->rotCurr.w = 
-			DECOMP_VehCalc_InterpBySpeed
-			(
-				d->rotCurr.w,
-				(elapsedTimeMS << 5) >> 5,
-				0
-			);
-			
-		d->turnAngleCurr =
-			DECOMP_VehCalc_InterpBySpeed
-			(
-				d->turnAngleCurr,
-				(elapsedTimeMS << 7) >> 5,
-				0
-			);
-			
-	#ifdef USE_60FPS
-	}
-	#endif
+	d->rotCurr.w = 
+		DECOMP_VehCalc_InterpBySpeed
+		(
+			d->rotCurr.w,
+			(elapsedTimeMS << 5) >> 5,
+			0
+		);
+		
+	d->turnAngleCurr =
+		DECOMP_VehCalc_InterpBySpeed
+		(
+			d->turnAngleCurr,
+			(elapsedTimeMS << 7) >> 5,
+			0
+		);
 		
 	#ifndef REBUILD_PS1
 	VehPhysForce_RotAxisAngle(&d->matrixMovingDir, &d->AxisAngle1_normalVec[0], d->angle);
