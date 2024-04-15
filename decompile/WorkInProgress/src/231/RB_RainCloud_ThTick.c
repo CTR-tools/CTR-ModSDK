09f2adbf9db05561d6d6ebcc614de7c01da63b81
Niko <a>
2024-04-15 15:48:00 -0700

    progress

--- decompile/WorkInProgress/src/231/RB_RainCloud_ThTick.c
+++ decompile/WorkInProgress/src/231/RB_RainCloud_ThTick.c
@@ -11,15 +11,15 @@ void DECOMP_RB_RainCloud_ThTick(struct Thread* t)
   struct RainCloud* rcloud;
   struct Instance* parentInst;
   
-  inst = t->inst;
+  struct GameTracker* gGT = sdata->gGT;
   
+  inst = t->inst;
   rcloud = t->object;
   
   animFrame = inst->animFrame;
   
   // get player who put the potion
   parent = t->parentThread->object;
-  
   parentInst = t->parentThread->inst;
   
   numFrames = INSTANCE_GetNumAnimFrames(inst,0);
@@ -54,7 +54,7 @@ void DECOMP_RB_RainCloud_ThTick(struct Thread* t)
     if (*(short *)(rcloud + 4) != 0) 
 	{
 	  // reduce by elapsed time
-      reduce = (int)*(short *)(rcloud + 4) - sdata->gGT->elapsedTimeMS;
+      reduce = (int)*(short *)(rcloud + 4) - gGT->elapsedTimeMS;
       *(short *)(rcloud + 4) = (short)reduce;
       
 	  // solve for negatives
@@ -71,18 +71,28 @@ void DECOMP_RB_RainCloud_ThTick(struct Thread* t)
         return;
       }
 	  
-	  // at this point, you must have a weapon
-	  
+	  // This is if weapo nwas already fired,
+	  // but is flickering in the UI
       if (parent->noItemTimer != 0) {
         return;
       }
+	  
+	  
+	  // === Have weapon, have not used it yet ===
+	  
+	  
+	  // === Naughty Dog Bug fixed ===
+	  // Allow warpball to be found in itemRNG
+	  // if the warpball "was" held, then lost
+	  if(parent->heldItemID == 9)
+		  gGT->gameMode1 &= ~(WARPBALL_HELD);
 	  
 	  // set weapon to "weapon roulette" to make it spin
       parent->heldItemID = 0x10;
 	  
 	  // you are always 5 frames away from new weapon,
 	  // so you get weapon 5 frames after cloud dies
-      parent->itemRollTimer = 5;
+      parent->itemRollTimer = FPS_DOUBLE(5);
 	  
 	  // you hold zero of this item
       parent->numHeldItems = 0;
