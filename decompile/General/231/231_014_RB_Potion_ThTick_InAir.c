087c907cb2905ea197488400962efb60f5b199b3
Niko <a>
2024-04-21 10:20:55 -0700

    progress

--- decompile/General/231/231_014_RB_Potion_ThTick_InAir.c
+++ decompile/General/231/231_014_RB_Potion_ThTick_InAir.c
@@ -60,7 +60,7 @@ void DECOMP_RB_Potion_ThTick_InAir(struct Thread* t)
 
 	RB_MakeInstanceReflective(SPS, inst);
 
-	// 1f800108+1A4
+  // if hit a wall
 	if ((*(int*)&SPS->dataOutput[0] & 4) != 0) 
 	{
 		RB_GenericMine_ThDestroy(t,inst,mw);
