b32100df571b0985731fcd2c4d2e25bce409167e
Niko <a>
2024-04-23 23:46:24 -0700

    fix tracker lock-on (again)

--- decompile/General/231/231_055_RB_GetThread_ClosestTracker.c
+++ decompile/General/231/231_055_RB_GetThread_ClosestTracker.c
@@ -18,28 +18,29 @@ struct Thread* DECOMP_RB_GetThread_ClosestTracker(struct Driver* d)
 		currThread = currThread->siblingThread
 	) 
   {
-	// if first item in thread's object is param_1,
-	// meaning, if tracker is chasing this driver
-    if (currThread->object == d)
-	{
-		struct Instance* dInst = d->instSelf;
-		struct Instance* currInst = currThread->inst;
+	struct TrackerWeapon* tw = 
+		currThread->object;
+	  
+    if (tw->driverTarget != d)
+		continue;
+	
+	struct Instance* dInst = d->instSelf;
+	struct Instance* currInst = currThread->inst;
 		
-		if(
-			// get distance between posX and posZ of 
-			// driver->instSelf->position, and tracker's position, 
-			distX = dInst->matrix.t[0] - currInst->matrix.t[0],
-			distZ = dInst->matrix.t[2] - currInst->matrix.t[2],
-			
-			// if this is a new closest distance
-			distX = distX * distX + distZ * distZ, distX < smallestDist
-		  )
-	   
-		{
-			// save closest distance
-			smallestDist = distX;
-			closestTh = currThread;
-		}
+	if(
+		// get distance between posX and posZ of 
+		// driver->instSelf->position, and tracker's position, 
+		distX = dInst->matrix.t[0] - currInst->matrix.t[0],
+		distZ = dInst->matrix.t[2] - currInst->matrix.t[2],
+		
+		// if this is a new closest distance
+		distX = distX * distX + distZ * distZ, distX < smallestDist
+	  )
+	
+	{
+		// save closest distance
+		smallestDist = distX;
+		closestTh = currThread;
 	}
   }
   // return thread of closest tracker
