7519924e353594125177320e1439af113c9d40c2
Niko <a>
2024-04-29 22:59:08 -0700

    fix 231 globals

--- decompile/General/231/231_GROUP_55_72.c
+++ decompile/General/231/231_GROUP_55_72.c
@@ -1,12 +1,82 @@
+
+// NOT ready to defrag full region until globals are in
+#if 0
+
 #include "231_055_RB_GetThread_ClosestTracker.c"
 
 // This came between 055 and 056, 
 // even in OG game
 
+// 800b295c (OG addr)
+short tntSitArr[] =
+{
+	
+}
+
+// 800b2ac4 (OG addr)
+short tntThrowArr[] =
+{
+	
+}
+
+// 800b2ae4 (OG addr)
+struct ParticleEmitter emSet_Missile[] =
+{
+	
+}
+
+// 800b2c88 (OG addr)
+short warpballScaleArr[] =
+{
+	
+}
+
+// 800b2cac (OG addr)
+short warpballPosArr[] =
+{
+	
+}
+
+// 800b2cc4 (OG addr)
+short maskPosArr[] =
+{
+	
+}
+
+// 800b2d14 (OG addr)
+short bubbleScaleArr1[] =
+{
+	
+}
+
+// 800b2cf4 (OG addr)
+short bubbleScaleArr2[] =
+{
+	
+}
+
+// 800b2d40 (OG addr)
+short bubbleScaleArr3[] =
+{
+	
+}
+#endif
+
+
+
+// === WARNING ===
+// All the MinePool stuff is now double-allocated
+// because the block compiles to Baron instead of
+// RB_GetThread_ClosestTracker
+
+
+
+
 // OG game allocated 50 of these,
 // but only allowed up to 40 items,
 // causing Nitro Court Crystal Challenge bug.
 // Here, only allocate 40, save some RAM
+#include <common.h>
 struct WeaponSlot231 minePoolItem[40];
 struct LinkedList minePoolTaken;
 struct LinkedList minePoolFree;
