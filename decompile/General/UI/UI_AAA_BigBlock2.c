3ccd34a7e0767729e57218201e5fb47bbe8bc850
Niko <a>
2024-05-06 02:18:40 -0700

    UI defrag

--- decompile/General/UI/UI_AAA_BigBlock2.c
+++ decompile/General/UI/UI_AAA_BigBlock2.c
@@ -1,5 +1,9 @@
 #include <common.h>
 
+#include "UI_DrawDriverIcon.c"
+#include "UI_Lerp2D_Angular.c"
+#include "UI_Lerp2D_HUD.c"
+#include "UI_Lerp2D_Linear.c"
 #include "UI_DrawRaceClock.c"
 #include "UI_DrawLimitClock.c"
 
