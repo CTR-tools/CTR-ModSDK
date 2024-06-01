432ce0b144d1dff275587a85298e4a8d6b5e3375
Niko <a>
2024-06-01 00:02:21 -0400

    June progress

--- decompile/General/GHOST/GhostTape_Start.c
+++ decompile/General/GHOST/GhostTape_Start.c
@@ -8,8 +8,8 @@ void DECOMP_GhostTape_Start(void)
   
   d = gGT->drivers[0];
 
-  // v1 - PizzaHut (June)
-  // v4 - Retail (September)
+  // v1 - PizzaHut (June), Spyro2 (July)
+  // v4 - Aug5, Aug14, Sep3, Retail
 
   gh = sdata->GhostRecording.ptrGhost;
   gh->version = -4;
