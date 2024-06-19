3b47d058460c7501ca51ced5c8afbfa634106379
mateusfavarin <mateusfavarinc@gmail.com>
2024-06-19 00:52:32 -0300

    Rewrite reserve bar mod

--- decompile/General/AltMods/Mods4.c
+++ decompile/General/AltMods/Mods4.c
@@ -1,107 +1,76 @@
-// used for boost bar (part 1)
+#include <common.h>
+#include <redhot/gpu.h>
+#include <redhot/time.h>
 
 #ifdef USE_BOOSTBAR
 void DrawBoostBar(short posX, short posY, struct Driver* driver)
 {
-	// Copies code from UI_DrawSlideMeter
-	
-	struct GameTracker *gGT;
-	u_int colorAndCode;
-	int maxRoom;
-	struct DB* backDB;
-	POLY_F4 *p;
-	u_long* ptrOT;
-	u_long* primmemCurr;
-	int fullWidth;
-	short fullHeight;
-	short meterLength;
+	struct GameTracker * gGT = sdata->gGT;
+
+	short fullHeight = 3;
+	int fullWidth = WIDE_PICK(0x31, 0x25);
+
+	short meterLength = ((driver->reserves * 0xE)/0x960);
+	if ((meterLength > fullWidth) || (driver->reserves < 0)) { meterLength = fullWidth; }
+
 	RECT box;
-	int currentRoomRemaining;
-	
-	gGT = sdata->gGT;
-
-	// === BoostBar ===
-	// always height of 3
-	fullHeight = 3;
-
-	// width of full bar
-	fullWidth = WIDE_PICK(0x31, 0x25);
-
-	// === BoostBar ===
-	// length depends on reserves
-	meterLength = ((driver->reserves * 0xE)/0x960);
-	if((meterLength > fullWidth) || (driver->reserves < 0))
-		meterLength = fullWidth;
-	
 	box.x = posX - fullWidth;
 	box.y = posY - fullHeight;
 	box.w = fullWidth;
 	box.h = fullHeight;
-	
-	backDB = gGT->backBuffer;
+
+	struct DB * backDB = gGT->backBuffer;
 	int boxColor = 0;
-	
+
 	DECOMP_CTR_Box_DrawWireBox(
-		&box, &boxColor, 
-		gGT->pushBuffer_UI.ptrOT, 
+		&box, &boxColor,
+		gGT->pushBuffer_UI.ptrOT,
 		&backDB->primMem);
-		
+
 	int topY = posY - fullHeight;
 
-	// === BoostBar ===
-	// red: 0-1599
-	// yellow: 1600-3839
-	// green: 3840-full
-	// blue: full-inf
-	// purple: inf
-	
-	colorAndCode = 0x280000ff; // red
-	if (driver->reserves >= 1600)
-		colorAndCode = 0x2800ffff; // yellow
-		if (driver->reserves >= 3840)
-			colorAndCode = 0x2800ff00; // green
+	/* === BoostBar ===
+		red: 0-2s
+		yellow: 2s-4s
+		green: 4s-full
+		blue: full-saffi
+		purple: saffi */
 
-	if (meterLength == fullWidth)
-		colorAndCode = 0x28ff0000; // blue
-		
-	if (driver->reserves < 0)
-		colorAndCode = 0x28ff00ff; // purple
+	PolyCode renderCode = (PolyCode){.quad = 1, .renderCode = RENDER_CODE_POLYGON};
+	ColorCode colorCode = MakeColorCode(0xFF, 0, 0, renderCode); // red
 
-	for(int i = 0; i < 2; i++)
+	if (driver->reserves < 0) {
+		colorCode = MakeColorCode(0xFF, 0x0, 0xFF, renderCode); // purple
+	}
+	else if (meterLength == fullWidth) {
+		colorCode = MakeColorCode(0, 0, 0xFF, renderCode); // blue
+	}
+	else if (driver->reserves >= SECONDS(4)) {
+		colorCode = MakeColorCode(0, 0xFF, 0, renderCode); // green
+	}
+	else if (driver->reserves >= SECONDS(2)) {
+		colorCode = MakeColorCode(0xFF, 0xFF, 0, renderCode); // yellow
+	}
+
+	for (int i = 0; i < 2; i++)
 	{
-		primmemCurr = backDB->primMem.curr;
-		p = 0;
-	
-		// if there is room remaining to draw
-		if (primmemCurr <= (u_long *)backDB->primMem.endMin100)
-		{
-			p = (POLY_F4 *)primmemCurr;
-			backDB->primMem.curr = p+1;
-		}
-	
-		if (p == 0)
-			return;
-	
-		*(int*)&p->r0 = colorAndCode;
-		
-		p->y0 = topY;
-		p->y1 = topY;
-		p->y2 = posY;
-		p->y3 = posY;
-		
-		p->x0 = posX - meterLength;
-		p->x1 = posX;
-		p->x2 = posX - meterLength;
-		p->x3 = posX;
-	
-		ptrOT = gGT->pushBuffer_UI.ptrOT;
-		*(int*)p = *ptrOT | 0x5000000;
-		*ptrOT = (u_int)p & 0xffffff;
-		
+		PolyF4 * p;
+		GetPrimitiveMem(p, PolyF4, backDB->primMem.curr, backDB->primMem.endMin100)
+		if (p == nullptr) { return; }
+
+		p->colorCode = colorCode;
+		p->v[0].pos.x = posX - meterLength;
+		p->v[0].pos.y = topY;
+		p->v[1].pos.x = posX;
+		p->v[1].pos.y = topY;
+		p->v[2].pos.x = posX - meterLength;
+		p->v[2].pos.y = posY;
+		p->v[3].pos.x = posX;
+		p->v[3].pos.y = posY;
+		AddPrimitive(p, gGT->pushBuffer_UI.ptrOT);
+
 		// Gray color for Prim #2
-		colorAndCode = 0x28808080;
-		
-		// full length of meter
+		colorCode = MakeColorCode(0x80, 0x80, 0x80, renderCode);
 		meterLength = fullWidth;
 	}
 }
---
+++ include/redhot/time.h
@@ -0,0 +1,8 @@
+#ifndef REDHOT_TIME_H
+#define REDHOT_TIME_H
+
+#define FRAME 32
+#define FPS 30
+#define SECONDS(x) (((float)x) * FRAME * FPS)
+
+#endif
