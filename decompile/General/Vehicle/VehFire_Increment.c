2b4a76bbbddffabbb9e3eface9909fa145f9bdee
mateusfavarin <mateusfavarinc@gmail.com>
2024-06-28 22:30:11 -0300

    fix saffi, changereserve counter to second counter

--- decompile/General/Vehicle/VehFire_Increment.c
+++ decompile/General/Vehicle/VehFire_Increment.c
@@ -8,24 +8,24 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 {
 	char kartState;
 	u_char count;
-	
+
 	int newFireSpeedCap;
 	int newFireSize;
 	int oldOTT;
-	
+
 	u_int addFlags;
 	struct Turbo* turboObj;
 	struct Thread* turboThread;
 	struct Instance* turboInst1;
 	struct Instance* turboInst2;
-	
-	struct GameTracker* gGT = sdata->gGT;
 
+	struct GameTracker* gGT = sdata->gGT;
+	printf("reserves: %d %X\n", reserves, &driver->reserves);
 	if
 	(
 		// if this is a turbo pad
 		((type & 4) != 0) &&
-		
+
 		// racer is in accel prevention (holding square)
 		((driver->actionsFlagSet & 8) != 0)
 	)
@@ -64,7 +64,7 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 
 	// check all turbo threads
 	while(turboThread != 0)
-	{	
+	{
 		// if this turbo thread is owned by this driver
 		if (((struct Turbo*)turboThread->object)->driver == driver)
 		{
@@ -75,7 +75,7 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 		// next turbo thread in bucket
 		turboThread = turboThread->siblingThread;
 	}
-	
+
 	// if no turbo exists, create one
 	if(turboThread == 0)
 	{
@@ -104,64 +104,64 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 		}
 
 		#endif
-		
+
 #ifndef REBUILD_PS1
 		turboInst1 = INSTANCE_BirthWithThread(
-			0x2c, 0, SMALL, TURBO, 
+			0x2c, 0, SMALL, TURBO,
 			DECOMP_VehTurbo_ThTick,
 			sizeof(struct Turbo), 0
 		);
-	
+
 		turboObj = 0;
-	
+
 		if (turboInst1 != 0)
 		{
 			// get thread, ignore all collisions
 			turboThread = turboInst1->thread;
 			turboThread->flags |= 0x1000;
 			turboThread->funcThDestroy = DECOMP_VehTurbo_ThDestroy;
-			
+
 			// turbo #2
 			turboInst2 = INSTANCE_Birth3D(
 				gGT->modelPtr[0x2C], 	// model
 				&sdata->s_turbo2[0], 		// name
 				turboThread					// parent thread
 			);
-			
+
 			// get object, set essentials
 			turboObj = turboThread->object;
 			turboObj->inst = turboInst2;
 			turboObj->driver = driver;
 			turboObj->fireAnimIndex = 0;
 			turboObj->fireVisibilityCooldown = 0x60;
-			
+
 			// make flame disappear after
 			// 	- powerslide: two frames (quick death)
 			//	- all others: -1 frames (255 = 'no' death)
 			if (type & 2)	count = FPS_DOUBLE(2);
 			else			count = -1;
 			turboObj->fireDisappearCountdown = count;
-	
+
 			// if modelIndex == "player" of any kind
 			if (driver->instSelf->thread->modelIndex == 0x18)
 			{
 				turboObj->fireAudioDistort = 0;
-				
+
 				if(driver->kartState != KS_CRASHING)
 				{
 					DECOMP_VehFire_Audio(driver, fireLevel);
 				}
 			}
-	
+
 			// 2P 3P 4P flags
 			addFlags = 0;
-	
+
 			// 1P flags
 			if (gGT->numPlyrCurrGame == 1)
 			{
 				addFlags = 0x2000000;
 			}
-	
+
 			// make turbos invisible, and transparent.
 			// why does numPlyrCurrGame alter pause-menu invisible toggle?
 			turboInst1->flags = turboInst1->flags | addFlags | 0x1040080;
@@ -178,37 +178,37 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 	{
 		// get the turbo's object
 		turboObj = turboThread->object;
-	
+
 		// get the turbo's instances
 		turboInst1 = turboThread->inst;
 		turboInst2 = turboObj->inst;
-	
+
 		// remove "dead thread" flag
 		turboThread->flags &= 0xfffff7ff;
-	
+
 		// turbo pad
 		if ((type & 4) != 0)
 		{
 			// only increase counter on the first frame of turbo pad
-			
+
 			if ((driver->actionsFlagSetPrevFrame & 0x200000) == 0)
 			{
 				driver->numTurbos++;
-				
+
 				#if BUILD == JpnRetail
 				// the japanese version of the game keeps track of your highest turbo chain in a race
 				if (driver->numTurbosHighScore < driver->numTurbos && (gGT->gameMode1 & END_OF_RACE) == 0) driver->numTurbosHighScore = driver->numTurbos;
 				#endif
 			}
 		}
-	
+
 		// all other boosts
 		else
 		{
 			// make fire invisible for the sake of the visibility cooldown as explained in common.h
 			turboInst1->flags |= 0x1000080;
 			turboInst2->flags |= 0x1000080;
-	
+
 			turboObj->fireVisibilityCooldown = 0x60;
 			driver->numTurbos++;
 			#if BUILD == JpnRetail
@@ -216,22 +216,22 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 			if (driver->numTurbosHighScore < driver->numTurbos && (gGT->gameMode1 & END_OF_RACE) == 0) driver->numTurbosHighScore = driver->numTurbos;
 			#endif
 		}
-	
+
 		turboObj->fireDisappearCountdown = -1;
 		turboInst1->alphaScale = 0;
 		turboInst2->alphaScale = 0;
-	
+
 		// driver -> instance -> thread -> modelIndex == "player" of any kind
 		if(*(short *)&driver->instSelf->thread->modelIndex == 0x18)
 		{
 			if
 			(
-				// if racer is not getting an Outside turbo (turbo pad or powerup), 
-				// this prevents audio-spam from multiple boosts  
+				// if racer is not getting an Outside turbo (turbo pad or powerup),
+				// this prevents audio-spam from multiple boosts
 				((driver->actionsFlagSet & 0x200000) == 0) ||
 				((driver->actionsFlagSetPrevFrame & 0x200000) == 0)
 			)
-			
+
 			{
 				turboObj->fireAudioDistort = 0;
 				DECOMP_VehFire_Audio(driver, fireLevel);
@@ -239,23 +239,23 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 		}
 	}
 
-	newFireSpeedCap = 
-	
+	newFireSpeedCap =
+
 		(int)driver->const_SingleTurboSpeed +
-		
+
 		// fireLevel * 8
 		(
-			fireLevel * 
-			
+			fireLevel *
+
 			#if 1
-			
+
 			8
-			
+
 			#else
-				
+
 			// this can all be simplified to: 8
 			((int)driver->const_SacredFireSpeed - (int)driver->const_SingleTurboSpeed) >> 8
-			
+
 			#endif
 		);
 
@@ -283,7 +283,7 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 			((driver->stepFlagSet & 2) == 0)
 		)
 	)
-	
+
 	{
 		driver->fireSpeedCap = (short)newFireSpeedCap;
 
@@ -317,22 +317,32 @@ void DECOMP_VehFire_Increment(struct Driver* driver, int reserves, u_int type, i
 	else if (!(type & 1))
 	{
 		// increase reserves BY param2
+		#ifdef USE_ONLINE
+		if (driver->reserves > 30000) { driver->uncappedReserves += reserves; }
+		else { driver->reserves += reserves; }
+		#else
 		driver->reserves += reserves;
+		#endif
 	}
-	
+
 	// turbo pad, boost powerup
 	else
 	{
 		// this adds reserves on the first frame you touch the turbo pad,
-		// then prevent reserves from decreasing until the first frame 
+		// then prevent reserves from decreasing until the first frame
 		// you leave the turbo pad
-		
+
 		oldOTT = driver->turbo_outsideTimer;
-		
+
 		if (oldOTT < reserves)
 		{
-			driver->turbo_outsideTimer += 	(reserves - oldOTT);
+			#ifdef USE_ONLINE
+			if (driver->reserves > 30000) { driver->uncappedReserves += (reserves - oldOTT); }
+			else { driver->reserves += (reserves - oldOTT); }
+			#else
 			driver->reserves += 			(reserves - oldOTT);
+			#endif
+			driver->turbo_outsideTimer += 	(reserves - oldOTT);
 		}
 	}
 
