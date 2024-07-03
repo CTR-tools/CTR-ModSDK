ce4392d1a7b528a066faac7dfd34c2b90ebc063f
TheUbMunster <theubmunster@gmail.com>
2024-07-03 12:13:01 -0600

    comment out ps1 logging, new plan for concurrent reads & writes via PINE.

--- mods/Windows/OnlineCTR/Network_PC/Client/DSPINE.h
+++ mods/Windows/OnlineCTR/Network_PC/Client/DSPINE.h
@@ -22,12 +22,12 @@
 //information, that said, these comments reflect my observations.
 
 //I believe this is how it's actually formatted (at least for DSPINEMsgRead64):
-//         Packet size (the literal value 10 occupies these 4 bytes) (9 bytes once the ds bug is fixed)
+//         Packet size (the literal value 9 occupies these 4 bytes)
 //         |           IPC Message Event (1 byte)
 //         |           |  Address (4 bytes)
-//         |           |  |           Empty (duckstation has a bug, and an unused byte at the end is required).
-//         |           |  |           |
-// format: XX XX XX XX YY ZZ ZZ ZZ ZZ ??
+//         |           |  |
+//         |           |  |
+// format: XX XX XX XX YY ZZ ZZ ZZ ZZ
 //         Packet size (the literal value 13 occupies this 1 byte).
 //         |           Reply code (00 if success, FF if fail)
 //         |           |  Memory at that address
@@ -35,13 +35,13 @@
 // reply:  XX XX XX XX YY ZZ ZZ ZZ ZZ ZZ ZZ ZZ ZZ
 
 //I believe this is how it's actually formatted (at least for DSPINEMsgWrite64):
-//         Packet size (the literal value 18 occupies these 4 bytes) (17 bytes once the ds bug is fixed)
+//         Packet size (the literal value 17 occupies these 4 bytes)
 //         |           IPC Message Event (1 byte)
 //         |           |  Address (4 bytes)
 //         |           |  |           The value to write to memory
-//         |           |  |           |                       Empty (duckstation has a bug, and an unused byte at the end is required).
-//         |           |  |           |                       |
-// format: VV VV VV VV WW XX XX XX XX YY YY YY YY YY YY YY YY ??
+//         |           |  |           |
+//         |           |  |           |
+// format: VV VV VV VV WW XX XX XX XX YY YY YY YY YY YY YY YY
 //         Packet size (the literal value 5 occupies this 1 byte).
 //         |           Reply code (00 if success, FF if fail)
 //         |           |
@@ -64,3 +64,93 @@
 #define DSPINEMsgGameVersion 0xE    /**< Returns the game verion. */
 #define DSPINEMsgStatus 0xF         /**< Returns the emulator status. */
 #define DSPINEMsgUnimplemented 0xFF /**< Unimplemented IPC message. */
+
+// Only struct impl for Read64, Write8/16/32/64, no more were needed at time of writing.
+
+#pragma pack(push, 1) //api requires packed, this pragma works for VC++ and GCC, maybe some others
+struct DSPINERead64Send
+{
+	unsigned int packetSize = sizeof(DSPINERead64Send); //should be sizeof(DSPINERead64Send) (9)
+	unsigned char DSPINEMsgIPC = DSPINEMsgRead64; //one of the "DSPINEMsg" defines in DSPINE.h
+	unsigned int address;
+};
+
+struct DSPINERead64Recv
+{
+	unsigned int packetSize = sizeof(DSPINERead64Recv); //should be sizeof(DSPINERead64Recv) (13)
+	unsigned char DSPINEMsgReplyCode; //0x00 if success, 0xFF if failure
+	union
+	{
+		unsigned long long whole;
+		char bytes[8];
+	} data;
+};
+
+struct DSPINEWrite8Send
+{
+	unsigned int packetSize = sizeof(DSPINEWrite8Send); //should be sizeof(DSPINEWrite8Send) (10)
+	unsigned char DSPINEMsgIPC = DSPINEMsgWrite8; //one of the "DSPINEMsg" defines in DSPINE.h
+	unsigned int address;
+	unsigned char data;
+};
+
+struct DSPINEWrite8Recv
+{
+	unsigned int packetSize = sizeof(DSPINEWrite8Recv); //should be sizeof(DSPINEWrite8Recv) (5)
+	unsigned char DSPINEMsgReplyCode; //0x00 if success, 0xFF if failure
+};
+
+struct DSPINEWrite16Send
+{
+	unsigned int packetSize = sizeof(DSPINEWrite16Send); //should be sizeof(DSPINEWrite16Send) (11)
+	unsigned char DSPINEMsgIPC = DSPINEMsgWrite16; //one of the "DSPINEMsg" defines in DSPINE.h
+	unsigned int address;
+	union
+	{
+		unsigned short whole;
+		char bytes[2];
+	} data;
+};
+
+struct DSPINEWrite16Recv
+{
+	unsigned int packetSize = sizeof(DSPINEWrite16Recv); //should be sizeof(DSPINEWrite16Recv) (5)
+	unsigned char DSPINEMsgReplyCode; //0x00 if success, 0xFF if failure
+};
+
+struct DSPINEWrite32Send
+{
+	unsigned int packetSize = sizeof(DSPINEWrite32Send); //should be sizeof(DSPINEWrite32Send) (13)
+	unsigned char DSPINEMsgIPC = DSPINEMsgWrite32; //one of the "DSPINEMsg" defines in DSPINE.h
+	unsigned int address;
+	union
+	{
+		unsigned int whole;
+		char bytes[4];
+	} data;
+};
+
+struct DSPINEWrite32Recv
+{
+	unsigned int packetSize = sizeof(DSPINEWrite32Recv); //should be sizeof(DSPINEWrite32Recv) (5)
+	unsigned char DSPINEMsgReplyCode; //0x00 if success, 0xFF if failure
+};
+
+struct DSPINEWrite64Send
+{
+	unsigned int packetSize = sizeof(DSPINEWrite64Send); //should be sizeof(DSPINEWrite64Send) (17)
+	unsigned char DSPINEMsgIPC = DSPINEMsgWrite64; //one of the "DSPINEMsg" defines in DSPINE.h
+	unsigned int address;
+	union
+	{
+		unsigned long long whole;
+		char bytes[8];
+	} data;
+};
+
+struct DSPINEWrite64Recv
+{
+	unsigned int packetSize = sizeof(DSPINEWrite64Recv); //should be sizeof(DSPINEWrite64Recv) (5)
+	unsigned char DSPINEMsgReplyCode; //0x00 if success, 0xFF if failure
+};
+#pragma pack(pop)
