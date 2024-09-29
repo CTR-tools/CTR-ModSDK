d02a8c27238cadc7b643ab94b5dc4eb032a735c0
TheUbMunster <theubmunster@gmail.com>
2024-09-29 08:25:56 -0600

    collisions no longer happen in itemless rooms.

--- decompile/General/PROC/PROC_CollidePointWithBucket.c
+++ decompile/General/PROC/PROC_CollidePointWithBucket.c
@@ -7,14 +7,11 @@ void Online_CollidePointWithBucket(struct Thread* th, short* vec3_pos);
 
 void DECOMP_PROC_CollidePointWithBucket(struct Thread* th, short* vec3_pos)
 {
-	struct Thread* other;
-
 	// only used with drivers colliding
 	// with other drivers, disabled online
-#if defined(USE_ONLINE) //this puts us over the byte budget by 4 bytes, but idk how to shrink it.
+#if defined(USE_ONLINE)
 	Online_CollidePointWithBucket(th, vec3_pos);
-#endif
-//#if !defined(USE_ONLINE) //this may not be a valid optimization, leaving it enabled for now.
+#else
 	while(th != 0)
 	{
 		DECOMP_PROC_CollidePointWithSelf(th, vec3_pos);
@@ -22,5 +19,5 @@ void DECOMP_PROC_CollidePointWithBucket(struct Thread* th, short* vec3_pos)
 		// next
 		th = th->siblingThread;
 	}
-//#endif
+#endif
 }
