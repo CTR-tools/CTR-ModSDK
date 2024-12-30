067881062bc39091732de4e36449b8525cf67a54
TheUbMunster <theubmunster@gmail.com>
2024-12-29 19:59:52 -0700

    working on decomp

--- decompile/General/BOTS/BOTS_06_SetRotation.c
+++ decompile/General/BOTS/BOTS_06_SetRotation.c
@@ -1,26 +1,61 @@
 #include <common.h>
 
-void BOTS_SetRotation(struct Driver* param_1, short param_2) //UNTESTED
+void DECOMP_BOTS_SetRotation(struct Driver* bot, short param_2) //UNTESTED
 {
-	//1st param may be `struct Driver*`
-	//2nd param may be `short`
-	//TODO
-	struct NavFrame* nf = param_1->botNavFrame;
+	struct NavFrame* nf = bot->botNavFrame;
 
-	(*(int*)&param_1->unk5bc[0x28]) = 0;
-	(*(int*)&param_1->unk5bc[0x2C]) = 0;
-	(*(int*)&param_1->unk5bc[0x30]) = 0;
-	param_1->estimatePos[0] = (short)(((u_int)param_1->posCurr.x) >> 8);
-	param_1->estimatePos[1] = (short)(((u_int)param_1->posCurr.y) >> 8);
-	param_1->estimatePos[2] = (short)(((u_int)param_1->posCurr.z) >> 8);
-	int dx = nf->pos[0] - param_1->estimatePos[0];
-	int dy = nf->pos[1] - param_1->estimatePos[1];
-	int dz = nf->pos[2] - param_1->estimatePos[2];
-	int xzDist = SquareRoot0(dx * dx + dz * dz);
-	param_1->distToNextNavXZ = xzDist;
-	int xyzDist = SquareRoot0(dx * dx + dy * dy + dz * dz);
-	param_1->distToNextNavXYZ = xyzDist;
+	(*(int*)&bot->unk5bc[0x28]) = 0;
+	(*(int*)&bot->unk5bc[0x2C]) = 0;
+	(*(int*)&bot->unk5bc[0x30]) = 0;
 
-	//there's more to this function, but at this point I realized that there's already an entry
-	//in /WIP for this file. Whoops
+	// ======== Get Driver Position =============
+
+	bot->estimatePos[0] = (short)(((u_int)bot->posCurr.x) >> 8);
+	bot->estimatePos[1] = (short)(((u_int)bot->posCurr.y) >> 8);
+	bot->estimatePos[2] = (short)(((u_int)bot->posCurr.z) >> 8);
+
+	// ======== Compare to Nav Position =============
+
+	int dx = nf->pos[0] - bot->estimatePos[0];
+	int dy = nf->pos[1] - bot->estimatePos[1];
+	int dz = nf->pos[2] - bot->estimatePos[2];
+
+	// ======== Calculate Distance =============
+
+	//xz dist from driver to nav
+	int xzDist = SquareRoot0_stub(dx * dx + dz * dz);
+	bot->distToNextNavXZ = xzDist;
+	//xyz distance from driver to nav
+	int xyzDist = SquareRoot0_stub(dx * dx + dy * dy + dz * dz);
+	bot->distToNextNavXYZ = xyzDist;
+
+	// ======== Calculate Rotation =============
+
+	int rot = ratan2(dy * 0x1000, bot->distToNextNavXZ * 0x1000);
+	bot->estimateRotCurrY = rot >> 4;
+	bot->unk5a8 = 0;
+
+	// "if BOTS_ThTick_Drive or BOTS_Driver_Convert"
+	if (param_2 == 0)
+	{
+		bot->estimateRotNav[0] = nf->rot[0];
+		rot = ratan2(-dx, -dz);
+		bot->estimateRotNav[1] = ((rot + 0x800) >> 4);
+		bot->estimateRotNav[2] = nf->rot[1];
+	}
+	else
+	{
+		bot->estimateRotNav[1] = (char)((sdata->gGT->level1->DriverSpawn[0].rot[1] + 0x400) >> 4);
+	}
+
+	short v = bot->estimateRotNav[1] << 4;
+
+	//why does the Driver class have so many ways to store y rotation >:(
+	bot->ai_rotY_608 = v;
+	bot->angle = v;
+	bot->rotCurr.y = v;
+	bot->rotPrev.y = v;
+	bot->ai_rot4[1] = v;
+
+	bot->botFlags |= 1;
 }
