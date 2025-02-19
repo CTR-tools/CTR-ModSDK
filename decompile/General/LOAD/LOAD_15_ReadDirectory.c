d270428322dcb20ed2f3f820fa0ae61c4d73c9ac
Niko <a>
2025-02-19 02:16:27 -0500

    PCDRV size fix

--- decompile/General/LOAD/LOAD_15_ReadDirectory.c
+++ decompile/General/LOAD/LOAD_15_ReadDirectory.c
@@ -37,7 +37,7 @@ void* DECOMP_LOAD_ReadDirectory(char* filename)
 		v1 = PCopen("BIGFILE.BIG", PCDRV_MODE_READ);
 		sdata->fd_bigfile = v1;
 		
-		v1 = PCread(v1, bh, 0x80);
+		v1 = PCread(v1, bh, 0x4000);
 		printf("PCread: %d\n", v1);
 		
 	#endif
