ead585bcf9a42c5dab6fa086bea031f1b42dbf7b
TheUbMunster <theubmunster@gmail.com>
2025-02-23 01:58:36 -0700

    BOTS_GotoStartingLine

--- decompile/General/BOTS/BOTS_14_GotoStartingLine.c
+++ decompile/General/BOTS/BOTS_14_GotoStartingLine.c
@@ -1,7 +1,70 @@
 #include <common.h>
 
-void BOTS_GotoStartingLine(int param_1) //TODO: & add "DECOMP_" prefix
+void DECOMP_BOTS_GotoStartingLine(struct Driver* d)
 {
-	//1st param may be `int`, but is almost certainly a `struct*` of some kind
-	//TODO
+	int accelDuration;
+	short rotY;
+
+	sdata->unk_counter_upTo450 = 0;
+
+	VehBirth_TeleportSelf(d, 3, 0);
+
+	// get position where driver should spawn
+	char spawnPos = sdata->kartSpawnOrderArray[d->driverID];
+
+	//all within unk5bc
+	*(int*)(d + 0x5ec) = 0;
+	*(int*)(d + 0x5e8) = 0;
+	*(int*)(d + 0x5e4) = 0;
+	*(int*)(d + 0x5e0) = 0;
+	*(int*)(d + 0x5dc) = 0;
+	*(int*)(d + 0x5d8) = 0;
+	*(int*)(d + 0x5d4) = 0;
+	*(int*)(d + 0x5d0) = 0;
+	*(int*)(d + 0x5cc) = 0;
+
+	d->ai_posBackup[0] = d->posCurr.x;
+	d->ai_posBackup[1] = d->posCurr.y;
+	d->ai_posBackup[2] = d->posCurr.z;
+
+	d->unk5a8 = 0;
+
+	// current navFrame pointer, first navFrame on path
+	d->botNavFrame = sdata->NavPath_ptrNavFrameArray[d->botPath];
+
+	BOTS_SetRotation(d, 1);
+
+	// time until full acceleration from start
+	accelDuration = sdata->AI_AccelFrameCount;
+
+	// get acceleration order from spawn order
+	char accel = sdata->accelerateOrder[spawnPos];
+
+	d->rotCurr.z = 0;
+	d->rotPrev.z = 0;
+	d->ai_rot4[2] = 0;
+	d->rotCurr.x = 0;
+	d->rotPrev.x = 0;
+	d->ai_rot4[0] = 0;
+	d->turnAngleCurr = 0;
+
+	// turn on 21st flag of actions flag set, means driver is AI
+	d->actionsFlagSet |= 0x100000;
+
+	// calculate Y rotation
+	rotY = (short)d->estimateRotNav[1] << 4;
+
+	// every possible Y rotation
+	d->ai_rotY_608 = rotY;
+	d->angle = rotY;
+	d->rotCurr.y = rotY;
+	d->rotPrev.y = rotY;
+	d->ai_rot4[1] = rotY;
+
+	// acceleration from start-line to full speed
+	d->botAccel = accelDuration * accel;
+
+	// cooldown before next weapon
+	int rng = RngDeadCoed(&sdata->const_0x30215400);
+	d->weaponCooldown = (rng & 0xff) + 300;
 }
