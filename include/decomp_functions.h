414c870711a3b4a173d2e089d7dccdcaf1d28ae3
TheUbMunster <theubmunster@gmail.com>
2025-03-06 01:22:13 -0700

    More BOTS_ThTick progress

--- include/decomp_functions.h
+++ include/decomp_functions.h
@@ -897,3 +897,4 @@ void DECOMP_UI_RaceEnd_GetDriverClock(struct Driver* d);
 void DECOMP_GAMEPAD_JogCon2(struct Driver* d, char val, short timeMS);
 void DECOMP_CAM_EndOfRace(struct CameraDC* cDC, struct Driver* d);
 u_int DECOMP_BOTS_ChangeState(struct Driver* driverVictim, int damageType, struct Driver* driverAttacker, int reason);
+void DECOMP_BOTS_Killplane(struct Thread* botThread);
