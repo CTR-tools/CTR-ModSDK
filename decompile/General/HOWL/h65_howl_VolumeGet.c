f80a86e414895dbbcd2803a57b16074cf7e9f579
Niko <a>
2025-03-08 02:06:47 -0500

    optimization

--- decompile/General/HOWL/h65_howl_VolumeGet.c
+++ decompile/General/HOWL/h65_howl_VolumeGet.c
@@ -1,11 +1,19 @@
 #include <common.h>
 
+// Shared between VolumeSet and Get
+extern int* settingsPtrArr[3];
+
 // real Naughty Dog name
 int DECOMP_howl_VolumeGet(int type)
 {
-	if(type == 0) return sdata->vol_FX;
-	if(type == 1) return sdata->vol_Music;
-	if(type == 2) return sdata->vol_Voice;
-	
-	return 0;
+	return *settingsPtrArr[type];
 }
+
+// Reminder, ONLY use sdata_static
+// for compile-time pointers, NEVER runtime
+int* settingsPtrArr[3] =
+{
+	&sdata_static.vol_FX,
+	&sdata_static.vol_Music,
+	&sdata_static.vol_Voice
+};
