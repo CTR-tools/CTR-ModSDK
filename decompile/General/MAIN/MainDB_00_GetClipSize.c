7b2894a5ad21b76c7481ef259c0cbb37946bf5fc
Niko <a>
2025-04-10 19:53:40 -0400

    Clip Buffer Size Optimizations

--- decompile/General/MAIN/MainDB_00_GetClipSize.c
+++ decompile/General/MAIN/MainDB_00_GetClipSize.c
@@ -4,6 +4,34 @@
 #define JAL(dest) (((unsigned long)dest & 0x3FFFFFF) >> 2 | 0xC000000)
 void RunHook226();
 
+static char clipSizeArr[20] =
+{
+	// from Discord testers, plus padding,
+	// multiply all these numbers by 100
+	
+	25,	// DINGO_CANYON
+	28,	// DRAGON_MINES
+	25,	// BLIZZARD_BLUFF
+	20,	// CRASH_COVE
+	20,	// TIGER_TEMPLE
+	18,	// PAPU_PYRAMID
+	25,	// ROO_TUBES
+	20,	// HOT_AIR_SKYWAY
+	25,	// SEWER_SPEEDWAY
+	24,	// MYSTERY_CAVES
+	25,	// CORTEX_CASTLE
+	30,	// N_GIN_LABS
+	22,	// POLAR_PASS
+	20,	// OXIDE_STATION
+	16,	// COCO_PARK
+	20,	// TINY_ARENA
+	28,	// SLIDE_COLISEUM
+	28,	// TURBO_TRACK
+
+	// padding
+	0, 0
+};
+
 int DECOMP_MainDB_GetClipSize(u_int levelID, int numPlyrCurrGame)
 {
 #if 0
@@ -25,12 +53,18 @@ int DECOMP_MainDB_GetClipSize(u_int levelID, int numPlyrCurrGame)
 			return 1050;
 	}
 	
-	if(levelID == SEWER_SPEEDWAY) 	return 1500;	// down from 6000
-	if(levelID == MYSTERY_CAVES) 	return 2500;	// same
-	if(levelID == MAIN_MENU_LEVEL) 	return 1; 		// down from 16
-	if(levelID == ADVENTURE_GARAGE)	return 4500; 	// down from 24000
-
-	return 3000;
+	
+	if (levelID >= CREDITS_CRASH) 		return 1;		// 44 - 64
+	if (levelID >= OXIDE_ENDING)		return 2700;	// 42, 43
+	if (levelID == NAUGHTY_DOG_CRATE)	return 1;		// 41
+	if (levelID == ADVENTURE_GARAGE)	return 4500; 	// 40
+	if (levelID == MAIN_MENU_LEVEL) 	return 1; 		// 39
+	if (levelID >= INTRO_RACE_TODAY)	return 2800;	// 30-38
+	if (levelID >= GEM_STONE_VALLEY)	return 2750;	// 25-29
+	if (levelID >= NITRO_COURT)			return 2400;	// 18-24
+	
+	// 0 - 17
+	return (int)clipSizeArr[levelID] * 100;
 }
 
 #if 0
