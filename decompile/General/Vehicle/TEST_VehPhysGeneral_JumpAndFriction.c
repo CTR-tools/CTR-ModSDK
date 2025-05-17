a789d7a195fa01e39cb03f17bc75e227eea443b6
Niko <a>
2025-05-17 17:52:02 -0400

    TEST works, _4_ has bounce bug

--- decompile/General/Vehicle/TEST_VehPhysGeneral_JumpAndFriction.c
+++ decompile/General/Vehicle/TEST_VehPhysGeneral_JumpAndFriction.c
@@ -30,31 +30,27 @@ void FUN_80060630(struct Thread* t, struct Driver* d)
   unsigned int uVar12;
   int iVar13;
   unsigned int uVar14;
+  char *puVar15;
   int param_1;
   int param_2;
   int param_3;
-  VECTOR movement;
+  char auStack_40 [40];
   
+  puVar15 = auStack_40;
   uVar12 = d;
   gte_SetRotMatrix(&d->matrixMovingDir);
-    
-  if (
-		(d->kartState != KS_DRIFTING) &&
-		((d->actionsFlagSet & 0x800000) == 0) &&
-		(d->reserves == 0)
-	 )
-  {
-    short ampTurnState = (short)d->ampTurnState >> 8;
-    if (ampTurnState < 0)
-    {
-      ampTurnState = -ampTurnState;
+  if (((*(char *)(uVar12 + 0x376) != '\x02') && ((*(unsigned int *)(uVar12 + 0x2c8) & 0x800000) == 0)) &&
+     (param_2 = 0, *(short *)(uVar12 + 0x3e2) == 0)) {
+    uVar1 = *(char *)(uVar12 + 0x43b);
+    iVar9 = (int)((unsigned int)*(unsigned short *)(uVar12 + 0xc0) << 0x10) >> 0x18;
+    if (iVar9 < 0) {
+      iVar9 = -iVar9;
     }
-	
-    param_1 = DECOMP_VehCalc_MapToRange(ampTurnState, 0, d->const_BackwardTurnRate, 0, d->const_TurnDecreaseRate);
-    
-	param_3 = d->baseSpeed;
-    
-	iVar9 = param_3;
+    *(int *)(puVar15 + 0x10) = (int)*(short *)(uVar12 + 0x43c);
+    param_1 = DECOMP_VehCalc_MapToRange(iVar9,0,uVar1,0, (int)*(short *)(uVar12 + 0x43c));
+    param_3 = (int)*(short *)(uVar12 + 0x39c);
+    param_2 = (unsigned int)*(unsigned short *)(uVar12 + 0x39c);
+    iVar9 = param_3;
     if (param_3 < 0) {
       iVar9 = -param_3;
     }
@@ -67,78 +63,45 @@ void FUN_80060630(struct Thread* t, struct Driver* d)
       param_1 = -param_1;
       sVar3 = sVar7;
     }
-    d->baseSpeed = d->baseSpeed + sVar3;
+    *(unsigned short *)(uVar12 + 0x39c) = *(unsigned short *)(uVar12 + 0x39c) + sVar3;
   }
-  
-  if (d->set_0xF0_OnWallRub != 0) 
-  {
-    if (d->baseSpeed > d->scrubMeta8)
-		d->baseSpeed = d->scrubMeta8;
-
-	if (d->baseSpeed < -d->scrubMeta8) 
-		d->baseSpeed = -d->scrubMeta8;
+  if (*(short *)(uVar12 + 0x3fe) != 0) {
+    if (*(short *)(uVar12 + 0x38a) < *(short *)(uVar12 + 0x39c)) {
+      *(unsigned short *)(uVar12 + 0x39c) = *(unsigned short *)(uVar12 + 0x38a);
+    }
+    if ((int)*(short *)(uVar12 + 0x39c) < -(int)*(short *)(uVar12 + 0x38a)) {
+      *(short *)(uVar12 + 0x39c) = -*(short *)(uVar12 + 0x38a);
+    }
   }
-  
-  movement.vx = d->velocity.x;
-  movement.vy = d->velocity.y;
-  movement.vz = d->velocity.z;
-  
+  *(unsigned int *)(puVar15 + 0x18) = *(unsigned int *)(uVar12 + 0x88);
+  *(unsigned int *)(puVar15 + 0x1c) = *(unsigned int *)(uVar12 + 0x8c);
   uVar14 = 0;
+  *(unsigned int *)(puVar15 + 0x20) = *(unsigned int *)(uVar12 + 0x90);
   iVar9 = 0;
-  
-  if ((d->actionsFlagSet & 1) == 0) 
-  {
-CHECK_FOR_ANY_JUMP:
-    // If you want to fire a weapon
-    if (((d->actionsFlagSet & 0x8000) != 0) &&
-
-        // If that weapon is a spring
-        (d->heldItemID == 5))
-    {      
-	  // Remove the request to fire a weapon, since we will use it now
-      d->actionsFlagSet &= ~(0x8000);
-	  
-      // if coyoteTimerMS has not expired, and cooldownMS is over
-      if ((d->jump_CoyoteTimerMS != 0) && (d->jump_CooldownMS == 0))
-	  {
-        // driver is now forced to jump
-        d->jump_ForcedMS = 0xa0;
-		
-        iVar9 = d->const_JumpForce * 9;
-        
-		if (iVar9 < 0) {
+  if ((*(unsigned int *)(uVar12 + 0x2c8) & 1) == 0) {
+LAB_80060ab0:
+    if (((*(unsigned int *)(uVar12 + 0x2c8) & 0x8000) != 0) && (*(char *)(uVar12 + 0x36) == '\x05')) {
+      *(unsigned int *)(uVar12 + 0x2c8) = *(unsigned int *)(uVar12 + 0x2c8) & 0xffff7fff;
+      if ((*(short *)(uVar12 + 0x3f4) != 0) && (*(short *)(uVar12 + 0x3f2) == 0)) {
+        *(unsigned short *)(uVar12 + 0x3f6) = 0xa0;
+        iVar9 = *(short *)(uVar12 + 0x418) * 9;
+        if (iVar9 < 0) {
           iVar9 = iVar9 + 3;
         }
-        d->jump_InitialVelY = (short)(iVar9 >> 2);
-		
-		// spring weapon sound
-        OtherFX_Play_Echo(9, 1, d->actionsFlagSet & 0x10000);
-		
-		d->jump_unknown = 0x180;
-        goto PROCESS_JUMP;
+        *(short *)(uVar12 + 0x3f8) = (short)(iVar9 >> 2);
+        DECOMP_OtherFX_Play_Echo(9,1,*(unsigned short *)(uVar12 + 0x2ca) & 1);
+        *(unsigned short *)(uVar12 + 0x3fa) = 0x180;
+        goto LAB_80060c30;
       }
-      d->noItemTimer = 0;
+      *(unsigned short *)(uVar12 + 0x3c) = 0;
     }
-	
-    // if not being forced to jump (turtles), this should cause the tiny jumps on top of walls.
-    if (d->forcedJump_trampoline == 0)
-    {
-      if (
-			// if driver left quadblock more than 0.16s ago
-			(d->jump_CoyoteTimerMS == 0) ||
-
-			// if haven't jumped in last 10 frames
-			(d->jump_TenBuffer == 0) ||
-
-			// jump_CooldownMS not over (so can't jump again)
-			(d->jump_CooldownMS != 0)
-		  )
-      {
-        if ((d->actionsFlagSet & 1) != 0) 
-		{
+    if (*(char *)(uVar12 + 0x366) == '\0') {
+      if (((*(short *)(uVar12 + 0x3f4) == 0) || (*(short *)(uVar12 + 0x3f0) == 0)) ||
+         (*(short *)(uVar12 + 0x3f2) != 0)) {
+        if ((*(unsigned int *)(uVar12 + 0x2c8) & 1) != 0) {
           if ((*(int *)(uVar12 + 0x350) != 0) &&
              (iVar9 = (int)*(char *)(*(int *)(uVar12 + 0x350) + 0x3b), iVar9 != 0)) {
-            iVar13 = (int)d->speedApprox;
+            iVar13 = (int)*(short *)(uVar12 + 0x38e);
             if (iVar13 < 0) {
               iVar13 = -iVar13;
             }
@@ -146,115 +109,101 @@ CHECK_FOR_ANY_JUMP:
             gte_ldVZ0(0);
             gte_rtv0();
 			
-			int temp;
-            read_mt(iVar9,temp,param_3);
-            movement.vx += iVar9;
-            movement.vy += temp;
-            movement.vz += param_3;
+			
+            read_mt(iVar9,puVar15 + 0x18,param_3);
+            *(int *)(puVar15 + 0x18) = *(int *)(puVar15 + 0x18) + iVar9;
+            *(int *)(puVar15 + 0x20) = *(int *)(puVar15 + 0x20) + param_3;
+            *(char **)(puVar15 + 0x1c) = puVar15 + 0x18 + *(int *)(puVar15 + 0x1c);
+			
+			//int temp;
+            //read_mt(iVar9,temp,param_3);
+            //*(int *)(puVar15 + 0x18) = *(int *)(puVar15 + 0x18) + iVar9;
+            //*(int *)(puVar15 + 0x1c) = *(int *)(puVar15 + 0x1c) + temp;
+            //*(int *)(puVar15 + 0x20) = *(int *)(puVar15 + 0x20) + param_3;
           }
         }
-        goto NOT_JUMPING;
+        goto LAB_80060e1c;
       }
-	  
-      // force driver to jump
-      d->jump_ForcedMS = 0xa0;
-
-      // increment jump counter
-      d->numberOfJumps++;
-
-      d->jump_InitialVelY = d->const_JumpForce;
-
-      // play jump sound
-      DECOMP_OtherFX_Play_Echo(8, 1, d->actionsFlagSet & 0x10000);
+      *(unsigned short *)(uVar12 + 0x3f6) = 0xa0;
+      *(short *)(uVar12 + 0x554) = *(short *)(uVar12 + 0x554) + 1;
+      *(unsigned short *)(uVar12 + 0x3f8) = *(unsigned short *)(uVar12 + 0x418);
+      DECOMP_OtherFX_Play_Echo(8,1,*(unsigned short *)(uVar12 + 0x2ca) & 1);
     }
-    
-	// if being forced to jump (by turtles)
-	else 
-	{
-      // if first frame (basically)
-      if (
-          // if not currently airborne from forced jump
-          (d->jump_ForcedMS == 0) ||
-
-          // if jump_InitialVelY was just now set to const_jump
-          (d->jump_InitialVelY == d->const_JumpForce))
-      {
+    else {
+      if ((*(short *)(uVar12 + 0x3f6) == 0) ||
+         (*(short *)(uVar12 + 0x3f8) == *(short *)(uVar12 + 0x418))) {
         DECOMP_OtherFX_Play(0x7e,1);
       }
       *(unsigned short *)(uVar12 + 0x3f6) = 0xa0;
       if (*(char *)(uVar12 + 0x366) == '\x02') {
-        d->jump_unknown = 0x180;
-        d->jump_InitialVelY = *(short *)(uVar12 + 0x418) * 3;
+        *(unsigned short *)(uVar12 + 0x3fa) = 0x180;
+        *(short *)(uVar12 + 0x3f8) = *(short *)(uVar12 + 0x418) * 3;
       }
       else {
-        d->jump_InitialVelY = (short)((*(short *)(uVar12 + 0x418) * 3) / 2);
+        *(short *)(uVar12 + 0x3f8) = (short)((*(short *)(uVar12 + 0x418) * 3) / 2);
       }
       *(char *)(uVar12 + 0x366) = 0;
     }
   }
-  
-  // ((d->actionsFlagSet & 1) == 1)
   else {
-    if (((d->stepFlagSet & 3) == 0) || (d->baseSpeed < 1)) {
-      if (d->baseSpeed != 0) {
-        if ((((d->terrainMeta1->flags & 4) == 0) ||
-            (d->baseSpeed < 1)) || (-1 < d->speedApprox)) {
-          iVar8 = (int)d->speedApprox;
+    if (((*(unsigned int *)(uVar12 + 0xbc) & 3) == 0) || (*(short *)(uVar12 + 0x39c) < 1)) {
+      if (*(short *)(uVar12 + 0x39c) != 0) {
+        if ((((*(unsigned int *)(*(int *)(uVar12 + 0x358) + 4) & 4) == 0) ||
+            (*(short *)(uVar12 + 0x39c) < 1)) || (-1 < *(short *)(uVar12 + 0x38e))) {
+          iVar8 = (int)*(short *)(uVar12 + 0x38e);
           iVar13 = iVar8;
           if (iVar8 < 0) {
             iVar13 = -iVar8;
           }
-          if (((0x2ff < iVar13) && ((d->baseSpeed < 1 || (iVar8 < 1)))) &&
-             ((-1 < d->baseSpeed || (-1 < iVar8)))) goto PROCESS_ACCEL;
+          if (((0x2ff < iVar13) && ((*(short *)(uVar12 + 0x39c) < 1 || (iVar8 < 1)))) &&
+             ((-1 < *(short *)(uVar12 + 0x39c) || (-1 < iVar8)))) goto LAB_800608fc;
         }
         param_1 = (int)*(short *)(uVar12 + 0x428);
         iVar9 = param_1 + ((int)*(char *)(uVar12 + 0x33) << 5) / 5;
-        if ((d->stepFlagSet & 3) == 0) {
-          if ((*(short *)(uVar12 + 0x3e2) != 0) && (0 < d->baseSpeed)) {
-            iVar9 = d->const_Accel_Reserves;
+        if ((*(unsigned int *)(uVar12 + 0xbc) & 3) == 0) {
+          if ((*(short *)(uVar12 + 0x3e2) != 0) && (0 < *(short *)(uVar12 + 0x39c))) {
+            iVar9 = (int)*(short *)(uVar12 + 0x42a);
           }
-          param_1 = d->terrainMeta1->slowUntilSpeed;
-          if ((param_1 != 0x100) && ((d->actionsFlagSet & 0x800000) == 0)) {
+          param_1 = *(int *)(*(int *)(uVar12 + 0x358) + 0xc);
+          if ((param_1 != 0x100) && ((*(unsigned int *)(uVar12 + 0x2c8) & 0x800000) == 0)) {
             iVar9 = param_1 * iVar9 >> 8;
           }
         }
-        else if (0 < d->baseSpeed) goto SET_HIGH_ACCEL;
+        else if (0 < *(short *)(uVar12 + 0x39c)) goto LAB_8006089c;
       }
     }
     else {
-SET_HIGH_ACCEL:
+LAB_8006089c:
       iVar9 = 8000;
     }
-PROCESS_ACCEL:
+LAB_800608fc:
     uVar10 = (iVar9 * sdata->gGT->elapsedTimeMS) >> 5;
     gte_ldVXY0(0);
     gte_ldVZ0(uVar10 & 0xffff);
     gte_rtv0();
     read_mt(param_1,param_2,param_3);
-	
-    if (d->baseSpeed < 0) {
-      d->unk_offset3B2 = -(short)uVar10;
-      d->unkVectorX = -(short)param_1;
-      d->unkVectorY = -(short)param_2;
-      d->unkVectorZ = -(short)param_3;
+    if (*(short *)(uVar12 + 0x39c) < 0) {
+      *(short *)(uVar12 + 0x3b2) = -(short)uVar10;
+      *(int *)(puVar15 + 0x18) = *(int *)(puVar15 + 0x18) - param_1;
+      *(int *)(puVar15 + 0x20) = *(int *)(puVar15 + 0x20) - param_3;
+      *(unsigned int *)(puVar15 + 0x1c) = *(int *)(puVar15 + 0x1c) - param_2;
+      *(short *)(uVar12 + 0x3ac) = -(short)param_1;
+      *(short *)(uVar12 + 0x3ae) = -(short)param_2;
+      *(short *)(uVar12 + 0x3b0) = -(short)param_3;
     }
     else {
-      d->unk_offset3B2 = (short)uVar10;
-      d->unkVectorX = (short)param_1;
-      d->unkVectorY = (short)param_2;
-      d->unkVectorZ = (short)param_3;
+      *(short *)(uVar12 + 0x3b2) = (short)uVar10;
+      *(int *)(puVar15 + 0x18) = *(int *)(puVar15 + 0x18) + param_1;
+      *(int *)(puVar15 + 0x20) = *(int *)(puVar15 + 0x20) + param_3;
+      *(unsigned int *)(puVar15 + 0x1c) = *(int *)(puVar15 + 0x1c) + param_2;
+      *(short *)(uVar12 + 0x3ac) = (short)param_1;
+      *(short *)(uVar12 + 0x3ae) = (short)param_2;
+      *(short *)(uVar12 + 0x3b0) = (short)param_3;
     }
-	
-    movement.vx += d->unkVectorX;
-    movement.vy += d->unkVectorY;
-    movement.vz += d->unkVectorZ;
-    
-	uVar14 = VehCalc_FastSqrt(
-		movement.vx * movement.vx +
-        movement.vy * movement.vy +
-        movement.vz * movement.vz, 0x10);
-    
-	iVar9 = (int)d->baseSpeed;
+    uVar14 = VehCalc_FastSqrt(*(int *)(puVar15 + 0x18) * *(int *)(puVar15 + 0x18) +
+                          *(int *)(puVar15 + 0x1c) * *(int *)(puVar15 + 0x1c) +
+                          *(int *)(puVar15 + 0x20) * *(int *)(puVar15 + 0x20),0x10);
+    iVar9 = (int)*(short *)(uVar12 + 0x39c);
     if (iVar9 < 0) {
       iVar9 = -iVar9;
     }
@@ -267,50 +216,47 @@ PROCESS_ACCEL:
     if (bVar2) {
       uVar14 = uVar10;
     }
-	
-    if (((d->actionsFlagSet & 1) == 0) || (d->jump_ForcedMS == 0))
-		goto CHECK_FOR_ANY_JUMP;
-    
-	if (d->jump_unknown != 0)
-		d->jump_unknown = 0x180;
-
-    if (d->kartState == KS_BLASTED) 
-	{
-      DECOMP_GAMEPAD_ShockFreq(d,8,0);
-      DECOMP_GAMEPAD_ShockForce1(d,8,0x7f);
+    if (((*(unsigned int *)(uVar12 + 0x2c8) & 1) == 0) || (*(short *)(uVar12 + 0x3f6) == 0))
+    goto LAB_80060ab0;
+    if (*(short *)(uVar12 + 0x3fa) != 0) {
+      *(unsigned short *)(uVar12 + 0x3fa) = 0x180;
+    }
+    if (*(char *)(uVar12 + 0x376) == '\x06') {
+      DECOMP_GAMEPAD_ShockFreq(uVar12,8,0);
+      DECOMP_GAMEPAD_ShockForce1(uVar12,8,0x7f);
     }
   }
 
 // PROCESS_JUMP
-PROCESS_JUMP:
+LAB_80060c30:
 
   iVar9 = 0;
   iVar8 = 0x378;
   *(unsigned short *)(uVar12 + 0x3f2) = 0x180;
   *(unsigned short *)(uVar12 + 0x3f0) = 0;
-  d->actionsFlagSet |= 0x480;
+  *(unsigned int *)(uVar12 + 0x2c8) = *(unsigned int *)(uVar12 + 0x2c8) | 0x480;
   iVar13 = iVar9;
-  
-  iVar4 = DECOMP_VehPhysGeneral_JumpGetVelY(uVar12 + iVar8,&movement);
-  iVar6 = iVar4;
-  if (iVar4 < 0) {
-    iVar6 = -iVar4;
-  }
-  iVar5 = iVar9;
-  if (iVar9 < 0) {
-    iVar5 = -iVar9;
-  }
-  if (iVar5 < iVar6) {
-    iVar9 = iVar4;
-  }
-  
-  // AxisAngle
+  do {
+    iVar4 = DECOMP_VehPhysGeneral_JumpGetVelY(uVar12 + iVar8,puVar15 + 0x18);
+    iVar6 = iVar4;
+    if (iVar4 < 0) {
+      iVar6 = -iVar4;
+    }
+    iVar5 = iVar9;
+    if (iVar9 < 0) {
+      iVar5 = -iVar9;
+    }
+    if (iVar5 < iVar6) {
+      iVar9 = iVar4;
+    }
+    iVar13 = iVar13 + 1;
+    iVar8 = iVar8 + 8;
+  } while (iVar13 < 1);
   iVar13 = uVar12 + 0x360;
-  if ((d->actionsFlagSet & 1) == 0) {
+  if ((*(unsigned int *)(uVar12 + 0x2c8) & 1) == 0) {
     iVar13 = uVar12 + 0x368;
   }
-  iVar8 = DECOMP_VehPhysGeneral_JumpGetVelY(iVar13,&movement);
-  
+  iVar8 = DECOMP_VehPhysGeneral_JumpGetVelY(iVar13,puVar15 + 0x18);
   iVar13 = iVar8;
   if (iVar8 < 0) {
     iVar13 = -iVar8;
@@ -324,55 +270,50 @@ PROCESS_JUMP:
     iVar4 = iVar8 * iVar8;
     iVar9 = iVar8;
   }
-  
-  iVar13 = VehCalc_FastSqrt(
-			iVar4 + (int)d->jump_InitialVelY * (int)d->jump_InitialVelY >> 8, 8);
-						
-  // 0 for most tracks, 0x50 for Sewer Speedway
+  iVar13 = VehCalc_FastSqrt(iVar4 + (int)*(short *)(uVar12 + 0x3f8) * (int)*(short *)(uVar12 + 0x3f8) >>
+                        8, 8);
   uVar10 = sdata->gGT->level1->unk_18C << 8;
-  if (uVar10 == 0) {
+  if (sdata->gGT->level1->unk_18C == 0) {
     uVar10 = 0x3700;
   }
   else if (0x5000 < uVar10) {
     uVar10 = 0x5000;
   }
-  
   uVar11 = iVar13 - iVar9;
   if ((int)uVar10 < iVar13 - iVar9) {
     uVar11 = uVar10;
   }
-  if (movement.vy < (int)uVar11) {
-	movement.vy = uVar11;
+  if (*(int *)(puVar15 + 0x1c) < (int)uVar11) {
+    *(unsigned int *)(puVar15 + 0x1c) = uVar11;
   }
 
 // [end of the first frame of jump]
 
 // skip here if not jumping
   
-NOT_JUMPING:
-
-  VehPhysCrash_ConvertVecToSpeed(d,&movement);
-  iVar9 = d->speed - uVar14;
-  d->speed = (short)iVar9;
+LAB_80060e1c:
+  VehPhysCrash_ConvertVecToSpeed(uVar12,puVar15 + 0x18/*,0*/);
+  iVar9 = *(unsigned short *)(uVar12 + 0x38c) - uVar14;
+  *(short *)(uVar12 + 0x38c) = (short)iVar9;
   if (iVar9 * 0x10000 < 0) {
-    d->speed = 0;
+    *(unsigned short *)(uVar12 + 0x38c) = 0;
   }
-  iVar9 = (int)d->speedApprox;
+  iVar9 = (int)*(short *)(uVar12 + 0x38e);
   if (iVar9 < 0) {
     if (iVar9 < 0) {
       iVar9 = -iVar9;
     }
     if (iVar9 < 0x100) {
-      sVar7 = d->unk36E - (d->unk36E >> 3);
+      sVar7 = *(short *)(uVar12 + 0x36e) - (*(short *)(uVar12 + 0x36e) >> 3);
     }
     else {
-      sVar7 = (short)(d->unk36E * 0xd +
+      sVar7 = (short)(*(short *)(uVar12 + 0x36e) * 0xd +
                       (sdata->gGT->timer & 7) * 0x300 >> 4);
     }
   }
   else {
-    sVar7 = (short)(d->unk36E * 0xd + iVar9 * 3 >> 4);
+    sVar7 = (short)(*(short *)(uVar12 + 0x36e) * 0xd + iVar9 * 3 >> 4);
   }
-  d->unk36E = sVar7;
+  *(short *)(uVar12 + 0x36e) = sVar7;
   return;
 }
