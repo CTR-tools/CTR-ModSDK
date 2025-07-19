c08da5ae6588ae720f49b2d027533f890060dae4
nprocopi2017@gmail.com <nprocopi2017@gmail.com>
2025-07-19 16:59:27 -0400

    Fix MEMCARD_SkipEvents

--- decompile/General/MEMCARD/MEMCARD_11_SkipEvents.c
+++ decompile/General/MEMCARD/MEMCARD_11_SkipEvents.c
@@ -3,8 +3,6 @@
 void DECOMP_MEMCARD_SkipEvents(void)
 {
     // Flush all "previous" Events until everything shows PENDING
-    while (
-			(MEMCARD_GetNextSwEvent() != MC_RETURN_PENDING) && 
-			(MEMCARD_GetNextHwEvent() != MC_RETURN_PENDING)
-		);
+    while (DECOMP_MEMCARD_GetNextSwEvent() != MC_RETURN_PENDING);
+    while (DECOMP_MEMCARD_GetNextHwEvent() != MC_RETURN_PENDING);
 }
