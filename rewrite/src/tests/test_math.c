8e4445d698f84d36f38496042c35a2b82ba8ab8d
mateusfavarin <mateusfavarinc@gmail.com>
2025-09-21 17:21:02 -0700

    code review 2

--- rewrite/src/tests/test_math.c
+++ rewrite/src/tests/test_math.c
@@ -6,7 +6,7 @@ void TEST_MATH_Sin(u32 angle, s32 ret)
 {
     const u32 index = PatchFunction_Beg((u32*)(&ND_MATH_Sin));
     const s32 expected = ND_MATH_Sin(angle);
-    if (expected != ret) { ND_printf("[MATH_Sin] Test Failed:\nInput: %d\nExpected: %d\nResult:%d\n", angle, expected, ret); }
+    if (expected != ret) { ND_printf("[MATH_Sin] Test Failed:\nInput: %d\nExpected: %d\nResult: %d\n", angle, expected, ret); }
     PatchFunction_End(index);
 }
 
@@ -14,7 +14,7 @@ void TEST_MATH_Cos(u32 angle, s32 ret)
 {
     const u32 index = PatchFunction_Beg((u32*)(&ND_MATH_Cos));
     const s32 expected = ND_MATH_Cos(angle);
-    if (expected != ret) { ND_printf("[MATH_Cos] Test Failed:\nInput: %d\nExpected: %d\nResult:%d\n", angle, expected, ret); }
+    if (expected != ret) { ND_printf("[MATH_Cos] Test Failed:\nInput: %d\nExpected: %d\nResult: %d\n", angle, expected, ret); }
     PatchFunction_End(index);
 }
 
@@ -22,7 +22,7 @@ void TEST_MATH_Sqrt(u32 n, u32 shift, u32 ret)
 {
     const u32 index = PatchFunction_Beg((u32*)(&ND_MATH_Sqrt));
     const u32 expected = ND_MATH_Sqrt(n, shift);
-    if (expected != ret) { ND_printf("[MATH_Sqrt] Test Failed:\nInput: %d %d\nExpected: %d\nResult:%d\n", n, shift, expected, ret); }
+    if (expected != ret) { ND_printf("[MATH_Sqrt] Test Failed:\nInput: %d %d\nExpected: %d\nResult: %d\n", n, shift, expected, ret); }
     PatchFunction_End(index);
 }
 
@@ -39,7 +39,7 @@ void TEST_MATH_VectorLength(const SVec3* vector, s32 ret)
 {
     const u32 index = PatchFunction_Beg((u32*)(&ND_MATH_VectorLength));
     const s32 expected = ND_MATH_VectorLength(vector);
-    if (expected != ret) { ND_printf("[MATH_VectorLength] Test Failed:\nInput: %d %d %d\nExpected: %d\nResult:%d\n", vector->x, vector->y, vector->z, expected, ret); }
+    if (expected != ret) { ND_printf("[MATH_VectorLength] Test Failed:\nInput: %d %d %d\nExpected: %d\nResult: %d\n", vector->x, vector->y, vector->z, expected, ret); }
     PatchFunction_End(index);
 }
 
