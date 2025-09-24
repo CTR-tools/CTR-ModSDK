c4b8bf77612b252f2c10ac5a6b4f38a345a1b2eb
Niko <nprocopi2017@gmail.com>
2025-09-23 21:30:28 -0400

    profiler template for many tests

--- rewrite/src/profiler/benchmark.c
+++ rewrite/src/profiler/benchmark.c
@@ -1,37 +1,48 @@
+#ifdef REWRITE_PROFILER
+
 #include <ctr/nd.h>
 #include <ctr/coll.h>
 #include <ctr/test.h>
 
-// When running OG functions,
-// dont forget to undefine these in ctr\test.h:
-// TEST_MATH_IMPL, TEST_RNG_IMPL, TEST_COLL_IMPL
-#ifdef REWRITE_PROFILER
-
-void RunBenchmark()
+void RunCollRW()
 {
-		// Sample input from no$psx
-		// $a0: 1f800154 - 0, 0, 0
-		// $a1: 1f800270 - 0000, 032A, EDE4
-		// $a2: 1f800220 - FD00, 02EC, EA58
-		// $a3: 1f800164 - FD6F, 02F5, EDCE
-		SVec3 out = {};
-		SVec3 v1 = {0x0, 0x32A, 0xEDE4};
-		SVec3 v2 = {0xFD00, 0x2EC, 0xEA58};
-		SVec3 point = {0xFD6F, 0x2F5, 0xEDCE};
-
-		// if 0 + COLL_XXX: Run the New function
-		// if 1 + ND_COLL_XXX: Run the OG function
-		#if 1
-			// Revert the Hook from OG -> DECOMP
-			PatchFunction_Beg((u32*)(&ND_COLL_ProjectPointToEdge));
-		#endif
-
-		// Test 1000 times
-		for(int i = 0; i < 100; i++)
-		{
-			ND_COLL_ProjectPointToEdge(&out, &v1, &v2, &point);
-		}
-
+	// Sample input from no$psx
+	// $a0: 1f800154 - 0, 0, 0
+	// $a1: 1f800270 - 0000, 032A, EDE4
+	// $a2: 1f800220 - FD00, 02EC, EA58
+	// $a3: 1f800164 - FD6F, 02F5, EDCE
+	SVec3 out = {};
+	SVec3 v1 = {0x0, 0x32A, 0xEDE4};
+	SVec3 v2 = {0xFD00, 0x2EC, 0xEA58};
+	SVec3 point = {0xFD6F, 0x2F5, 0xEDCE};
+	
+	// million
+	for(int i = 0; i < 1000000; i++)
+	{
+		COLL_ProjectPointToEdge(&out, &v1, &v2, &point);
+	}
 }
 
-#endif // REWRITE_PROFILER
+void RunCollOG()
+{
+	// Sample input from no$psx
+	// $a0: 1f800154 - 0, 0, 0
+	// $a1: 1f800270 - 0000, 032A, EDE4
+	// $a2: 1f800220 - FD00, 02EC, EA58
+	// $a3: 1f800164 - FD6F, 02F5, EDCE
+	SVec3 out = {};
+	SVec3 v1 = {0x0, 0x32A, 0xEDE4};
+	SVec3 v2 = {0xFD00, 0x2EC, 0xEA58};
+	SVec3 point = {0xFD6F, 0x2F5, 0xEDCE};
+	
+	// Revert the Hook from OG -> DECOMP
+	PatchFunction_Beg((u32*)(&ND_COLL_ProjectPointToEdge));
+	
+	// million
+	for(int i = 0; i < 1000000; i++)
+	{
+		ND_COLL_ProjectPointToEdge(&out, &v1, &v2, &point);
+	}
+}
+
+#endif
--- rewrite/src/profiler/profiler.c
+++ rewrite/src/profiler/profiler.c
@@ -1,7 +1,7 @@
+#ifdef REWRITE_PROFILER
+
 #include <ctr/profiler.h>
 #include <ctr/nd.h>
-
-#ifdef REWRITE_PROFILER
 
 // No Vehicle.h
 struct MetaPhys
@@ -42,69 +42,87 @@ struct MetaPhys
 #include <namespace_UI.h>
 #include <regionsEXE.h>
 
+void ND_EnterCriticalSection();
+void ND_ExitCriticalSection();
+
 void ND_DrawOTag(int a);
 void ND_DecalFont_DrawLine(char* a, int b, int c, int d, int e);
 
-
 int ND_GetRCnt(int a);
+int ND_ResetRCnt(int a);
 
-int Debug_GetPreciseTime()
-{
-	int sysClock =
-		ND_GetRCnt(0xf2000001) +
-		sdata->rcntTotalUnits;
-
-	return sysClock;
-}
-
-// Jam this here, cause byte budget
-static int timeRed=0;
-static int timeStart=0;
-static int timeEnd=0;
-void Hook_DrawOTag(int a)
-{
-	// Calculate ONE time, then save persistently
-	if (timeRed == 0)
-	{
-		timeStart = Debug_GetPreciseTime();
-
-		void RunBenchmark();
-		RunBenchmark();
-
-		timeEnd = Debug_GetPreciseTime();
-
-
-		timeRed = timeEnd - timeStart;
-	}
-
-	struct GameTracker* gGT = sdata->gGT;
-
-	if((gGT->gameMode1 & (LOADING|1)) == 0)
-	{
-		// reset depth to CLOSEST
-		gGT->pushBuffer_UI.ptrOT =
-			gGT->otSwapchainDB[gGT->swapchainIndex];
-
-
-		#ifndef REBUILD_PC
-		char* string = (char*)0x1f800000;
-		#else
-		char string[128];
-		#endif
-
-		ND_sprintf(string, "RED  %d", timeRed);
-		ND_DecalFont_DrawLine(string, 0x14, 0x5C, FONT_SMALL, 0);
-	}
-
-	ND_DrawOTag(a);
-}
+// OG = Original
+// RW = Rewrite
+int timeOG_COLL_ProjectPointToEdge;
+int timeRW_COLL_ProjectPointToEdge;
 
 #define JMP(dest) (((unsigned long)dest & 0x3FFFFFF) >> 2 | 0x8000000)
 #define JAL(dest) (((unsigned long)dest & 0x3FFFFFF) >> 2 | 0xC000000)
 
+void Hook_DrawOTag(int a);
+void RunCollOG();
+void RunCollRW();
+
+// When running OG functions,
+// dont forget to undefine these in ctr\test.h:
+// TEST_MATH_IMPL, TEST_RNG_IMPL, TEST_COLL_IMPL
+
+// Also, required to run RW function before OG,
+// because of how assembly hooking system works
+
 void LoadProfilerPatches()
-{
+{	
 	*(int*)0x800379b0 = JAL(Hook_DrawOTag);
 }
 
-#endif // REWRITE_PROFILER
+int doThisOnce=0;
+void RunAllBenchmarks()
+{
+	if(doThisOnce != 0)
+		return;
+	
+	int r = 0;
+	doThisOnce = 1;
+	
+	ND_ResetRCnt(0xf2000001);
+	RunCollRW();
+	r = ND_GetRCnt(0xf2000001);
+	timeRW_COLL_ProjectPointToEdge = r;
+	
+	ND_ResetRCnt(0xf2000001);
+	RunCollOG();
+	r = ND_GetRCnt(0xf2000001);
+	timeOG_COLL_ProjectPointToEdge = r;
+}
+
+void Hook_DrawOTag(int a)
+{
+	ND_EnterCriticalSection();
+	RunAllBenchmarks();
+	ND_ExitCriticalSection();
+
+	// Why does this break on console?
+	// struct GameTracker* gGT = sdata->gGT;
+	struct GameTracker* gGT = (struct GameTracker*)0x80096b20;
+	
+	if((gGT->gameMode1 & (LOADING|1)) == 0)
+	{
+		// reset depth to CLOSEST
+		gGT->pushBuffer_UI.ptrOT =
+			gGT->otSwapchainDB[gGT->swapchainIndex];	
+
+		char string[128];
+		
+		ND_DecalFont_DrawLine("1s  15720", 0x14, 0x8, FONT_SMALL, 0);
+		
+		ND_sprintf(string, "RW  %d", timeRW_COLL_ProjectPointToEdge);
+		ND_DecalFont_DrawLine(string, 0x14, 0x10, FONT_SMALL, 0);
+		
+		ND_sprintf(string, "OG  %d", timeOG_COLL_ProjectPointToEdge);
+		ND_DecalFont_DrawLine(string, 0x14, 0x18, FONT_SMALL, 0);
+	}
+	
+	ND_DrawOTag(a);
+}
+
+#endif
