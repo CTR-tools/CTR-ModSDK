b4ec3a553be36c39690b7a00409c7daf240cbdc1
Niko <nprocopi2017@gmail.com>
2025-09-26 23:13:39 -0400

    Scratch vs Stack benchmarking for COLL_PPTE

--- rewrite/src/profiler/benchmark.h
+++ rewrite/src/profiler/benchmark.h
@@ -1,5 +1,11 @@
 
-void RunTest_RW_COLL_ProjectPointToEdge()
+// PPTE = ProjectPointToEdge
+// RW = Rewrite
+// ND = Naughty Dog (original)
+// STACK = $sp memory
+// SCRATCH = scratchpad
+
+void RunTest_RW_COLL_PPTE_STACK()
 {
 	// Sample input from no$psx
 	// $a0: 1f800154 - 0, 0, 0
@@ -18,7 +24,7 @@ void RunTest_RW_COLL_ProjectPointToEdge()
 	}
 }
 
-void RunTest_ND_COLL_ProjectPointToEdge()
+void RunTest_ND_COLL_PPTE_STACK()
 {
 	// Sample input from no$psx
 	// $a0: 1f800154 - 0, 0, 0
@@ -40,9 +46,74 @@ void RunTest_ND_COLL_ProjectPointToEdge()
 	}
 }
 
+void RunTest_RW_COLL_PPTE_SCRATCH()
+{
+	SVec3* out = (SVec3*)0x1f800154;
+	SVec3* v1 = (SVec3*)0x1f800270; //{0x0, 0x32A, 0xEDE4};
+	SVec3* v2 = (SVec3*)0x1f800220; //{0xFD00, 0x2EC, 0xEA58};
+	SVec3* point = (SVec3*)0x1f800164; //{0xFD6F, 0x2F5, 0xEDCE};
+	
+	out->v[0] = 0;
+	out->v[1] = 0;
+	out->v[2] = 0;
+	
+	v1->v[0] = 0x0;
+	v1->v[1] = 0x32A;
+	v1->v[2] = 0xEDE4;
+	
+	v2->v[0] = 0xFD00;
+	v2->v[1] = 0x2EC;
+	v2->v[2] = 0xEA58;
+	
+	point->v[0] = 0xFD6F;
+	point->v[1] = 0x2F5;
+	point->v[2] = 0xEDCE;
+	
+	// million
+	for(int i = 0; i < 1000000; i++)
+	{
+		COLL_ProjectPointToEdge(out, v1, v2, point);
+	}
+}
+
+void RunTest_ND_COLL_PPTE_SCRATCH()
+{
+	SVec3* out = (SVec3*)0x1f800154;
+	SVec3* v1 = (SVec3*)0x1f800270; //{0x0, 0x32A, 0xEDE4};
+	SVec3* v2 = (SVec3*)0x1f800220; //{0xFD00, 0x2EC, 0xEA58};
+	SVec3* point = (SVec3*)0x1f800164; //{0xFD6F, 0x2F5, 0xEDCE};
+	
+	out->v[0] = 0;
+	out->v[1] = 0;
+	out->v[2] = 0;
+	
+	v1->v[0] = 0x0;
+	v1->v[1] = 0x32A;
+	v1->v[2] = 0xEDE4;
+	
+	v2->v[0] = 0xFD00;
+	v2->v[1] = 0x2EC;
+	v2->v[2] = 0xEA58;
+	
+	point->v[0] = 0xFD6F;
+	point->v[1] = 0x2F5;
+	point->v[2] = 0xEDCE;
+	
+	// Revert the Hook from OG -> DECOMP
+	PatchFunction_Beg((u32*)(&ND_COLL_ProjectPointToEdge));
+	
+	// million
+	for(int i = 0; i < 1000000; i++)
+	{
+		ND_COLL_ProjectPointToEdge(out, v1, v2, point);
+	}
+}
+
 // Always run RW (rewrite) before OG (original)
 struct BenchTest tests[] =
 {
-	ADDTEST(RunTest_RW_COLL_ProjectPointToEdge),
-	ADDTEST(RunTest_ND_COLL_ProjectPointToEdge)
+	ADDTEST(RunTest_RW_COLL_PPTE_STACK),
+	ADDTEST(RunTest_ND_COLL_PPTE_STACK),
+	ADDTEST(RunTest_RW_COLL_PPTE_SCRATCH),
+	ADDTEST(RunTest_ND_COLL_PPTE_SCRATCH),
 };
